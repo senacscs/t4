9e4638611e8b9be087b8a873693ff26c8decf763
iuriwickert <167103303+iuriwickert@users.noreply.github.com>
2024-05-27 12:19:06 -0300

    sla

---
+++ wickert/Indicacoescss/script.js
@@ -0,0 +1,16 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function() {
+    nextImage();
+}, 5000);
+
+function nextImage(){
+    count++;
+    if(count>7){
+        count = 1
+    }
+
+    document.getElementById("radio"+count).checked = true;
+
+}
