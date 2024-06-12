52e741c6e11734d44d5905a9f099f7a28349b26b
CiciinhoCC <87839799+CiciinhoCC@users.noreply.github.com>
2024-06-12 18:30:54 -0300

    index da turma

---
+++ main.js
@@ -0,0 +1,35 @@
+function criarElem(tag, atrib) {
+    let elem = document.createElement(tag);
+    if (atrib != [[]]) {
+        for (j = 0; j < atrib.length; j++) {
+            elem.setAttribute(atrib[j][0], atrib[j][1]);
+        }
+    }
+    return elem;
+}
+
+alunos.sort(function(a,b){
+    if (a.nome < b.nome) {
+        return -1;
+      }
+      if (a.nome > b.nome) {
+        return 1;
+      }
+      return 0;
+})
+
+console.log(alunos)
+
+const lista = document.getElementById('lista');
+
+for (i = 0; i < alunos.length; i++) {
+    const p = criarElem('p', [[]]);
+    const div = criarElem('div',[["class","nomes"]]);
+    const a = criarElem('a',[["href","./"+alunos[i].pasta]]);
+
+    p.appendChild(document.createTextNode(alunos[i].nome));
+
+    div.appendChild(p);
+    a.appendChild(div);
+    lista.appendChild(a);
+}
