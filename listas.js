905181bc8780484b41fe5e5084d04f05213cbcf0
CiciinhoCC <87839799+CiciinhoCC@users.noreply.github.com>
2024-06-19 09:16:00 -0300

    adicionei lista de filtros (perdoe-me pela falta de estilo)

---
+++ listas.js
@@ -0,0 +1,50 @@
+alunos = [
+    { "nome": "Analice", "pasta": "ana" },
+    { "nome": "Andrei", "pasta": "andrei" },
+    { "nome": "Andri", "pasta": "andri" },
+    { "nome": "Bernardo", "pasta": "aquino" },
+    { "nome": "Augusto", "pasta": "augusto" },
+    { "nome": "Pedro B.", "pasta": "backes" },
+    { "nome": "Thom", "pasta": "brandt" },
+    { "nome": "Cícero", "pasta": "cicero" },
+    { "nome": "Clara", "pasta": "clara" },
+    { "nome": "Guilherme D.", "pasta": "dalke" },
+    { "nome": "Davi", "pasta": "davie" },
+    { "nome": "Eduardo D.", "pasta": "edu" },
+    { "nome": "Ana E.", "pasta": "elisa" },
+    { "nome": "Enzo", "pasta": "enzo" },
+    { "nome": "João E.", "pasta": "ernesto" },
+    { "nome": "Gabriel", "pasta": "gabriel" },
+    { "nome": "Luís", "pasta": "garcia" },
+    { "nome": "Gustavo", "pasta": "gustavo" },
+    { "nome": "Henrique", "pasta": "henrique" },
+    { "nome": "João G.", "pasta": "joaogoetze" },
+    { "nome": "Joaquim", "pasta": "joaquim" },
+    { "nome": "Guilherme K.", "pasta": "kipper" },
+    { "nome": "Larissa", "pasta": "larii" },
+    { "nome": "Lucas", "pasta": "lucas" },
+    { "nome": "Marina", "pasta": "marina" },
+    { "nome": "Manuela", "pasta": "mohr" },
+    { "nome": "Eduardo M.", "pasta": "moraes" },
+    { "nome": "Nicole", "pasta": "nick" },
+    { "nome": "Pedro A.", "pasta": "pedroa" },
+    { "nome": "Pedro F.", "pasta": "pedrofa" },
+    { "nome": "Rafael", "pasta": "rafaelmarmitt" },
+    { "nome": "Sara", "pasta": "sararosa" },
+    { "nome": "Arthur", "pasta": "scherer" },
+    { "nome": "Matheus B.", "pasta": "teteu" },
+    { "nome": "Theo", "pasta": "theo" },
+    { "nome": "Iuri", "pasta": "wickert" },
+    { "nome": "Matheus F.", "pasta": "" }
+];
+
+
+// ---------------------------
+
+projetos = [
+    { "nome": "Indicações HTML", "url": "indicacoeshtml" },
+    { "nome": "Indicações CSS", "url": "indicacoescss" },
+    { "nome": "Site pros responsáveis", "url": "responsaveis" },
+    { "nome": "Currículo", "url": "curriculo" },
+    { "nome": "Olá Mundo", "url": "olamundo" },
+]
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function criarElem(tag, atrib) {
+function criarElem(tag, atrib = null) {
     let elem = document.createElement(tag);
-    if (atrib != [[]]) {
+    if(atrib != null){
         for (j = 0; j < atrib.length; j++) {
             elem.setAttribute(atrib[j][0], atrib[j][1]);
         }
@@ -8,14 +8,30 @@ function criarElem(tag, atrib) {
     return elem;
 }
 
-alunos.sort(function(a,b){
+
+//CRIANDO LISTA DE FILTROS
+
+const filtros = document.getElementById('filters');
+
+for (i = 0; i < projetos.length; i++) {
+    const span = criarElem('span');
+    const input = criarElem('input',[["type","radio"],["name","filter-param"],["class","filter"],["value",projetos[i].url]]);
+
+    span.appendChild(document.createTextNode(projetos[i].nome));
+    filtros.appendChild(input);
+    filtros.appendChild(span);
+}
+
+//CRIANDO LISTA DE ALUNOS
+
+alunos.sort(function (a, b) {
     if (a.nome < b.nome) {
         return -1;
-      }
-      if (a.nome > b.nome) {
+    }
+    if (a.nome > b.nome) {
         return 1;
-      }
-      return 0;
+    }
+    return 0;
 })
 
 console.log(alunos)
@@ -27,9 +43,9 @@ erro.remove()
 
 
 for (i = 0; i < alunos.length; i++) {
-    const p = criarElem('p', [[]]);
-    const div = criarElem('div',[["class","nomes"]]);
-    const a = criarElem('a',[["href","./"+alunos[i].pasta],["target","_blank"]]);
+    const p = criarElem('p');
+    const div = criarElem('div', [["class", "nomes"]]);
+    const a = criarElem('a', [["href", `./${alunos[i].pasta}/`],["target", "_blank"]]);
 
     p.appendChild(document.createTextNode(alunos[i].nome));
 
