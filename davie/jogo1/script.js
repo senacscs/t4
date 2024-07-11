5bc6b1f975c7eec2c30b8abd22694980714c4a63
davie500 <167103798+davie500@users.noreply.github.com>
2024-07-11 09:18:49 -0300

    upload site do jogo

--- davie/jogo1/script.js
+++ davie/jogo1/script.js
@@ -14,14 +14,14 @@ function check3(){
         location.href = "questao4.html"
         alert('Próximo Enigma.');
     }else{
-        location.href = "questao5.html"
+        location.href = "questao2.html"
     }
 }
 
 function check4(){
     var resposta = document.getElementById('resposta').value;
     if(resposta == "1/2 e 1"){
-        location.href = "questao4.html"
+        location.href = "questao3.html"
         alert('Próximo Enigma.');
     }else{
         location.href = "questao5.html"
@@ -31,40 +31,42 @@ function check4(){
 function check5(){
     var resposta = document.getElementById('resposta').value;
     if(resposta == "1 e -3/2"){
-        location.href = "questao4.html"
+        location.href = "questao6.html"
         alert('Próximo Enigma.');
     }else{
-        location.href = "questao5.html"
+        location.href = "questao4.html"
     }
 }
 
 function check6(){
     var resposta = document.getElementById('resposta').value;
     if(resposta == "{1,5}"){
-        location.href = "questao4.html"
+        location.href = "questao7.html"
         alert('Tá indo muito bem!');
     }else{
-        location.href = "questao5.html"
+        location.href = "questao1.html"
+        alert("Voltou tudo, HAHA!")
     }
 }
 
 function check7(){
     var resposta = document.getElementById('resposta').value;
     if(resposta == "-4"){
-        location.href = "questao4.html"
+        location.href = "questao8.html"
         alert("Foi indo muito bem! Agora é a última!");
     }else{
-        location.href = "questao5.html"
+        location.href = "questao1.html"
+        alert("Quase chegou na última questão!")
     }
 }
 
 function check8(){
     var resposta = document.getElementById('resposta').value;
     if(resposta == "{-2,3}"){
-        location.href = "questao4.html"
-        alert("Tá indo bem!");
+        location.href = ".html"
+        alert("Parabéns, acertou todas as questões do jeito que foi pedido.");
     }else{
         location.href = "questao1.html"
-        alert("Voltou tudo!");
+        alert("Voltou tudo, agora estude mais e para de querer adivinhar!");
     }
 }
