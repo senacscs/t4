81967417dac9aa9e207b86d79524548533fadc2c
Manu Mohr <167103609+manumohrr@users.noreply.github.com>
2024-08-02 09:20:29 -0300

    index atualizada

--- mohr/script.js
+++ mohr/script.js
@@ -4,7 +4,7 @@ function updateTime() {
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    const timeString = `${hours}:${minutes}`;
     timeElement.textContent = timeString;
 }
 
