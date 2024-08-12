495e30d628afb5c937f4a3c23b8f3110fec538d2
Manu Mohr <167103609+manumohrr@users.noreply.github.com>
2024-08-12 11:39:32 -0300

    pondo alguns sites no ar

---
+++ mohr/indexerrada/script.js
@@ -0,0 +1,12 @@
+function updateTime() {
+    const timeElement = document.getElementById('time');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    timeElement.textContent = timeString;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
