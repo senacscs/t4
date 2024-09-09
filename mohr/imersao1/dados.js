10909037dfca8d9f12fec6c857d93ead7d0ff487
Manu Mohr <167103609+manumohrr@users.noreply.github.com>
2024-09-09 11:16:52 -0300

    imersão 1 comit

---
+++ mohr/imersao1/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Bob Esponja",
+        descricao: "Bob Esponja é uma esponja do mar super feliz que mora numa casa de abacaxi no fundo do mar. Ele adora cozinhar hambúrgueres no Siri Cascudo e brincar com seus amigos Patrick e Lula Molusco. Bob Esponja é sempre otimista e adora se divertir!",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
