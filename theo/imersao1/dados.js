b699de897ec8bb8e27c56645f6b4ff61929cbdd9
theocarpes <70979960+theocarpes@users.noreply.github.com>
2024-09-09 11:15:43 -0300

    colocando imersao modificada

---
+++ theo/imersao1/dados.js
@@ -0,0 +1,21 @@
+
+let dados = [
+    {
+        titulo: "Luiz Rauber",
+        descricao: "Luiz Rauber é o professor de técnico no Senac Santa Cruz.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "tecnico técnico"
+    },
+    {
+        titulo: "Carlos",
+        descricao: "Carlos é o professor de sociologia e PV. Geralmente é bem deboa mas qunado atrapalham a sua aula fica bem bravo.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "sociologia pv projeto de vida"
+    },
+    {
+        titulo: "João",
+        descricao: "João é o professor de Educação Fisica e PI. Nas aulas de Ed. Fisica sempre diz que quanto mais conversamos menos tempo de jogar vamos ter.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "educação fisica PI ed fisica"
+    }
+];
