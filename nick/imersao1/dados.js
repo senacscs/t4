789fbf65d789f8e5165a2c86a2de6ba6cee31056
nicolekauane <167103683+nicolekauane@users.noreply.github.com>
2024-09-09 11:37:56 -0300

    index e js

---
+++ nick/imersao1/dados.js
@@ -0,0 +1,21 @@
+
+let dados = [
+    {
+        titulo: "Meredith Grey",
+        descricao: "Meredith Grey é a protagonista de Grey's Anatomy, uma cirurgiã brilhante e determinada que enfrenta inúmeros desafios pessoais e profissionais ao longo da série. Sua jornada é marcada por perdas, amor, crescimento e a busca incansável por excelência na medicina. A personagem se tornou um ícone da televisão, inspirando milhões de fãs com sua força e resiliência.",
+        link: "https://pt.wikipedia.org/wiki/Meredith_Grey",
+        tags: "Grey"
+    },
+    {
+        titulo: "George O'Malley",
+        descricao: "George O'Malley foi um dos internos mais queridos de Grey's Anatomy. Sua jornada no Seattle Grace Hospital foi marcada por desafios, crescimento pessoal e um grande amor por Meredith Grey. Tragicamente, seu destino foi interrompido de forma abrupta, deixando uma marca profunda nos corações dos fãs da série.",
+        link: "https://pt.wikipedia.org/wiki/George_O%27Malley",
+        tags: "o'malley"
+    },
+    {
+        titulo: "Alex Karev",
+        descricao: "Alex Karev, um dos personagens mais complexos de Grey's Anatomy, iniciou sua jornada como um interno arrogante e rude, mas evoluiu para um médico talentoso e leal. Sua relação com Meredith, Izzie e Cristina moldou sua personalidade, e sua saída inesperada para reatar com Izzie marcou o fim de uma era na série.",
+        link: "https://pt.wikipedia.org/wiki/Alex_Karev",
+        tags: "kavev"
+    }
+];
