04eeae5f5698acb977008a06b2ed8e91c8ffe584
luscaszs <167103492+luscaszs@users.noreply.github.com>
2024-09-09 11:26:41 -0300

    adição site imersão

---
+++ lucas/imersao1/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Taylor Swift",
+        descricao: "Taylor venceu nas categorias: Álbum do ano com 'Midnights';  Melhor álbum pop novamente com 'Midnights'.",
+        link: "https://g1.globo.com/pop-arte/musica/noticia/2024/02/04/grammy-2024-veja-os-vencedores.ghtml",
+        tags: "taylor basica"
+    },
+    {
+        titulo: "Billie Eilish",
+        descricao: "Billie venceu nas gategorias: Canção do ano com 'What was I made for?';  Melhor canção composta para mídia visual novamente com 'What was I made for?'.",
+        link: "https://www.grammy.com/news/2024-grammys-billie-eilish-wins-song-of-the-year-what-was-i-made-for-barbie",
+        tags: "billie queen"
+    },
+    {
+        titulo: "Miley Cyrus",
+        descricao: "Miley venceu nas categorias: Gravação do ano com 'Flowers';  Melhor performance solo de pop com 'Flowers'.",
+        link: "https://www.grammy.com/news/miley-cyrus-grammys-performance-2024-flowers",
+        tags: "miley flop"
+    }
+];
