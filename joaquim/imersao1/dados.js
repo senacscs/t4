b0e7d5537a770bc8b28d6915869ae3e2ed02ef66
joaquimthome <167103266+joaquimthome@users.noreply.github.com>
2024-09-09 11:27:27 -0300

    imersao + index

---
+++ joaquim/imersao1/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Derek",
+        descricao: "Derek é um rapper e freestyler brasileiro, amplamente conhecido no cenário das batalhas de rap. Ele se destacou especialmente em competições de batalha de rimas.",
+        link: "https://notthesamo.com/por-tras-do-corre-de-derek-luccas/",
+        tags: "trap ttf rap"
+    },
+    {
+        titulo: "Ryu the Runner",
+        descricao: "Ryu the Runner é um rapper e freestyler brasileiro, conhecido por seu estilo agressivo e técnico, além de sua presença em batalhas, Ryu também desenvolveu uma carreira no rap, lançando músicas e projetos que refletem seu talento com letras fortes e densas.",
+        link: "https://www.last.fm/pt/music/Ryu,+the+Runner/+wiki",
+        tags: "corredor rap trap"
+    },
+    {
+        titulo: "BK",
+        descricao: "BK (também conhecido como Abebe Bikila) é um rapper carioca, um dos nomes mais respeitados no rap nacional contemporâneo. Ele é conhecido por suas letras introspectivas, que abordam temas sociais, políticos e pessoais com profundidade e lirismo.",
+        link: "https://pt.wikipedia.org/wiki/BK%27",
+        tags: "Abebe rap"
+    }
+];
