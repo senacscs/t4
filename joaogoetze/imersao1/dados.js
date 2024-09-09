edd71a576d0a8655f9263726e8bb923e0366a077
joaogoetzedasilva <167103392+joaogoetzedasilva@users.noreply.github.com>
2024-09-09 11:18:46 -0300

    Fazendo comit do codigo modificado da alura

---
+++ joaogoetze/imersao1/dados.js
@@ -0,0 +1,44 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "Skate Fada Fadinha Sk8 Rayssa skate fada fadinha sk8 rayssa"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "Judô Judo Judoca Judoka Beatriz judô judo judoca judoka beatriz"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "Ginástica Ginastica Ouro Rebeca  ginástica ginastica ouro rebeca "
+    },
+    {
+        "titulo": "Isaquias Queiroz",
+        "descricao": "Isaquias Queiroz é um canoísta brasileiro que já conquistou várias medalhas olímpicas. Ele é conhecido por sua determinação e habilidade nas competições de canoagem.",
+        "link": "https://pt.wikipedia.org/wiki/Isaquias_Queiroz",
+        "tags": "Canoagem Canoe Isaquias Queiroz canoagem canoe isaquias queiroz"
+    },
+    {
+        "titulo": "Ana Marcela Cunha",
+        "descricao": "Ana Marcela Cunha é uma nadadora brasileira especializada em maratonas aquáticas. Ela é uma das principais atletas do mundo em sua modalidade.",
+        "link": "https://pt.wikipedia.org/wiki/Ana_Marcela_Cunha",
+        "tags": "Natação Natacao Maratona Ana Marcela Cunha natação natacao maratona ana marcela cunha"
+    },
+    {
+        "titulo": "Alison dos Santos",
+        "descricao": "Alison dos Santos, também conhecido como 'Piu', é um atleta brasileiro especializado nos 400 metros com barreiras. Ele é um dos principais nomes do atletismo brasileiro.",
+        "link": "https://pt.wikipedia.org/wiki/Alison_dos_Santos",
+        "tags": "Atletismo Atletismo 400m Barreiras Alison dos Santos atletismo atletismo 400m barreiras alison dos santos"
+    },
+    {
+        "titulo": "Gabriel Medina",
+        "descricao": "Gabriel Medina é um surfista brasileiro que já conquistou vários títulos mundiais. Ele é um dos maiores nomes do surfe mundial.",
+        "link": "https://pt.wikipedia.org/wiki/Gabriel_Medina",
+        "tags": "Surfe Surf Gabriel Medina surfe surf gabriel medina"
+    }
+];
