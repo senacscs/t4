83d3158069585a66ff4bf54978245f5d5d3e187a
pedrofa07 <167103014+pedrofa07@users.noreply.github.com>
2024-09-09 11:20:11 -0300

    alurapersonalizacao

---
+++ pedrofa/personalizaçãocódigoalura/dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8",
+        imagem: "https://th.bing.com/th/id/OIP.jJV-jcEKO6aoq0ZRChTq9QAAAA?w=245&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" // Adicione o URL da imagem aqui
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka",
+        imagem: "" // Adicione o URL da imagem aqui
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro",
+        imagem: "" // Adicione o URL da imagem aqui
+    }
+];
