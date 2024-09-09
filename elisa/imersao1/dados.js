69a50f2c657900e9f80e928cd5855c3f8ef2a683
Ana Sprenger <167103067+anasprenger@users.noreply.github.com>
2024-09-09 10:56:54 -0300

    imersaoalura da barbie

---
+++ elisa/imersao1/dados.js
@@ -0,0 +1,114 @@
+let dados = [
+    {
+        titulo: "Barbie: A Estrela do Rock (1987)",
+        descricao: "Barbie sonha em ser uma grande estrela do rock. Com a ajuda de suas amigas, ela forma uma banda e embarca em uma emocionante jornada para realizar seu sonho.",
+        link: "https://youtu.be/3oUMf4Y-H54?si=z8nuyNkkq9tS30hs",
+        imagem: "rockstar.webp",
+        tags: "antigos estrela rock banda musica" 
+    },
+    {
+        titulo: "Barbie em Quebra-Nozes (2001)",
+        descricao: "Em uma noite de Natal mágica, Barbie é transportada para um mundo encantado e se torna a Clara, uma jovem que precisa quebrar um feitiço para salvar o reino.",
+        link: "https://globoplay.globo.com/barbie-e-o-quebra-nozes/t/dL7SQhZtV9/",
+        imagem: "quebranozes.webp",
+        tags: "antigos quebra-nozes natal magia"
+    },
+    {
+        titulo: "Barbie como Rapunzel (2002)",
+        descricao: "Barbie vive presa em uma torre, mas sua paixão pela arte e a esperança de um dia ser livre a impulsionam a buscar a felicidade.",
+        link: "https://globoplay.globo.com/barbie-como-rapunzel/t/FrxMcyddPR/",
+        imagem: "rapunzel.jpg",
+        tags: "antigos barbie rapunzel"
+    },
+    {
+        titulo: "Barbie Lago dos Cisnes (2003)",
+        descricao: "Barbie faz o papel de Odette, a jovem filha de um padeiro, que segue um unicórnio mágico que a levará até a incrível Floresta Encantada. Ali, ela será transformada num cisne pelo mago Rothbart, até ser salva pelo galante príncipe Daniel (vivido pelo inseparável Ken).",
+        link: "https://www.primevideo.com/-/pt/detail/Barbie-em-Lago-dos-Cisnes/0JFG4EVQOI50OLN1LSD3KT7OAB",
+        imagem: "lagodoscisneis.webp",
+        tags: "antigos cisnes cisneis lago "
+    },
+    {
+        titulo: "Barbie em a Princesa e a Plebeia (2004)",
+        descricao: "A princesa Anneliese constrói uma amizade com a humilde Erika, com quem se parece muito. Quando ela é raptada, Erika arma um plano para enganar seu raptor e conseguir salvá-la.",
+        link: "https://globoplay.globo.com/barbie-em-a-princesa-e-a-plebeia/t/wWWTxwFG1F/",
+        imagem: "princesaplebeia.webp",
+        tags: "antigos princesa princesas plebeia"
+    },
+    {
+        titulo: "Barbie Fairytopia (2005)",
+        descricao: "Estrelando Barbie como Elina! Logo após o arco-íris, em um mundo chamado Fairytopia, vive Elina, uma bela fada das flores, que sonha em ter asas. Ela vive em uma flor enorme com seu amigo Bibble, um bichinho engraçado e adorável. Um dia Elina descobre que sua casa de flor está doente, e que seus amigos náo podem mais voar!",
+        link: "https://www.primevideo.com/-/pt/detail/Barbie-Fairytopia/0PHPAEDUMMGFUWB3URKX84NNGS",
+        imagem: "farytopia.webp",
+        tags: "antigos fairytopia fadas magia arco-iris princesas sereias"
+    },
+    {
+        titulo: "Barbie em a Magia de Aladus (2005)",
+        descricao: "Barbie é a Princesa Annika, que junto com seu belo cavalo alado viverá uma grande aventura! O Reino das Nuvens está tomado por um feiticeiro malvado que rapta a Princesa Annika e a sua família. Porém, ela consegue escapar e contará com a ajuda do cavalo alado Pegasus para derrotar o bruxo e devolver a liberdade a todos.",
+        link: "https://www.primevideo.com/-/pt_PT/detail/Barbie-e-a-Magia-de-Aladus/0N5IZIOVDYJ6QWD57VP2XMKZZ3",
+        imagem: "magiadealadus.webp",
+        tags: "antigos aladus magia princesa princesas pegasus"
+    },
+    {
+        titulo: "Barbie: A Princesa da Ilha (2007)",
+        descricao: "A princesa Rosella, ainda criança, sofre um naufrágio e vai parar em uma ilha. Lá ela aprende a cantar e falar com uma adorável família de animais, que inclui Sagi, um panda vermelho, Azul, o pavão, e Tika, o bebê elefante. Inesperadamente, Rosella conhece o príncipe Antonio que acabou de descobrir a ilha paradisíaca.",
+        link: "https://www.primevideo.com/-/pt_PT/detail/Barbie-em-A-Princesa-da-Ilha/0TMSO6MXX5O4PLHYVKDN3H4SWH",
+        imagem: "princesailha.png",
+        tags: "antigos princesas princesa ilha magia animais"
+    },
+    {
+        titulo: "Barbie em Vida de Sereia (2010)",
+        descricao: "Nesta nova aventura, Barbie é Merliah, uma campeã de surf de Malibu. Em um momento ela é uma adolescente e no outro aprende um chocante segredo de família: ela é uma sereia!",
+        link: "https://www.primevideo.com/-/pt/detail/Barbie-em-Vida-de-Sereia/0JZ1TIPY44GLLSJ58M9U4NOQAY",
+        imagem: "vidadesereia.webp",
+        tags: "antigos sereias vida sereia"
+    },
+    {
+        titulo: "Barbie e os Golfinhos Mágicos (2017)",
+        descricao: "Se junte à Barbie e suas irmãs Skipper, Stacie e Chelsea em uma aventura numa ilha tropical, onde Ken está fazendo um estágio de biologia marinha. Enquanto fazem mergulho, encontram um jovem golfinho preso em um recinto fechado — mas não é um golfinho qualquer, é um lindo Golfinho de Pedras Preciosas verde brilhante!",
+        link: "https://www.primevideo.com/-/pt/detail/Barbie-e-os-Golfinhos-M%C3%A1gicos/0HG4IJO1M9FXU7KW0X539ZIG8P",
+        imagem: "magiadosgolfinhos.webp",
+        tags: "magia golfilhos irmãs"
+    },
+    {
+        titulo: "Barbie em Um Mundo de Vídeo Game (2017)",
+        descricao: "Prepare-se para entrar em ação! Quando Barbie, num passe de mágica, entra em seu videogame favorito, ela fica muito empolgada ao ver que se transformou em uma divertida personagem patinadora.",
+        link: "https://www.primevideo.com/-/pt/detail/Barbie-No-Mundo-Dos-Jogos/0TCLML8SUGPWJYK5C9QE4FNT69",
+        imagem: "videogame.webp",
+        tags: "mundo video games jogos 'video games' tecnologia"
+    },
+    {
+        titulo: "Barbie e suas Irmãs em Busca dos Cachorrinhos (2018)",
+        descricao: "Barbie e suas irmãs descobrem que tudo é mais divertido em família nessa incrível viagem tropical! Depois que as meninas e os seus cachorrinhos chegam a uma ilha paradisíaca para a grande competição de dança da Chelsea™, elas decidem fazer um rápido passeio para conhecer o Festival dos Cavalos Dançarinos.",
+        link: "https://www.primevideo.com/-/pt/detail/Barbie-e-suas-irm%C3%A3s-em-Busca-dos-cachorrinhos/0P0NLRWXF66L7NYIQYQSEWMBKN",
+        imagem: "cachorrinhos.jpg",
+        tags: "irmãs irmas cachorrinhos cachorros busca procura"
+    },
+    {
+        titulo: "Barbie: Aventura nas Estrelas (2018)",
+        descricao: "Mesmo com receio de deixar seu lar, Barbie™ viaja para o Planeta Capital e se une a uma equipe incrível em uma missão para salvar as estrelas. Lá, ela encontra a campeã galáctica de hoverboard, Sal-lee, e um grupo de amigos cheios de talento. Barbie™ logo descobre que escutando seu coração e com a ajuda de seus amigos, ela pode se tornar a líder que a galáxia inteira está esperando.",
+        link: "https://www.primevideo.com/-/pt/detail/Barbie-Aventura-Nas-Estrelas/0PYA2JD2FN5V1BOFHC756JOUJT",
+        imagem: "estrelas.webp",
+        tags: "aventura estrelas espaço magia tecnologia"
+    },
+    {
+        titulo: "Barbie e as Agentes Secretas (2019)",
+        descricao: "Barbie e suas amigas ginastas treinam duro para serem as melhores. Até que um golpe do destino gira o mundo delas de cabeça para baixo!",
+        link: "https://www.max.com/br/pt/movies/barbie-e-as-agentes-secretas/7731af29-30b6-449a-bfca-8a97edd90330?utm_source=universal_search",
+        imagem: "agentesscretas.jpg",
+        tags: "agentes 'agentes secretas' tecnologia secretas"
+    },
+    {
+        titulo: "Barbie e suas Irmãs em Uma Aventura de Cachorrinhos (2019)",
+        descricao: "Barbie™ e suas irmãs Skipper, Stacie e Chelsea, com seus adoráveis novos cachorrinhos, envolvem-se em uma inesperada aventura misteriosa, quando retornam para Willows, a cidade onde nasceram.",
+        link: "https://www.primevideo.com/dp/amzn1.dv.gti.f16bc780-d51a-4def-8258-cb4fd7a11c7c?autoplay=0&ref_=atv_cf_strg_wb",
+        imagem: "acachorrinhos.webp",
+        tags: "irmas irmãs cachorrinhos aventura"
+    },
+    {
+        titulo: "Barbie: Rainhas do Rock (2020)",
+        descricao: "Prepare-se para o rock! O mundo da princesa Courtney vira de cabeça para baixo quando ela troca de lugar com Erika, uma famosa estrela do rock.",
+        link: "https://globoplay.globo.com/barbie-rainhas-do-rock/t/xCq8kZ8CLJ/",
+        imagem: "rainhasdorock.jpg",
+        tags: "rock princesas musicas músicas rainhas"
+    },
+];
