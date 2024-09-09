efce4ae19abf037f7e3d08f595a71e1e6a4298f3
Thomas Brandt <167104596+polleco@users.noreply.github.com>
2024-09-09 11:28:01 -0300

    subindo o site da imersão1

---
+++ brandt/imersao1/dados.js
@@ -0,0 +1,56 @@
+let dados = [
+    {
+        titulo: "Bulbasaur",
+        descricao: "Bulbasaur é um Pokémon pequeno e verde, com uma cebola na sua cabeça. Ele é um Pokémon dócil e amigável, conhecido por sua natureza gentil. A cebola na sua cabeça é um órgão fotossintético que absorve energia solar, o que o ajuda a crescer.",
+        link: "https://www.pokemon.com/us/pokedex/bulbasaur",
+        tags: "bulbasaur 001 bulbassauro bulbasauro 1"
+    },
+    {
+        titulo: "Ivysaur",
+        descricao: "Ivysaur é uma evolução do Bulbasaur, com uma flor maior na sua cabeça. Ele é um Pokémon mais poderoso do que o Bulbasaur, com uma capacidade de ataque aprimorada. A flor na sua cabeça é um órgão fotossintético que absorve energia solar, o que o ajuda a crescer ainda mais.",
+        link: "https://www.pokemon.com/us/pokedex/ivysaur",
+        tags: "ivysaur 002 ivysauro ivyssauro 2"
+    },
+    {
+        titulo: "Venusaur",
+        descricao: "Venusaur é a evolução final do Bulbasaur, com uma flor gigante na sua cabeça. Ele é um Pokémon poderoso e temido, conhecido por sua força e capacidade de ataque. A flor na sua cabeça é um órgão fotossintético que absorve energia solar, o que o ajuda a crescer até o tamanho de uma árvore.",
+        link: "https://www.pokemon.com/br/pokedex/Venusaur",
+        tags: "Venusaur 003 venussauro venusauro 3"
+    },
+    {
+        titulo: "Charmander",
+        descricao: "Charmander é um Pokémon pequeno e laranja, com uma chama na ponta da cauda. Ele é um Pokémon ativo e energético, conhecido por sua natureza aventureira. A chama na sua cauda é um indicador de sua saúde e energia.",
+        link: "https://www.pokemon.com/us/pokedex/charmander",
+        tags: "charmander 004 lagartixa 4"
+    },
+    {
+        titulo: "Charmeleon",
+        descricao: "Charmeleon é uma evolução do Charmander, com uma chama maior na ponta da cauda. Ele é um Pokémon mais poderoso do que o Charmander, com uma capacidade de ataque aprimorada. A chama na sua cauda é um indicador de sua saúde e energia, e é mais intensa do que a do Charmander.",
+        link: "https://www.pokemon.com/br/pokedex/charmeleon",
+        tags: "charmeleon 005 charmilion salamandra 5"
+    },
+    {
+        titulo: "Charizard",
+        descricao: "Charizard é a evolução final do Charmander, com asas e uma chama gigante na ponta da cauda. Ele é um Pokémon poderoso e temido, conhecido por sua força e capacidade de ataque. A chama na sua cauda é um indicador de sua saúde e energia, e é muito mais intensa do que a do Charmeleon.",
+        link: "https://www.pokemon.com/us/pokedex/charizard",
+        tags: "charizard 006 dragão dragao 6"
+    },
+    {
+        titulo: "Squirtle",
+        descricao: "Squirtle é um Pokémon pequeno e azul, com uma concha na sua costas. Ele é um Pokémon dócil e amigável, conhecido por sua natureza brincalhona. A concha na sua costas serve como proteção e também como um escudo para ataques.",
+        link: "https://www.pokemon.com/us/pokedex/squirtle",
+        tags: "squirtle 007 7"
+    },
+    {
+        titulo: "Wartortle",
+        descricao: "Wartortle é uma evolução do Squirtle, com uma concha maior na sua costas. Ele é um Pokémon mais poderoso do que o Squirtle, com uma capacidade de ataque aprimorada. A concha na sua costas serve como proteção e também como um escudo para ataques, e é maior do que a do Squirtle.",
+        link: "https://www.pokemon.com/us/pokedex/wartortle",
+        tags: "wartortle 008 8"
+    },
+    {
+        titulo: "Blastoise",
+        descricao: "Blastoise é a evolução final do Squirtle, com canhões de água nas suas costas. Ele é um Pokémon poderoso e temido, conhecido por sua força e capacidade de ataque. Os canhões de água nas suas costas são capazes de disparar jatos de água com força incrível.",
+        link: "https://www.pokemon.com/us/pokedex/blastoise",
+        tags: "blastoise 009 9"
+    }
+];
