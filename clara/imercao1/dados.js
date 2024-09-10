6ab774e02bef165c78f485fede3e3354dba73289
claradick <167199495+claradick@users.noreply.github.com>
2024-09-10 08:27:16 -0300

    subindo imerção

---
+++ clara/imercao1/dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        titulo: "Magic Kingdom Park",
+        descricao: "O Magic Kingdom Park é um Parque Temático do Walt Disney World Resort com atrações clássicas, fogos de artifício encantadores, desfiles musicais e Personagens Disney.",
+        link: "https://disneyworld.disney.go.com/pt-br/destinations/magic-kingdom/",
+        tags: "Parque do mikey",
+    },
+    {
+        titulo: "Epcot",
+        descricao: "O nome Epcot é, na verdade, uma abreviação de Experimental Prototype of Community of Tomorrow (“Protótipo Experimental da Comunidade do Amanhã”). Foi inspirado pelo sonho de Walt Disney de construir uma cidade do futuro.",
+        link: "https://disneyworld.disney.go.com/pt-br/destinations/epcot/",
+        tags: "Parque dos paises.",
+    },
+    {
+        titulo: "Hollywood Studios",
+        descricao: "O Hollywood Studios é o menor parque do complexo Disney, o que não apaga o seu brilho. Está dividido em 8 áreas temáticas, repletas de grandes histórias e aventuras: Hollywood Boulevard, Echo Lake, Commissary Lane, Grand Avenue, Star Wars: Galaxy’s Edge, Toy Story Land, Animation Courtyard e Sunset Boulevard.",
+        link: "https://disneyworld.disney.go.com/pt-br/destinations/hollywood-studios/",
+        tags: "Hollywood.",
+    },
+
+    {
+        titulo: "Animal kingdom",
+        descricao: "Animal Kingdom, o parque que possui uma dose de aventura, a magia da natureza dia e noite, shows com nossos personagens favoritos e a mais nova e fantástica área de Pandora do filme Avatar. Fique por dentro de todas as dicas para aproveitar como nunca esse parque da Disney em Orlando.",
+        link: "https://disneyworld.disney.go.com/pt-br/destinations/animal-kingdom/",
+        tags: "parque dos animais.",
+    }
+];
