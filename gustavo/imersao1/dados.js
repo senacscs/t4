c51692bd6249bb673f50b0770e13b69a30a36218
GustavoFlorisbalTatsch <167103391+GustavoFlorisbalTatsch@users.noreply.github.com>
2024-09-10 08:30:35 -0300

    imersao1

--- gustavo/imersao1/dados.js
+++ gustavo/imersao1/dados.js
@@ -7,65 +7,86 @@ const atletas = [
     {
         nome: 'Rebeca Andrade',
         esporte: 'Ginástica Artística',
-        titulos: ['Medalha de Ouro Olímpica 2020', 'Medalha de Prata Olímpica 2020']
+        titulos: ['Medalha de Prata Olímpica 2020', 'Medalha de ouro Paris 2024']
     },
     {
-        nome: 'Neymar Jr.',
+        nome: 'Neymar Jr',
         esporte: 'Futebol',
         titulos: ['Ouro Olímpico 2016', 'Copa das Confederações 2013']
     },
-    
     {
-        nome: 'Vanderlei Cordeiro de Lima.',
+        nome: 'Vanderlei Cordeiro de Lima',
         esporte: 'Atletismo',
         titulos: ['Medalha de bronze', 'Medalha de bronze na maratona 2004']
     },
-
     {
-        nome: 'Maurren Maggi.',
+        nome: 'Maurren Maggi',
         esporte: 'Atletismo',
         titulos: ['Medalha de ouro', 'Medalha de ouro no salto em distância 2008']
     },
-
     {
-        nome: 'Isaquias Queiroz.',
+        nome: 'Isaquias Queiroz',
         esporte: 'Atletismo',
         titulos: ['Medalha de ouro', 'Medalha de ouro na canoagem 2016']
     },
-
     {
-        nome: 'César Cielo.',
+        nome: 'César Cielo',
         esporte: 'Natação',
         titulos: ['Medalha de ouro', 'Medalha de ouro  nos 50m e 100m livre Pequim 2008']
     },
-
     {
-        nome: 'Thiago Pereira.',
+        nome: 'Thiago Pereira',
         esporte: 'Natação',
         titulos: ['Medalha de prata ', 'Medalha de prata nos 400m medley Londres 2012']
     },
-
     {
-        nome: 'Rafael Silva.',
+        nome: 'Rafael Silva',
         esporte: 'Judô',
-        titulos: [' Medalha de bronze ', 'Medalha de bronze na categoria pesado Londres 2012']
+        titulos: ['Medalha de bronze ', 'Medalha de bronze na categoria pesado Londres 2012']
     },
-
     {
-        nome: 'Sarah Menezes.',
+        nome: 'Sarah Menezes',
         esporte: 'Judô',
-        titulos: [' Medalha de ouro ', 'Medalha de ouro na categoria 48 kg Londres 2012']
+        titulos: ['Medalha de ouro ', 'Medalha de ouro na categoria 48 kg Londres 2012']
     },
-
     {
-        nome: 'Giba (Gilberto Amaury de Godoy Filho).',
+        nome: 'Giba (Gilberto Amaury de Godoy Filho)',
         esporte: 'Vôlei',
-        titulos: [' Medalhas de ouro ', 'Medalhas de ouro em Atenas 2004 e Pequim 2008']
+        titulos: ['Medalhas de ouro ', 'Medalhas de ouro em Atenas 2004 e Pequim 2008']
     },
-
     {
-        nome: 'Sheilla Castro.',
+        nome: 'Sheilla Castro',
         esporte: 'Vôlei',
-        titulos: [' Medalhas de ouro ', 'Medalhas de ouro em Pequim 2008 e Londres 2012']
+        titulos: ['Medalhas de ouro ', 'Medalhas de ouro em Pequim 2008 e Londres 2012']
+    },
+    {
+        nome: 'Éverton Lopes',
+        esporte: ' Boxe',
+        titulos: ['Medalha de ouro ', 'Medalha de ouro no boxe leve Londres 2012']
+    },
+    {
+        nome: 'Robson Conceição',
+        esporte: ' Boxe',
+        titulos: ['Medalha de ouro ', 'Medalha de ouro no boxe leve Rio 2016']
+    },
+    {
+        nome: 'Marcelo Melo',
+        esporte: ' Tênis',
+        titulos: ['Medalha de ouro ', 'Medalha de ouro  nas duplas masculinas Rio 2016']
+    },
+    {
+        nome: 'Laura Pigossi',
+        esporte: ' Tênis',
+        titulos: ['Medalha de bronze ', 'Medalha de bronze nas duplas femininas Tóquio 2020']
+    },
+    {
+        nome: 'Jacqueline Silva e Sandra Pires',
+        esporte: ' Vôlei',
+        titulos: ['Medalha de ouro ', 'Medalha de ouro em Sydney 2000']
+    },
+    {
+        nome: 'Joanna Maranhão',
+        esporte: ' Vôlei',
+        titulos: ['Medalha de ouro ', 'Medalha de ouro em Sydney 2000']
     },
 ];
