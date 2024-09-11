64189b5180a16cbeda00a660d64616bbcf51208d
gjohannk <167103316+gjohannk@users.noreply.github.com>
2024-09-11 08:22:29 -0300

    mais gente pra pesquisar

---
+++ kipper/imersao1/imersao1/dados.js
@@ -0,0 +1,39 @@
+let dados = [
+    {
+        titulo: "Matuê",
+        descricao: "Matuê - O Rei da Sofrência Trap. Matheus Brasileiro Aguiar (Fortaleza, 11 de outubro de 1993), mais conhecido pelo nome artístico Matuê, é um rapper, cantor, compositor e guitarrista brasileiro. Ficou conhecido com o single Anos Luz, lançado em 2017 e pelo álbum Máquina do Tempo lançado em 2020. É considerado um dos símbolos do trap brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Matuê",
+        tags: "trap rap matue 30praum"
+    },
+    {
+        titulo: "Teto",
+        descricao: "Teto - O Fenômeno da Internet. Clériton Sávio Santos Silva (Jacobina, 19 de outubro de 2001) mais conhecido pelo seu nome artístico Teto é um rapper, trapper, cantor e compositor brasileiro que ficou conhecido por meio das prévias de suas músicas que foram lançadas não oficialmente em vários canais do YouTube, alcançando milhões de visualizações e ganhando fama em redes sociais como o TikTok e Instagram.",
+        link: "https://pt.wikipedia.org/wiki/Teto_(rapper)",
+        tags: "trap rap teto 30praum"
+    },
+    {
+        titulo: "Wiu",
+        descricao: "WIU - A Voz da Rua. Vinicius William Sales de Lima, (Fortaleza, 16 de fevereiro de 2002) mais conhecido pelo nome artístico WIU, é um rapper, cantor, compositor e produtor musical brasileiro. Ficou conhecido com os singles Felina, Horas Iguais, Coração de Gelo e pelo álbum Manual de Como Amar Errado, lançado em 2022.",
+        link: "https://pt.wikipedia.org/wiki/WIU",
+        tags: "trap rap wiu 30praum"
+    },
+    {
+        titulo: "Veigh",
+        descricao: "Thiago Veigh da Silva (Itapevi, 12 de setembro de 2000), mais conhecido por seu nome artístico Veigh, é um rapper, cantor e compositor brasileiro. em 2023 lançou seu álbum Prédios Deluxe alcançou recorde na estreia ficando em top global de streaming.",
+        link: "https://pt.wikipedia.org/wiki/Veigh",
+        tags: "trap hip-hop Veigh supernova super nova dos predios"
+    },
+    {
+        titulo: "TZ da coronel",
+        descricao:"Matheus de Araújo Santos (Cabo Frio, Rio de Janeiro, 6 de julho de 2001) Também conhecido como TZ da Coronel, é um dos nomes em ascensão no trap nacional. Oriundo do Rio de Janeiro, ele se destaca pelo seu estilo cru e autêntico, com letras que retratam a realidade da periferia e suas vivências pessoais. Sua música tem forte apelo nas plataformas digitais, conquistando uma legião de fãs.",
+        link: "https://pt.wikipedia.org/wiki/TZ_da_Coronel",
+        tags: "trap rap TZ TZ da coronel"
+    },
+    {
+        titulo: "Yunk Vino",
+        descricao: "Marcos Vinícius Albano(Carapicuíba, São Paulo, 7 de junho de 1997) Yunk Vino é um dos representantes da nova geração do trap brasileiro, conhecido por seu estilo irreverente e suas músicas que combinam beats modernos com letras que abordam festas, estilo de vida e conquistas. Sua personalidade vibrante e flow único o destacam na cena musical urbana do Brasil.",
+        link: "https://www.last.fm/pt/music/Yunk+Vino/+wiki",
+        tags: "rap trap yunk vino"
+    }
+    
+];
