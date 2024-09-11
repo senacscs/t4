426e353b9b6ba7e6d2c10decd7d4bc304f5241c3
BarrosMatheus2008 <167106263+BarrosMatheus2008@users.noreply.github.com>
2024-09-11 09:17:26 -0300

    imersao

--- teteu/imersao1/app.js
+++ teteu/imersao1/app.js
@@ -1,23 +1,23 @@
 function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
-
+ 
     let campoPesquisa = document.getElementById("campo-pesquisa").value
-
+ 
     // se campoPesquisa for uma string sem nada
     if (!campoPesquisa) {
         section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
-        return 
+        return
     }
-
+ 
     campoPesquisa = campoPesquisa.toLowerCase()
-
+ 
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
-    let titulo = ""; 
+    let titulo = "";
     let descricao = "";
     let tags = "";
-
+ 
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
         titulo = dado.titulo.toLowerCase()
@@ -37,11 +37,11 @@ function pesquisar() {
         `;
         }
     }
-
+ 
     if (!resultados) {
         resultados = "<p>Nada foi encontrado</p>"
     }
-
+ 
     // Atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
 }
---
+++ teteu/imersao1/dados.js
@@ -0,0 +1,44 @@
+let dados = [
+    {
+        titulo: "Lebron james",
+        descricao: " LeBron James é um dos maiores jogadores de basquete da história. Nascido em 30 de dezembro de 1984 em Akron, Ohio, ele ganhou fama na escola secundária e foi selecionado como a primeira escolha no Draft da NBA de 2003 pelo Cleveland Cavaliers. Ao longo de sua carreira, jogou também pelo Miami Heat e pelo Los Angeles Lakers, conquistando vários campeonatos da NBA e prêmios de MVP. Além de seu sucesso em quadra, LeBron é conhecido por seu envolvimento em causas sociais e filantrópicas, incluindo a fundação de escolas e iniciativas para jovens.",
+        link: "https://pt.wikipedia.org/wiki/LeBron_James",
+        tags: "Lebron james king",
+    },
+    {
+        titulo: "Sthepen Curry",
+        descricao: "Stephen Curry é um renomado jogador de basquete da NBA, conhecido por sua habilidade excepcional em arremessos de longa distância. Nascido em 14 de março de 1988 em Akron, Ohio, ele foi selecionado pelo Golden State Warriors no Draft de 2009. Curry é amplamente considerado um dos melhores arremessadores de todos os tempos, tendo revolucionado o jogo com seu estilo de arremesso de três pontos. Ele levou os Warriors a vários campeonatos da NBA e ganhou vários prêmios de MVP. Além disso, Curry é conhecido por seu trabalho filantrópico e seu impacto positivo fora das quadras.Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Stephen_Curry",
+        tags: "Sthepen curry chef",
+    },
+    {
+        titulo: "Michael Jordan",
+        descricao: "Michael Jordan é amplamente considerado o maior jogador de basquete de todos os tempos. Nascido em 17 de fevereiro de 1963 em Brooklyn, Nova York, ele ganhou seis campeonatos da NBA com o Chicago Bulls na década de 1990 e foi nomeado MVP da liga cinco vezes. Jordan é famoso por sua habilidade de pontuar, sua ética de trabalho e seu impacto em jogos decisivos. Após se aposentar do basquete, ele também se tornou um bem-sucedido proprietário da equipe Charlotte Hornets e um ícone global, tanto no esporte quanto fora dele.",
+        link: "https://pt.wikipedia.org/wiki/Michael_Jordan",
+        tags: "Goat Michael jordan",
+    },
+    {
+        titulo: "hakeem olajuwon",
+        descricao: "Hakeem Olajuwon é um ex-jogador de basquete nigeriano, amplamente reconhecido como um dos melhores pivôs da história da NBA. Nascido em 21 de janeiro de 1963 em Lagos, Nigéria, ele jogou a maior parte de sua carreira pelo Houston Rockets, onde conquistou dois campeonatos da NBA (1994 e 1995) e foi eleito MVP das Finais em ambas as ocasiões. Olajuwon era conhecido por sua habilidade defensiva, seu jogo ofensivo sofisticado, especialmente seu famoso e sua presença dominante no garrafão. Ele foi introduzido no Hall da Fama do Basquete em 2008.",
+        link: "https://pt.wikipedia.org/wiki/Hakeem_Olajuwon",
+        tags: "hakeem olajuwon",
+    },
+    {
+        titulo: "Wilt Chamberlain",
+        descricao: "Wilt Chamberlain, nascido em 21 de agosto de 1936 em Philadelphia, Pennsylvania, é um dos jogadores mais icônicos da NBA. Ele é conhecido por suas estatísticas impressionantes, incluindo a famosa marca de 100 pontos em um único jogo. Chamberlain conquistou dois campeonatos da NBA e foi nomeado MVP da liga quatro vezes. Sua dominância física e habilidades ofensivas o tornaram uma lenda do basquete.",
+        link: "https://pt.wikipedia.org/wiki/Wilt_Chamberlain",
+        tags: "wilt chamberlain"
+    },
+    {
+        titulo: "Shaquille O'Neal",
+        descricao: "Shaquille O'Neal, nascido em 6 de março de 1972 em Newark, New Jersey, é um dos pivôs mais dominantes da história da NBA. Ele conquistou quatro campeonatos da NBA, jogando por equipes como o Los Angeles Lakers e o Miami Heat. O'Neal é conhecido por sua presença física imponente, habilidades ofensivas e defensivas, e seu carisma. Ele foi nomeado MVP das Finais três vezes e é membro do Hall da Fama do Basquete.",
+        link: "https://pt.wikipedia.org/wiki/Shaquille_O%27Neal",
+        tags: "shaquille o'neal"
+    },   
+    {
+        titulo: "Larry Bird",
+        descricao: "Larry Bird, nascido em 7 de dezembro de 1956 em West Baden Springs, Indiana, é uma lenda do basquete e um dos maiores jogadores da NBA. Ele jogou pelo Boston Celtics e conquistou três campeonatos da NBA na década de 1980. Bird era conhecido por sua habilidade de arremesso, sua visão de jogo e sua inteligência em quadra. Ele foi nomeado MVP da liga três vezes e é membro do Hall da Fama do Basquete.",
+        link: "https://pt.wikipedia.org/wiki/Larry_Bird",
+        tags: "larry bird"
+    }, 
+];
