c4adcce3aa7c28af8f31a665f4ef42527d013021
theocarpes <70979960+theocarpes@users.noreply.github.com>
2024-09-11 09:19:03 -0300

    atualizando imersao alura

--- theo/imersao1/app.js
+++ theo/imersao1/app.js
@@ -33,7 +33,6 @@ function pesquisar() {
                     <a href="#" target="_blank">${dado.titulo}</a>
                 </h2>
                 <p class="descricao-meta">${dado.descricao}</p>
-                <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
         `;
         }
--- theo/imersao1/dados.js
+++ theo/imersao1/dados.js
@@ -2,20 +2,52 @@
 let dados = [
     {
         titulo: "Luiz Rauber",
-        descricao: "Luiz Rauber é o professor de técnico no Senac Santa Cruz.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        descricao: "Luiz Rauber é o professor de técnico no Senac Santa Cruz. Depende.",
         tags: "tecnico técnico"
     },
     {
         titulo: "Carlos",
-        descricao: "Carlos é o professor de sociologia e PV. Geralmente é bem deboa mas qunado atrapalham a sua aula fica bem bravo.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        descricao: "Carlos é o professor de sociologia e PV. Geralmente é bem deboa mas quando atrapalham a sua aula fica bem bravo.",
         tags: "sociologia pv projeto de vida"
     },
     {
         titulo: "João",
         descricao: "João é o professor de Educação Fisica e PI. Nas aulas de Ed. Fisica sempre diz que quanto mais conversamos menos tempo de jogar vamos ter.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
         tags: "educação fisica PI ed fisica"
-    }
+    },
+    {
+        titulo: "Eduardo",
+        descricao: "Eduardo é o professor de Inglês. Bem tranquilo mas fica puto se atrapalharem a aula.",
+        tags: "ingles inglês",
+    },
+    {
+        titulo: "Livia",
+        descricao: "Livia é a professora de matematica e tambem era de física.",
+        tags: "mat matematica",
+    },
+    {
+        titulo: "Bruna",
+        descricao: "Bruna é a professora de história, geografia e filosofia.",
+        tags: " filo filosofia historia geo geografia",
+    },
+    {
+        titulo: "Poliana",
+        descricao: "Poliana é a professora de biologia. Começou a nos dar aula a pouco tempo mas parece gostar da nossa turma.",
+        tags: "bio biologia",
+    },
+    {
+        titulo: "Carla",
+        descricao: "Carla é a professora de português. Odeia nossa turma.",
+        tags: "port portugues português",
+    },
+    {
+        titulo: "Natalia",
+        descricao: "",
+        tags: "Física ",
+    },
+    {
+        titulo: "Alana",
+        descricao: "",
+        tags: "Artes",
+    },
 ];
