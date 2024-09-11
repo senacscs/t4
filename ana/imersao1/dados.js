7af7688ab62c73d38b32e19772c8922d012667f1
AnaliceWe <167103681+AnaliceWe@users.noreply.github.com>
2024-09-11 09:10:05 -0300

    subindo imersão1

---
+++ ana/imersao1/dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+        titulo: "Misha Collins / Castiel",
+        descricao: "Um anjo que se junta aos irmãos Winchester em sua luta contra o mal, formando uma forte amizade com eles.",
+        link: "https://www.themoviedb.org/tv/1622-supernatural/cast?language=pt-BR",
+        tags: "castiel"
+    },
+    {
+        titulo: "Jared Padalecki / Sam Winchester",
+        descricao: "Um dos protagonistas, Sam é o irmão mais novo da família Winchester, caçador de monstros e forças sobrenaturais.",
+        link: "https://www.themoviedb.org/tv/1622-supernatural/cast?language=pt-BR",
+        tags: "sam"
+    },
+    {
+        titulo: "Jensen Ackles / Dean Winchester",
+        descricao: "Irmão mais velho de Sam, Dean também é caçador e faz de tudo para proteger sua família e o mundo das criaturas sobrenaturais.",
+        link: "https://www.themoviedb.org/tv/1622-supernatural/cast?language=pt-BR",
+        tags: "dean"
+    },
+    {
+        titulo: "Mark Sheppard / Crowley",
+        descricao: "O rei do inferno, que tem uma relação complexa com os irmãos Winchester, oscilando entre inimigo e aliado.",
+        link: "https://www.themoviedb.org/tv/1622-supernatural/cast?language=pt-BR",
+        tags: "crowley"
+    }
+    
+];
