6a5476fb72eb960f05288bdc01f4bddd89b99b5d
enzo <167103155+EnzoBossardi208@users.noreply.github.com>
2024-09-11 07:59:22 -0300

    imersao1

---
+++ enzo/imersao1/dados.js
@@ -0,0 +1,26 @@
+let dados = [
+    {
+        titulo: "BloodBorne",
+        descricao: "Bloodborne se passa na cidade gótica de Yharnam, onde uma antiga e poderosa doença transformou seus habitantes em criaturas bestiais. O jogador assume o papel de um caçador que chega à cidade em busca de um remédio milagroso, mas logo se vê preso em um ciclo de pesadelos e horrores. O mistério da doença está ligado a uma civilização antiga e à busca pelo sangue sagrado, uma substância que promete poder e cura, mas também atrai deuses alienígenas e entidades cósmicas. À medida que o caçador explora Yharnam e seus segredos, descobre verdades perturbadoras sobre a natureza da humanidade, a loucura e o desejo de transcendência.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "Souls like, difícil, BloodBorne, Terror, Ação"
+    },
+    {
+        titulo: "Ghost of Tsushima",
+        descricao: "Ghost of Tsushima se passa no final do século XIII, durante a invasão mongol à ilha de Tsushima, no Japão. O jogador controla Jin Sakai, um samurai que sobrevive ao massacre inicial dos mongóis e se vê forçado a adotar táticas furtivas e não convencionais para derrotar o invasor, questionando o rígido código de honra samurai. Conforme Jin luta para libertar sua terra, ele enfrenta não apenas os mongóis liderados pelo astuto Khotun Khan, mas também o conflito interno entre preservar suas tradições e se tornar o (Fantasma), um guerreiro sombrio que utiliza qualquer meio necessário para salvar Tsushima.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "Samurai, Espada, Ghost of Tsushima, ghost of tsushima, japão, mongol"
+    },
+    {
+        titulo: "Minecraft",
+        descricao: " Minecraft é vaga e aberta, permitindo que os jogadores criem suas próprias histórias em um mundo vasto e gerado proceduralmente. No entanto, o jogo possui elementos ocultos que sugerem uma narrativa. O jogador começa sozinho em um mundo cheio de biomas diversificados e criaturas, coletando recursos para sobreviver e construir. À medida que explora, descobre templos antigos, ruínas e portais para outras dimensões, como o Nether e o End. No End, o jogador enfrenta o Ender Dragon, uma criatura misteriosa que parece ser o guardião desse reino. A lore implícita sugere que civilizações antigas podem ter habitado essas terras antes, deixando vestígios de sua existência, mas cabe ao jogador interpretar esses mistérios enquanto molda sua própria aventura.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "bloco, Bloco, minecraft, Minecraft, Exploração"
+    },
+    {
+        titulo: "Skate 3",
+        descricao: "Skate 3 é ambientada na cidade fictícia de Port Carverton, onde o jogador assume o papel de um skatista profissional que busca construir sua própria marca de skate. Após um acidente em uma sessão de fotos, o protagonista decide formar uma equipe de skatistas, expandindo seu império no mundo do skate. O jogo é focado no aspecto colaborativo e na criação de uma comunidade, permitindo que o jogador complete desafios, realize manobras icônicas e desenvolva seu próprio estilo enquanto promove a cultura do skate. A cidade de Port Carverton é o cenário perfeito para explorar diferentes terrenos, como parques, praças e ruas, refletindo a liberdade e a expressão do skateboarding.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "Skate, skate, skate 3, Skate 3, manobra, Manobra, "
+    },
+];
