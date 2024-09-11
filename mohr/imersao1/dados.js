d25a74736376d1e33f695adba82035f4319bea58
Manu Mohr <167103609+manumohrr@users.noreply.github.com>
2024-09-11 08:51:22 -0300

    bob esponja

--- mohr/imersao1/dados.js
+++ mohr/imersao1/dados.js
@@ -1,20 +1,59 @@
 let dados = [
     {
         titulo: "Bob Esponja",
-        descricao: "Bob Esponja é uma esponja do mar super feliz que mora numa casa de abacaxi no fundo do mar. Ele adora cozinhar hambúrgueres no Siri Cascudo e brincar com seus amigos Patrick e Lula Molusco. Bob Esponja é sempre otimista e adora se divertir!",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
-        tags: "skate fada sk8"
+        descricao: "Bob Esponja é uma esponja do mar super feliz que mora numa casa de abacaxi no fundo do mar. Ele adora cozinhar hambúrgueres no Siri Cascudo e brincar com seus amigos. Bob Esponja é sempre otimista e adora se divertir!",
+        link: "https://pt.wikipedia.org/wiki/SpongeBob_SquarePants_(personagem)",
+        imagem: "bob.png",
+        tags: "bob esponja"
     },
     {
-        titulo: "Beatriz Souza",
-        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
-        tags: "judô judo judoca judoka"
+        titulo: "Patrick Estrela",
+        descricao: "Patrick é a estrela do mar mais divertida dos oceanos! Ele mora numa rocha bem ao lado da casa de abacaxi e passa seus dias fazendo coisas bem legais, como caçar águas-vivas, comer hambúrgueres do Siri Cascudo e dar muitas risadas.",
+        link: "https://pt.wikipedia.org/wiki/Patrick_Estrela",
+        tags: "patrik estrela"
     },
     {
-        titulo: "Rebeca Andrade",
-        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
-        tags: "ginástica ginastica ouro"
+        titulo: "Lula Molusco",
+        descricao: "Lula Molusco é um polvo roxo que mora numa escultura de cabeça para baixo. Ele adora tocar clarinete, pintar quadros e, no fundo, só quer um pouco de paz e quietude. Mas com vizinhos tão animados, isso é quase impossível!",
+        link: "https://en.wikipedia.org/wiki/Squidward_Tentacles",
+        tags: "lula molusco"
+    },
+    {
+        titulo: "Sandy Bochechas",
+        descricao: "Sandy é uma esquila cientista e especialista em karatê. Ela mora no fundo do mar em uma cúpula especial que mantém ar para que ela possa respirar. Sandy é muito inteligente, aventureira e sempre disposta a enfrentar desafios com sua força e habilidades científicas.",
+        link: "https://pt.wikipedia.org/wiki/Sandy_Bochechas",
+        tags: "sandy bochechas"
+    },
+    {
+        titulo: "Sr. Sirigueijo",
+        descricao: "Sr. Sirigueijo é o dono do Siri Cascudo. Ele é obcecado por dinheiro e sempre está em busca de novas formas de aumentar seus lucros. Embora seja ganancioso, ele tem um lado mais suave, especialmente quando se trata de sua filha.",
+        link: "https://pt.wikipedia.org/wiki/Sr._Sirigueijo",
+        tags: "sr sirigueijo"
+    },
+    {
+        titulo: "Pérola",
+        descricao: "Pérola é a filha adolescente do dono do Siri Cascuco, apesar de ser uma baleia. Ela é cheia de vida, ama moda e música, e às vezes se sente envergonhada pelas atitudes gananciosas de seu pai. Pérola sonha em ser uma estrela e vive uma típica vida adolescente.",
+        link: "https://pt.wikipedia.org/wiki/SpongeBob_SquarePants",
+        tags: "pérola baleia"
+    },
+    {
+        titulo: "Plankton",
+        descricao: "Plankton é o rival do Siri Cascudo e dono do Balde de Lixo. Ele está sempre tentando roubar a fórmula secreta do hambúrguer de siri, mas suas tentativas geralmente falham por conta de seus planos mal elaborados.",
+        link: "https://pt.wikipedia.org/wiki/Sheldon_J._Plankton",
+        tags: "plankton"
+    },
+    {
+        titulo: "Gary",
+        descricao: "Gary é o caracol de estimação. Embora ele apenas mia como um gato, Gary é incrivelmente inteligente e serve como uma presença calma e sensata. Ele é muito amado por seu dono e é conhecido por sua sabedoria silenciosa.",
+        link: "https://pt.wikipedia.org/wiki/Gary_the_Snail",
+        tags: "gary caracol"
+    },
+    {
+        titulo: "Karen",
+        descricao: "Karen é a esposa computador e seu principal suporte em suas tentativas de roubar a fórmula secreta do hambúrguer de siri. Ela frequentemente oferece conselhos inteligentes, mas nem sempre a escutam, o que leva à maioria de seus fracassos.",
+        link: "https://pt.wikipedia.org/wiki/Karen_(SpongeBob_SquarePants)",
+        tags: "karen computador"
     }
+    
+    
 ];
