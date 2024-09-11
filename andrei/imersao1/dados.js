5439c27c54e45a86d0a17ee9d78997cd27950164
AndreiKohls <167135621+AndreiKohls@users.noreply.github.com>
2024-09-11 08:19:42 -0300

    imersao

---
+++ andrei/imersao1/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Andrés Nicolás D'Alessandro",
+        descricao: "Andrés Nicolás D'Alessandro é um ex-futebolista argentino, naturalizado brasileiro, que se tornou um verdadeiro ídolo do Sport Club Internacional. Sua habilidade, visão de jogo e paixão pelo clube o transformaram em um dos maiores jogadores da história do Inter.",
+        link: "https://pt.wikipedia.org/wiki/Andr%C3%A9s_D%27Alessandro",
+        tags: "Inter Idolo Argentino canhoto 10 meia"
+    },
+    {
+        titulo: "Fernando Lúcio da Costa",
+        descricao: "Fernando Lúcio da Costa, mais conhecido como Fernandão, foi um ídolo incontestável do Sport Club Internacional. Sua trajetória no futebol brasileiro, marcada por paixão, garra e títulos, o consagrou como um dos maiores atacantes da história do clube gaúcho.",
+        link: "https://pt.wikipedia.org/wiki/Fernand%C3%A3o",
+        tags: "inter idolo destro atacante 9 brasileiro"
+    },
+    {
+        titulo: "Paulo Roberto Falcão",
+        descricao: "Paulo Roberto Falcão é um dos maiores jogadores de futebol da história do Brasil. Conhecido por sua elegância, visão de jogo e inteligência tática, Falcão deixou sua marca tanto no Sport Club Internacional quanto na Seleção Brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Paulo_Roberto_Falc%C3%A3o",
+        tags: "inter idolo volante meia brasileiro destro"
+    }
+];
