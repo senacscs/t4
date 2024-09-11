e447f677880ddb31e8d1a1c7a2048baa81846033
AndreiKohls <167135621+AndreiKohls@users.noreply.github.com>
2024-09-11 08:41:57 -0300

    imersao

--- andrei/imersao1/dados.js
+++ andrei/imersao1/dados.js
@@ -3,18 +3,24 @@ let dados = [
         titulo: "Andrés Nicolás D'Alessandro",
         descricao: "Andrés Nicolás D'Alessandro é um ex-futebolista argentino, naturalizado brasileiro, que se tornou um verdadeiro ídolo do Sport Club Internacional. Sua habilidade, visão de jogo e paixão pelo clube o transformaram em um dos maiores jogadores da história do Inter.",
         link: "https://pt.wikipedia.org/wiki/Andr%C3%A9s_D%27Alessandro",
-        tags: "Inter Idolo Argentino canhoto 10 meia dale dalessandro"
+        tags: "Inter Idolo Argentino argentina canhoto 10 meia dale dalessandro"
     },
     {
         titulo: "Fernando Lúcio da Costa",
         descricao: "Fernando Lúcio da Costa, mais conhecido como Fernandão, foi um ídolo incontestável do Sport Club Internacional. Sua trajetória no futebol brasileiro, marcada por paixão, garra e títulos, o consagrou como um dos maiores atacantes da história do clube gaúcho.",
         link: "https://pt.wikipedia.org/wiki/Fernand%C3%A3o",
-        tags: "inter idolo destro atacante 9 brasileiro fernandão"
+        tags: "inter idolo destro atacante 9 brasileiro brasil fernandão"
     },
     {
         titulo: "Paulo Roberto Falcão",
         descricao: "Paulo Roberto Falcão é um dos maiores jogadores de futebol da história do Brasil. Conhecido por sua elegância, visão de jogo e inteligência tática, Falcão deixou sua marca tanto no Sport Club Internacional quanto na Seleção Brasileira.",
         link: "https://pt.wikipedia.org/wiki/Paulo_Roberto_Falc%C3%A3o",
-        tags: "inter idolo volante meia brasileiro destro falcão"
+        tags: "inter idolo volante meia brasileiro brasil destro falcão"
+    }
+    {
+        titulo: "Elías Ricardo Figueroa Brander",
+        descricao: "Elías Ricardo Figueroa Brander é considerado um dos maiores zagueiros da história do futebol, e um dos maiores atletas chilenos de todos os tempos. Sua classe, elegância e liderança em campo o transformaram em um verdadeiro ídolo, tanto no Chile quanto no Brasil.",
+        link: "https://pt.wikipedia.org/wiki/El%C3%ADas_Figueroa",
+        tags: "inter idolo zagueiro chileno chile figueroa"
     }
 ];
