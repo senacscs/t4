a2cd3247cfadf5a9464ff86fd4cb05a54262fe34
pedroarthur22 <167103123+pedroarthur22@users.noreply.github.com>
2024-09-11 09:21:47 -0300

    subindso imersao

---
+++ pedroa/imersao1/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Fiat Uno",
+        descricao: "O Fiat Uno é um carro compacto muito popular no Brasil, conhecido por sua economia de combustível e manutenção acessível. Ele foi lançado pela primeira vez em 1983 e passou por várias atualizações ao longo dos anos.",
+        link: "https://www.webmotors.com.br/carros/estoque/fiat/uno/de.1994/ate.2000",
+        tags: "Lenda"
+    },
+    {
+        titulo: "Gol Quadrado",
+        descricao: "O Gol “quadrado” é um modelo icônico da Volkswagen, muito querido no Brasil. Ele foi produzido entre 1980 e 1994 e é conhecido por seu design angular e robustez. O Gol quadrado é famoso por sua durabilidade e facilidade de manutenção, o que o torna um clássico entre os entusiastas de carros antigos.",
+        link: "https://www.webmotors.com.br/carros/estoque/volkswagen/gol/16-cl-8v-gasolina-2p-manual",
+        tags: "Ícone"
+    },
+    {
+        titulo: "Fusca",
+        descricao: "O Fusca, também conhecido como Volkswagen Beetle, é um dos carros mais icônicos e amados do mundo. Produzido pela Volkswagen, ele foi lançado pela primeira vez em 1938 e se tornou um símbolo de simplicidade, durabilidade e design clássico.",
+        link: "https://www.webmotors.com.br/carros/estoque/volkswagen/fusca/de.1960/ate.1969",
+        tags: ""
+    }
+];
