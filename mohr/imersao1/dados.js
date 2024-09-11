ce0a97752eb2769fecf7419a1ecb2c7afcef292e
Manu Mohr <167103609+manumohrr@users.noreply.github.com>
2024-09-11 09:12:26 -0300

    comit

--- mohr/imersao1/dados.js
+++ mohr/imersao1/dados.js
@@ -10,48 +10,56 @@ let dados = [
         titulo: "Patrick Estrela",
         descricao: "Patrick é a estrela do mar mais divertida dos oceanos! Ele mora numa rocha bem ao lado da casa de abacaxi e passa seus dias fazendo coisas bem legais, como caçar águas-vivas, comer hambúrgueres do Siri Cascudo e dar muitas risadas.",
         link: "https://pt.wikipedia.org/wiki/Patrick_Estrela",
+        imagem: "patrik.png",
         tags: "patrik estrela"
     },
     {
         titulo: "Lula Molusco",
         descricao: "Lula Molusco é um polvo roxo que mora numa escultura de cabeça para baixo. Ele adora tocar clarinete, pintar quadros e, no fundo, só quer um pouco de paz e quietude. Mas com vizinhos tão animados, isso é quase impossível!",
         link: "https://en.wikipedia.org/wiki/Squidward_Tentacles",
+        imagem: "lula.png",
         tags: "lula molusco"
     },
     {
         titulo: "Sandy Bochechas",
         descricao: "Sandy é uma esquila cientista e especialista em karatê. Ela mora no fundo do mar em uma cúpula especial que mantém ar para que ela possa respirar. Sandy é muito inteligente, aventureira e sempre disposta a enfrentar desafios com sua força e habilidades científicas.",
         link: "https://pt.wikipedia.org/wiki/Sandy_Bochechas",
+        imagem: "sandy.png",
         tags: "sandy bochechas"
     },
     {
         titulo: "Sr. Sirigueijo",
         descricao: "Sr. Sirigueijo é o dono do Siri Cascudo. Ele é obcecado por dinheiro e sempre está em busca de novas formas de aumentar seus lucros. Embora seja ganancioso, ele tem um lado mais suave, especialmente quando se trata de sua filha.",
         link: "https://pt.wikipedia.org/wiki/Sr._Sirigueijo",
+        imagem: "siriqueijo.png",
         tags: "sr sirigueijo"
     },
     {
         titulo: "Pérola",
         descricao: "Pérola é a filha adolescente do dono do Siri Cascuco, apesar de ser uma baleia. Ela é cheia de vida, ama moda e música, e às vezes se sente envergonhada pelas atitudes gananciosas de seu pai. Pérola sonha em ser uma estrela e vive uma típica vida adolescente.",
         link: "https://pt.wikipedia.org/wiki/SpongeBob_SquarePants",
+        imagem: "perola.png",
         tags: "pérola baleia"
     },
     {
         titulo: "Plankton",
         descricao: "Plankton é o rival do Siri Cascudo e dono do Balde de Lixo. Ele está sempre tentando roubar a fórmula secreta do hambúrguer de siri, mas suas tentativas geralmente falham por conta de seus planos mal elaborados.",
         link: "https://pt.wikipedia.org/wiki/Sheldon_J._Plankton",
+        imagem: "plankton.png",
         tags: "plankton"
     },
     {
         titulo: "Gary",
         descricao: "Gary é o caracol de estimação. Embora ele apenas mia como um gato, Gary é incrivelmente inteligente e serve como uma presença calma e sensata. Ele é muito amado por seu dono e é conhecido por sua sabedoria silenciosa.",
         link: "https://pt.wikipedia.org/wiki/Gary_the_Snail",
+        imagem: "gary.png",
         tags: "gary caracol"
     },
     {
         titulo: "Karen",
         descricao: "Karen é a esposa computador e seu principal suporte em suas tentativas de roubar a fórmula secreta do hambúrguer de siri. Ela frequentemente oferece conselhos inteligentes, mas nem sempre a escutam, o que leva à maioria de seus fracassos.",
         link: "https://pt.wikipedia.org/wiki/Karen_(SpongeBob_SquarePants)",
+        imagem: "karen.png",
         tags: "karen computador"
     }
     
