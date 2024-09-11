856f700adc58098a87524fe29df3e18f9776caaf
davie500 <167103798+davie500@users.noreply.github.com>
2024-09-11 09:24:29 -0300

    atualizando site imersao1 e index

---
+++ davie/imersao1/dados.js
@@ -0,0 +1,58 @@
+let dados = [
+    {
+        titulo: "Eminem",
+        descricao: "Marshall Bruce Mathers III, mais conhecido pelo seu nome artístico Eminem, é um rapper, compositor, produtor musical e ator estadunidense. Adquiriu rápida popularidade em 1999 com o lançamento do disco The Slim Shady LP, o qual venceu o Grammy Award de Melhor Álbum de Rap do ano.",
+        link: "https://pt.wikipedia.org/wiki/Eminem",
+        imagem: "https://hips.hearstapps.com/hmg-prod/images/eminem-a-k-a-marshall-bruce-mathers-iii-attends-a-ceremony-news-photo-1698936282.jpg",
+        tags: "goat rap"
+    },
+    {
+        titulo: "Jay-Z",
+        descricao: "Shawn Corey Carter, mais conhecido pelo seu nome artístico JAY-Z, é um rapper, compositor, produtor e empresário norte-americano. Ele é um dos artistas de hip hop mais bem sucedidos empresarialmente e financeiramente nos Estados Unidos.",
+        link: "https://pt.wikipedia.org/wiki/Jay-Z",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Jay-Z_%40_Shawn_%27Jay-Z%27_Carter_Foundation_Carnival_%28crop_2%29.jpg/330px-Jay-Z_%40_Shawn_%27Jay-Z%27_Carter_Foundation_Carnival_%28crop_2%29.jpg",
+        tags: "rap usa goat"
+    },
+    {
+        titulo: "Matuê",
+        descricao: "Matheus Brasileiro Aguiar, mais conhecido pelo nome artístico Matuê, é um rapper, cantor, compositor e guitarrista brasileiro. Ficou conhecido com o single 'Anos Luz', lançado em 2017 e pelo álbum 'Máquina do Tempo' lançado em 2020. É considerado um dos símbolos do trap brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Matuê",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Matu%C3%AA_em_entrevista.png/375px-Matu%C3%AA_em_entrevista.png",
+        tags: "trap 333 brasil"
+    },
+    {
+        titulo: "MC Ryan SP",
+        descricao: "Ryan Santana dos Santos, mais conhecido como MC Ryan SP, é um cantor brasileiro de funk paulista, carioca e do trap.",
+        link: "https://pt.wikipedia.org/wiki/MC_Ryan_SP",
+        imagem: "https://p2.trrsf.com.br/image/fget/cf/540/960/smart/images.terra.com/2023/05/18/card1-reproducao-facebookmc-ryan-sp-sof4n63n6v6u.png",
+        tags: "funk mc trap"
+    },
+    {
+        titulo: "Kanye West",
+        descricao: "Atualmente, como cantor, West é o 4º artista que mais vendeu músicas em formato digital.[9] Além disso, ele acumula uma impressionante coleção de prêmios, incluindo um total de 24 Grammys, tornando-se assim o maior rapper da história da premiação.",
+        link: "https://pt.wikipedia.org/wiki/Kanye_West",
+        imagem: "https://pagesix.com/wp-content/uploads/sites/3/2024/03/kanye-west-attends-marni-fashion-77054237.jpg",
+        tags: "grammy rap usa"
+    },
+    {
+        titulo: "Dr. Dre",
+        descricao: "Andre Romell Young, mais conhecido pelo seu nome artístico Dr. Dre, é um rapper, produtor musical, editor musical, empresário e ator estadunidense. Surgiu na cena musical no fim da década de 1980. É um dos mais conhecidos entre os produtores de rap da atualidade.",
+        link: "https://pt.wikipedia.org/wiki/Dr._Dre",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Dr._Dre_2013.jpg",
+        tags: "produtor dre rap"
+    },
+    {
+        titulo: "Ice Cube",
+        descricao: "O'Shea Jackson Sr., mais conhecido pelo seu nome artístico Ice Cube, é um rapper e ator americano. Ele iniciou sua carreira em 1984 como membro do grupo C.I.A. e depois entrou para o grupo de gangsta rap N.W.A em 1986.",
+        link: "https://pt.wikipedia.org/wiki/Ice_Cube",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Ice-Cube_2014-01-09-Chicago-photoby-Adam-Bielawski.jpg/375px-Ice-Cube_2014-01-09-Chicago-photoby-Adam-Bielawski.jpg",
+        tags: "gelo ice cubo usa"
+    },
+    {
+        titulo: "L7NNON",
+        descricao: "Lennon dos Santos Barbosa Frassetti, mais conhecido artisticamente pelo seu prenome estilizado L7nnon, é um rapper e compositor brasileiro. Ficou conhecido nacionalmente e internacionalmente pelas canções 'Freio da Blazer', 'Perdição', 'Ai, Preto' e 'Desenrola, Bate, Joga de Ladin'.",
+        link: "https://pt.wikipedia.org/wiki/L7nnon",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/L7nnon_at_Rock_in_Rio_in_September_2022.png/345px-L7nnon_at_Rock_in_Rio_in_September_2022.png",
+        tags: "funk brasa lennon ladin"
+    }
+];
