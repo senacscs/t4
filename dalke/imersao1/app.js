964a77f56f9da4101fe82d78976b3e5983cc5183
guidalke <167103038+guidalke@users.noreply.github.com>
2024-09-11 12:29:54 -0300

    subindo imersao

--- dalke/imersao1/app.js
+++ dalke/imersao1/app.js
@@ -1,22 +1,24 @@
 function pesquisar() {
-    const pesquisa = document.getElementById('campo-pesquisa').value.toLowerCase();
-    const resultados = document.getElementById('resultados-pesquisa');
-    resultados.innerHTML = '';
-  
-    jogadores.forEach(jogador => {
-      if (jogador.nome.toLowerCase().includes(pesquisa) ||
-          jogador.posicao.toLowerCase().includes(pesquisa) ||
-          jogador.nacionalidade.toLowerCase().includes(pesquisa)) {
-        const resultado = document.createElement('div');
-        resultado.classList.add('item-resultado');
-        resultado.innerHTML = `
-          <img src="${jogador.imagem}" alt="${jogador.nome}">
-          <h2>${jogador.nome}</h2>
-          <p><strong>Posição:</strong> ${jogador.posicao}</p>
-          <p><strong>Nacionalidade:</strong> ${jogador.nacionalidade}</p>
-          <p>${jogador.descricao}</p>
-        `;
-        resultados.appendChild(resultado);
-      }
-    });
-  }
+  const pesquisa = document.getElementById('campo-pesquisa').value.toLowerCase();
+  const resultados = document.getElementById('resultados-pesquisa');
+  resultados.innerHTML = '';
+
+  racas.forEach(raca => {
+      if (raca.raça.toLowerCase().includes(pesquisa) ||
+          raca.nomeCientifico.toLowerCase().includes(pesquisa) ||
+          raca.origem.toLowerCase().includes(pesquisa) ||
+          raca.aptidao.toLowerCase().includes(pesquisa) ||
+          raca.descricao.toLowerCase().includes(pesquisa)) {
+          const resultado = document.createElement('div');
+          resultado.classList.add('item-resultado');
+          resultado.innerHTML = `
+              <img class="capas" aling="left" src=${raca.imagem}>
+              <h2>${raca.raça} (${raca.nomeCientifico})</h2>
+              <p><strong>Origem:</strong> ${raca.origem}</p>
+              <p><strong>Aptidão:</strong> ${raca.aptidao}</p>
+              <p>${raca.descricao}</p>
+          `;
+          resultados.appendChild(resultado);
+      }
+  });
+}
--- dalke/imersao1/dados.js
+++ dalke/imersao1/dados.js
@@ -1,27 +1,154 @@
-const jogadores = [
+const racas = [
     {
-      nome: "Pelé",
-      nacionalidade: "Brasileira",
-      posicao: "Atacante",
-      clubes: ["Santos", "Cosmos"],
-      imagem: "peleicon.jpeg",
-      descricao: "Considerado por muitos o maior jogador de todos os tempos..."
+        raça: "Nelore",
+        nomeCientifico: "Bos indicus",
+        origem: "Índia",
+        aptidao: "corte",
+        pesoMedio: "450-550 kg",
+        pelagem: "cinzenta ou vermelha",
+        adaptabilidade: "clima tropical",
+        descricao: "Raça zebuína de origem indiana, amplamente difundida no Brasil. Adaptada a condições tropicais, possui grande resistência a doenças e parasitas, além de boa conversão alimentar. Apresenta bosse cervical proeminente e pelagem curta e brilhante.",
+        imagem: "nelore.jpg"
     },
+    {
+        raça: "Angus",
+        nomeCientifico: "Bos taurus",
+        origem: "Escócia",
+        aptidao: "corte",
+        pesoMedio: "600-800 kg",
+        pelagem: "preta",
+        adaptabilidade: "clima temperado",
+        descricao: "Raça bovina britânica, conhecida por sua carne de alta qualidade, marmoreio e precocidade sexual. Animais precoces, com carcaça pesada e bem conformada. Pelagem curta, densa e brilhante.",
+        imagem: "angus.jpg"
+    },
+    {
+        raça: "Jersey",
+        nomeCientifico: "Bos taurus taurus",
+        origem: "Ilha de Jersey",
+        aptidao: "leiteira",
+        pesoMedio: "400-500 kg",
+        pelagem: "castanha ou cinza",
+        adaptabilidade: "clima temperado",
+        descricao: "Pequena raça conhecida por seu leite rico em gordura e proteína. Alta fertilidade e longevidade.",
+        imagem: "jersey.jpg"
+    },
+    {
+        raça: "Red Poll",
+        nomeCientifico: "Bos taurus taurus",
+        origem: "Inglaterra",
+        aptidao: "Dupla aptidão (leite e corte)",
+        pesoMedio: "550-700 kg",
+        pelagem: "Vermelha",
+        adaptabilidade: "Clima temperado",
+        descricao: "Raça antiga, conhecida por sua pelagem vermelha e chifres em forma de rosca. Produz leite e carne de boa qualidade.",
+        imagem: "red_poll.jpg"
+    },
+    
+    {
+        raça: "Charolês",
+        nomeCientifico: "Bos taurus taurus",
+        origem: "França",
+        aptidao: "Corte",
+        pesoMedio: "1000-1200 kg",
+        pelagem: "Branca",
+        adaptabilidade: "Clima temperado",
+        descricao: "Uma das maiores raças bovinas, conhecida por seu crescimento rápido e carcaça musculosa.",
+        imagem: "charoles.jpg"
+    },
+    
+    {
+        raça: "Watusi",
+        nomeCientifico: "Bos taurus africanus",
+        origem: "África",
+        aptidao: "Corte",
+        pesoMedio: "400-600 kg",
+        pelagem: "Variada (vermelha, branca, cinza)",
+        adaptabilidade: "Clima tropical",
+        descricao: "Famosa por seus chifres longos e curvos, essa raça africana é adaptada a ambientes áridos.",
+        imagem: "watusi.jpg"
+    },
+    
+    {
+        raça: "Galloway",
+        nomeCientifico: "Bos taurus taurus",
+        origem: "Escócia",
+        aptidao: "Corte",
+        pesoMedio: "600-800 kg",
+        pelagem: "Preta ou vermelha",
+        adaptabilidade: "Clima temperado",
+        descricao: "Raça rústica, conhecida por sua pelagem longa e densa, ideal para climas frios.",
+        imagem: "galloway.jpg"
+    },
+    
+    {
+        raça: "Indubrasil",
+        nomeCientifico: "Bos taurus indicus x Bos taurus taurus",
+        origem: "Brasil",
+        aptidao: "Corte",
+        pesoMedio: "Variável",
+        pelagem: "Variada",
+        adaptabilidade: "Clima tropical",
+        descricao: "Raça sintética brasileira, resultado do cruzamento de raças zebuínas e europeias, com alta adaptação ao trópico.",
+        imagem: "indubrasil.jpg"
+    },
+   
+{
+    raça: "Holstein",
+    nomeCientifico: "Bos taurus taurus",
+    origem: "Países Baixos",
+    aptidao: "Leiteira",
+    pesoMedio: "650-800 kg",
+    pelagem: "Preto e branco",
+    adaptabilidade: "Clima temperado",
+    descricao: "A raça leiteira mais popular do mundo, conhecida por sua alta produção de leite.",
+    imagem: "holstein.jpg"
+},
 
-        {
-            nome: "Lionel Messi",
-            nacionalidade: "Argentina",
-            posicao: "Atacante",
-            clubes: ["Barcelona", "PSG"],
-            imagem: "messiicon.jpg",
-            descricao: "Considerado um dos maiores jogadores da história..."
-        },
-        {
-            nome: "Cristiano Ronaldo",
-            nacionalidade: "Portuguesa",
-            posicao: "Atacante",
-            clubes: ["Manchester United", "Real Madrid", "Juventus"],
-            imagem: "CR7icon.jpeg",
-            descricao: "Conhecido por sua habilidade e gols incríveis..."
-        }
-    ];
+{
+    raça: "Hereford",
+    nomeCientifico: "Bos taurus taurus",
+    origem: "Inglaterra",
+    aptidao: "Corte",
+    pesoMedio: "800-1000 kg",
+    pelagem: "Vermelha com face, pescoço e membros brancos",
+    adaptabilidade: "Clima temperado",
+    descricao: "Raça britânica de corte, conhecida por sua rusticidade e adaptabilidade a diferentes condições.",
+    imagem: "hereford.jpg"
+},
+
+{
+    raça: "Brahman",
+    nomeCientifico: "Bos indicus",
+    origem: "Índia",
+    aptidao: "Corte",
+    pesoMedio: "600-800 kg",
+    pelagem: "Cinza, vermelha ou branca",
+    adaptabilidade: "Clima tropical",
+    descricao: "Raça zebuína utilizada para melhorar a adaptação de outras raças a climas quentes e úmidos.",
+    imagem: "brahman.jpg"
+},
+
+{
+    raça: "Simental",
+    nomeCientifico: "Bos taurus taurus",
+    origem: "Suíça",
+    aptidao: "Dupla aptidão (leite e corte)",
+    pesoMedio: "800-1000 kg",
+    pelagem: "Variada (vermelha, branca, pintada)",
+    adaptabilidade: "Clima temperado",
+    descricao: "Raça de dupla aptidão, com boa produção de leite e carne de qualidade.",
+    imagem: "simental.jpg"
+},
+
+{
+    raça: "Guzerá",
+    nomeCientifico: "Bos indicus",
+    origem: "Índia",
+    aptidao: "Corte e leite",
+    pesoMedio: "500-700 kg",
+    pelagem: "Cinza ou branca",
+    adaptabilidade: "Clima tropical",
+    descricao: "Raça zebuína de grande porte, com boa produção de leite e carne.",
+    imagem: "guzera.jpg"
+},
+]
