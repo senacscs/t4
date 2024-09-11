5f66d20b968c3b2ae255a7c49ea5b736063d9374
rosasara <167102970+rosasara@users.noreply.github.com>
2024-09-11 10:32:09 -0300

    imersao alura

---
+++ sararosa/imersao1/dados.js
@@ -0,0 +1,93 @@
+let dados = [
+    {
+        titulo: "Scoups (에스쿱스)",
+        descricao: "S.Coups, cujo nome verdadeiro é Choi Seung-cheol, é o líder do grupo de K-pop SEVENTEEN. Ele nasceu em 8 de agosto de 1995, em Daegu, Coreia do Sul. S.Coups é responsável pela equipe de Hip Hop do grupo e também atua como rapper principal e sub-vocalista. Ele começou como trainee em 2010 e treinou por seis anos antes de estrear com SEVENTEEN em 2015.",
+        link: "https://en.wikipedia.org/wiki/S.Coups",
+        imagem:"S.Coups.png",
+        tags: "scoups s.coups seungcheol lider hiphop hip-hop raper"
+    },
+    {
+        titulo: "Jeonghan (정한)",
+        descricao: "Jeonghan, cujo nome completo é Yoo Jeonghan, é um dos vocalistas do Seventeen, conhecido por sua aparência angelical e voz suave. Ele nasceu em 4 de outubro de 1995, na Coreia do Sul. Além de seu talento musical, Jeonghan é apreciado pelos fãs por sua personalidade carismática e seu senso de humor. Ele também é conhecido por cuidar bem dos outros membros do grupo, ganhando o apelido de “anjo” entre os fãs",
+        link: "https://en.wikipedia.org/wiki/Jeonghan",
+        imagem:"Jeonghan.png",
+        tags: "jeonghan anjo vocal"
+    },
+    {
+        titulo: "Joshua (조슈아)",
+        descricao: "Joshua, cujo nome verdadeiro é Joshua Hong, nasceu em 30 de dezembro de 1995, em Los Angeles, Califórnia, EUA. Ele é um dos vocalistas principais do Seventeen e faz parte da sub-unidade Vocal. Joshua é conhecido por sua voz suave e personalidade calma. Ele é fluente em inglês e coreano, e antes de se juntar ao Seventeen, participava do grupo de louvor da igreja",
+        link: "https://en.wikipedia.org/wiki/Joshua_Hong",
+        imagem:"Joshua.png",
+        tags: "joshua vocal hong"
+    },  
+    {
+        titulo: "Jun (문준휘)",
+        descricao: "Jun, cujo nome verdadeiro é Wen Junhui, nasceu em 10 de junho de 1996, em Shenzhen, Guangdong, China. Ele é um dos dançarinos principais e sub-vocalista do Seventeen. Antes de se juntar ao grupo, Jun foi ator infantil na China, participando de vários filmes e séries de TV. Ele é conhecido por suas habilidades em artes marciais e por falar mandarim, cantonês e coreano",
+        link: "https://en.wikipedia.org/wiki/Wen_Junhui",
+        imagem:"Wen_junhui.png",
+        tags: "jun junhui moon dancer "
+    },
+    {
+        titulo: "Hoshi (호시)",
+        descricao: "Hoshi, cujo nome verdadeiro é Kwon Soon-young, nasceu em 15 de junho de 1996, em Namyangju, Gyeonggi-do, Coreia do Sul. Ele é o líder da sub-unidade Performance do Seventeen, além de ser um dos dançarinos principais e vocalista. Hoshi é conhecido por coreografar muitas das performances do grupo e por sua energia contagiante no palco.",
+        link: "https://en.wikipedia.org/wiki/Hoshi_(South_Korean_singer)",
+        imagem:"Hoshi.jpg",
+        tags: "hoshi tigre tiger lider dancer bss"
+    },
+    {
+        titulo: "Wonwoo (원우)",
+        descricao: "Wonwoo, cujo nome completo é Jeon Wonwoo, nasceu em 17 de julho de 1996, em Changwon, Gyeongsangnam-do, Coreia do Sul. Ele é um dos rappers do Seventeen e faz parte da sub-unidade Hip-Hop. Wonwoo é conhecido por sua voz profunda e presença calma. Ele começou como cantor, mas foi incentivado a se tornar rapper devido à sua voz única. Além disso, ele gosta de ler, jogar videogames e assistir filmes",
+        link: "https://en.wikipedia.org/wiki/Wonwoo",
+        imagem:"Wonwoo.png",
+        tags: "wonwoo wonu nunu hiphop hip-hop raper"
+    },
+    {
+        titulo: "woozi (우지)",
+        descricao: "Woozi, cujo nome verdadeiro é Lee Ji-hoon, nasceu em 22 de novembro de 1996, em Busan, Coreia do Sul. Ele é o líder da sub-unidade Vocal do Seventeen e é conhecido por ser o principal produtor e compositor do grupo. Woozi começou sua carreira musical estudando música clássica e tocando clarinete. Ele é responsável por compor e produzir a maioria das músicas do Seventeen, ganhando o apelido de 'Genius Composer'",
+        link: "https://en.wikipedia.org/wiki/Woozi",
+        imagem:"woozi.png",
+        tags: "woozi jihon lider vocal produtor"
+    },
+    {
+        titulo: "the8 (디에잇)",
+        descricao: "The8, cujo nome verdadeiro é Xu Minghao, nasceu em 7 de novembro de 1997, em Anshan, Liaoning, China. Ele é um dos dançarinos principais e sub-vocalista do Seventeen. The8 é conhecido por suas habilidades em b-boying e artes marciais, tendo praticado Wushu desde os 5 anos. Ele também participou como mentor de dança no programa de sobrevivência chinês 'Youth With You'",
+        link: "https://en.wikipedia.org/wiki/The8",
+        imagem:"The8.png",
+        tags: "the8 8 oito eight tieigt tiegth dancer"
+    },
+    {
+        titulo: "mingyu (민규)",
+        descricao: "Mingyu, cujo nome completo é Kim Min-gyu, nasceu em 6 de abril de 1997, em Anyang-si, Gyeonggi-do, Coreia do Sul. Ele é um dos rappers principais, sub-vocalista e visual do Seventeen. Mingyu é conhecido por sua altura impressionante (187 cm) e por ser o 'mão direita' do grupo, ajudando com tarefas como cozinhar e limpar. Ele também tem um grande interesse por moda e já trabalhou como modelo.",
+        link: "https://en.wikipedia.org/wiki/Mingyu",
+        imagem:"Mingyu.png",
+        tags: "mingyu gyu hiphop hip-hop"
+    },
+    {
+        titulo: "DK (도겸)",
+        descricao: "DK, cujo nome verdadeiro é Lee Seokmin, nasceu em 18 de fevereiro de 1997, em Suji-gu, Yongin-si, Gyeonggi-do, Coreia do Sul. Ele é um dos vocalistas principais do Seventeen e faz parte da sub-unidade Vocal. DK é conhecido por sua voz poderosa e carismática, além de sua personalidade alegre e energética. Ele se inspirou a se tornar cantor ao assistir performances de Yoon Do-hyun.",
+        link: "https://en.wikipedia.org/wiki/DK_(singer)",
+        imagem:"DK.png",
+        tags: "dk DK dokyeom bss vocal "
+    },
+    {
+        titulo: "Boo Seungkwan (부승관)",
+        descricao: "Boo Seung-kwan, nasceu em 16 de janeiro de 1998, em Busan, Coreia do Sul, mas cresceu em Jeju. Ele é um dos vocalistas principais do Seventeen e faz parte da sub-unidade Vocal. Seungkwan é conhecido por sua voz poderosa e por ser o 'mood maker' do grupo, sempre trazendo alegria e energia. Além de suas atividades musicais, ele é popular em programas de variedades coreanos.",
+        link: "https://en.wikipedia.org/wiki/Seungkwan",
+        imagem:"Boo_Seungkwan.png",
+        tags: "seungkwan boo kwan vocal bss"
+    },
+    {
+        titulo: "Vernon (버논)",
+        descricao: "Vernon, cujo nome verdadeiro é Hansol Vernon Chwe, nasceu em 18 de fevereiro de 1998, em Nova Iorque, EUA. Ele é um dos rappers principais do Seventeen e faz parte da sub-unidade Hip-Hop. Vernon é conhecido por seu estilo único e por ser metade coreano e metade americano. Ele se mudou para a Coreia do Sul quando tinha 5 anos e é fluente em inglês e coreano. Além de sua carreira musical, Vernon tem interesse por moda e atua como modelo ocasionalmente",
+        link: "https://en.wikipedia.org/wiki/Vernon_(rapper)",
+        imagem:"Vernon.png",
+        tags: "vernon hansol hiphop hip-hop"
+    },
+    {
+        titulo: "Dino (이찬)",
+        descricao: "Dino, cujo nome verdadeiro é Lee Chan, nasceu em 11 de fevereiro de 1999, em Iksan, Jeollabuk-do, Coreia do Sul. Ele é o dançarino principal, sub-vocalista e sub-rapper do Seventeen, além de ser o membro mais jovem (maknae) do grupo. Dino é conhecido por suas habilidades excepcionais em dança e coreografia, tendo criado coreografias para várias músicas do Seventeen. Ele também é fã de Michael Jackson e se inspira nele para suas performances.",
+        link: "https://en.wikipedia.org/wiki/Dino_(South_Korean_singer)",
+        imagem:"Dino.png",
+        tags: "dino dancer leechan chan lee "
+    },
+];
