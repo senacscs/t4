22915cd67b6776db3362a0b7aa9b6ced202934a4
luscaszs <167103492+luscaszs@users.noreply.github.com>
2024-09-11 09:10:46 -0300

    remoção dos comentários

--- lucas/imersao1/app.js
+++ lucas/imersao1/app.js
@@ -1,10 +1,10 @@
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
+   
     let section = document.getElementById("resultados-pesquisa");
 
     let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-    // se campoPesquisa for uma string sem nada
+
     if (!campoPesquisa) {
         section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
         return 
@@ -12,20 +12,20 @@ function pesquisar() {
 
     campoPesquisa = campoPesquisa.toLowerCase()
 
-    // Inicializa uma string vazia para armazenar os resultados
+   
     let resultados = "";
     let titulo = ""; 
     let descricao = "";
     let tags = "";
 
-    // Itera sobre cada dado da lista de dados
+    
     for (let dado of dados) {
         titulo = dado.titulo.toLowerCase()
         descricao = dado.descricao.toLowerCase()
         tags = dado.tags.toLowerCase()
-        // se titulo includes campoPesquisa
+        
         if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
-            // cria um novo elemento
+           
             resultados += `
             <div class="item-resultado">
                 <h2>
@@ -42,6 +42,6 @@ function pesquisar() {
         resultados = "<p>Nada foi encontrado</p>"
     }
 
-    // Atribui os resultados gerados à seção HTML
+    
     section.innerHTML = resultados;
 }
