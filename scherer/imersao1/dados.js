e192d7aae3bdb5238a04ee9c71493344f7de2f2d
schererarthur <167102905+schererarthur@users.noreply.github.com>
2024-09-12 11:39:44 -0300

    imersão conitado

--- scherer/imersao1/dados.js
+++ scherer/imersao1/dados.js
@@ -2,26 +2,31 @@ let dados = [
     {
       titulo: "Bitcoin",
       descricao: "É a criptomoeda mais famosa e a primeira a ser criada. Muitas pessoas a consideram como o ouro digital.",
-      tags: "bitcoin"
+      tags: "bitcoin",
+      imagem: "bitcion.png"
     },
     {
       titulo: "Ethereum (ETH)",
       descricao: "Além de ser uma moeda, o Ethereum é uma plataforma que permite a criação de outras criptomoedas e aplicativos descentralizados.",
-      tags: "ethereum, eth"
+      tags: "ethereum, eth",
+      imagem: "eth.png"
     },
     {
       titulo: "Binance Coin (BNB)",
       descricao: "É a criptomoeda da plataforma de exchange Binance, uma das maiores do mundo.",
-      tags: "binance, bnb"
+      tags: "binance, bnb",
+      imagem: "binance.png"
     },
     {
       titulo: "Cardano (ADA)",
       descricao: "Conhecida por sua abordagem científica e sustentável, o Cardano busca resolver alguns dos problemas do Ethereum.",
-      tags: "cardano, ada"
+      tags: "cardano, ada",
+      imagem: "car.png"
     },
     {
       titulo: "Solana (SOL)",
       descricao: "Uma plataforma de blockchain de alto desempenho, com foco em contratos inteligentes e aplicativos descentralizados.",
-      tags: "solana, sol"
+      tags: "solana, sol",
+      imagem: "solana.png"
     }
   ];
