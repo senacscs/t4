c8e958677253f075a08f0fce1b8f842e97ed691d
nicolekauane <167103683+nicolekauane@users.noreply.github.com>
2024-09-12 11:28:58 -0300

    imersao

--- nick/imersao1/dados.js
+++ nick/imersao1/dados.js
@@ -4,87 +4,202 @@ let dados = [
         titulo: "Meredith Grey",
         descricao: "Meredith Grey é a protagonista de Grey's Anatomy, uma cirurgiã brilhante e determinada que enfrenta inúmeros desafios pessoais e profissionais ao longo da série. Sua jornada é marcada por perdas, amor, crescimento e a busca incansável por excelência na medicina. A personagem se tornou um ícone da televisão, inspirando milhões de fãs com sua força e resiliência.",
         link: "https://pt.wikipedia.org/wiki/Meredith_Grey",
-        tags: "Grey"
+        tags: "Grey",
+        imagem: "imagens/mer.webp"
     },
     {
         titulo: "George O'Malley",
         descricao: "George O'Malley foi um dos internos mais queridos de Grey's Anatomy. Sua jornada no Seattle Grace Hospital foi marcada por desafios, crescimento pessoal e um grande amor por Meredith Grey. Tragicamente, seu destino foi interrompido de forma abrupta, deixando uma marca profunda nos corações dos fãs da série.",
         link: "https://pt.wikipedia.org/wiki/George_O%27Malley",
-        tags: "o'malley"
+        tags: "o'malley",
+        imagem: "imagens/geoge.webp"
     },
     {
         titulo: "Alex Karev",
         descricao: "Alex Karev, um dos personagens mais complexos de Grey's Anatomy, iniciou sua jornada como um interno arrogante e rude, mas evoluiu para um médico talentoso e leal. Sua relação com Meredith, Izzie e Cristina moldou sua personalidade, e sua saída inesperada para reatar com Izzie marcou o fim de uma era na série.",
         link: "https://pt.wikipedia.org/wiki/Alex_Karev",
-        tags: "kavev"
+        tags: "kavev",
+        imagem: "imagens/karev.jpg"
     },
         {
             titulo: "Cristina Yang",
             descricao: "Cristina Yang é uma das personagens mais ambiciosas de Grey's Anatomy. Seu foco incansável em sua carreira de cirurgiã cardíaca fez dela uma das melhores da área, embora suas relações pessoais tenham sido muitas vezes complicadas.",
             link: "https://pt.wikipedia.org/wiki/Cristina_Yang",
-            tags: "yang"
+            tags: "yang",
+            imagem: "imagens/yang.jpg"
         },
       
         {
             titulo: "Izzie Stevens",
             descricao: "Izzie Stevens era uma cirurgiã dedicada e carinhosa, conhecida por seu forte vínculo emocional com seus pacientes. Sua história em Grey's Anatomy foi marcada por relacionamentos intensos e uma luta contra o câncer.",
             link: "https://pt.wikipedia.org/wiki/Izzie_Stevens",
-            tags: "stevens"
+            tags: "stevens",
+            imagem: "imagens/izzie.jpg"
         },
         {
             titulo: "Miranda Bailey",
             descricao: "Miranda Bailey é uma das figuras mais fortes e inspiradoras do hospital, com uma personalidade implacável, mas também extremamente justa e dedicada ao ensino de seus internos.",
             link: "https://pt.wikipedia.org/wiki/Miranda_Bailey",
-            tags: "bailey"
+            tags: "bailey",
+            imagem: "imagens/bailey.jpg"
         },
         {
             titulo: "Derek Shepherd",
             descricao: "Derek Shepherd, também conhecido como 'McDreamy', era um dos neurocirurgiões mais talentosos de Grey's Anatomy. Seu romance com Meredith Grey e sua morte trágica marcaram profundamente a série.",
             link: "https://pt.wikipedia.org/wiki/Derek_Shepherd",
-            tags: "shepherd"
-        },
-        {
-            titulo: "Derek Shepherd",
-            descricao: "Derek Shepherd, também conhecido como 'McDreamy', era um dos neurocirurgiões mais talentosos de Grey's Anatomy. Seu romance com Meredith Grey e sua morte trágica marcaram profundamente a série.",
-            link: "https://pt.wikipedia.org/wiki/Derek_Shepherd",
-            tags: "shepherd"
+            tags: "shepherd",
+            imagem: "imagens/derek.webp"
         },
         {
             titulo: "Richard Webber",
             descricao: "Richard Webber é o chefe de cirurgia por boa parte da série. Um mentor respeitado por todos, ele enfrenta suas próprias lutas pessoais com o alcoolismo e sua vida amorosa.",
             link: "https://pt.wikipedia.org/wiki/Richard_Webber",
-            tags: "webber"
+            tags: "webber",
+            imagem: "imagens/Richard-Webber.jpg"
         },
         {
             titulo: "Callie Torres",
             descricao: "Callie Torres, uma cirurgiã ortopédica talentosa, é conhecida por sua personalidade forte e relações amorosas intensas. Ela se destaca como uma personagem que luta pelo equilíbrio entre sua carreira e vida pessoal.",
             link: "https://pt.wikipedia.org/wiki/Callie_Torres",
-            tags: "torres"
+            tags: "torres",
+            imagem: "imagens/calie.jpg"
         },
         {
             titulo: "Owen Hunt",
             descricao: "Owen Hunt, um cirurgião traumatologista com um passado militar, traz uma abordagem disciplinada e prática ao hospital. Ele também enfrenta desafios em sua vida pessoal, especialmente com Cristina Yang.",
             link: "https://pt.wikipedia.org/wiki/Owen_Hunt",
-            tags: "hunt"
+            tags: "hunt",
+            imagem: "imagens/owen.jpeg"
         },
         {
             titulo: "Arizona Robbins",
             descricao: "Arizona Robbins é uma cirurgiã pediátrica com uma atitude positiva e otimista. Ela é uma das personagens mais amadas, especialmente por sua relação com Callie Torres.",
             link: "https://pt.wikipedia.org/wiki/Arizona_Robbins",
-            tags: "robbins"
+            tags: "robbins",
+            imagem: "imagens/arizona.webp"
         },
         {
             titulo: "April Kepner",
             descricao: "April Kepner, uma cirurgiã com fortes convicções religiosas, enfrenta muitos desafios tanto no lado pessoal quanto profissional. Sua jornada de autodescoberta e crescimento é uma das mais emocionantes da série.",
             link: "https://pt.wikipedia.org/wiki/April_Kepner",
-            tags: "kepner"
+            tags: "kepner",
+            imagem: "imagens/april.jpg"
         },
         {
             titulo: "Jackson Avery",
             descricao: "Jackson Avery, neto do famoso cirurgião Harper Avery, é um talentoso cirurgião plástico. Seu charme e habilidades fazem dele um dos personagens mais populares da série.",
             link: "https://pt.wikipedia.org/wiki/Jackson_Avery",
-            tags: "avery"
+            tags: "avery",
+            imagem: "imagens/avery.jpg"
+        },
+        {
+            titulo: "Maggie Pierce",
+            descricao: "Maggie Pierce, meia-irmã de Meredith Grey, é uma cirurgiã cardíaca extremamente talentosa. Sua inteligência e energia positiva fazem dela uma peça fundamental no hospital.",
+            link: "https://pt.wikipedia.org/wiki/Maggie_Pierce",
+            tags: "pierce",
+            imagem: "imagens/mag.jpg"
+        },
+        {
+            titulo: "Jo Wilson",
+            descricao: "Jo Wilson é uma cirurgiã geral que teve uma infância difícil e lutou para se destacar no hospital. Sua resiliência e habilidades a tornam uma personagem importante na série.",
+            link: "https://pt.wikipedia.org/wiki/Jo_Wilson",
+            tags: "wilson",
+            imagem: "imagens/jo.jpg"
+        },
+        {
+            titulo: "Levi Schmitt",
+            descricao: "Levi Schmitt é um dos internos mais recentes no Grey Sloan Memorial. Apesar de ser inicialmente tímido, ele evolui e ganha confiança ao longo da série.",
+            link: "https://pt.wikipedia.org/wiki/Levi_Schmitt",
+            tags: "schmitt",
+            imagem: "imagens/levi.jpeg"
+        },
+        {
+            titulo: "Amelia Shepherd",
+            descricao: "Amelia Shepherd, irmã de Derek Shepherd, é uma neurocirurgiã talentosa que luta com seus próprios demônios pessoais, mas que mostra grande habilidade e dedicação à medicina.",
+            link: "https://pt.wikipedia.org/wiki/Amelia_Shepherd",
+            tags: "shepherd",
+            imagem: "imagens/ame.jpeg"
+        },
+        {
+            titulo: "Catherine Fox",
+            descricao: "Catherine Fox (anteriormente Avery) é uma cirurgiã urológica de renome e mãe de Jackson Avery. Ela tem grande influência no hospital e é presidente da Fundação Harper Avery.",
+            link: "https://pt.wikipedia.org/wiki/Catherine_Fox",
+            tags: "fox",
+            imagem: "imagens/fox.jpeg"
+        },
+        {
+            titulo: "Teddy Altman",
+            descricao: "Teddy Altman é uma cirurgiã cardiotorácica com uma história complicada de amizade e amor com Owen Hunt. Ela traz uma abordagem emocional e técnica à medicina.",
+            link: "https://pt.wikipedia.org/wiki/Teddy_Altman",
+            tags: "altman",
+            imagem: "imagens/ted.avif"
+        },
+        {
+            titulo: "Andrew DeLuca",
+            descricao: "Andrew DeLuca é um jovem cirurgião com uma paixão pela medicina. Sua trajetória na série envolve tanto desafios profissionais quanto pessoais, incluindo suas batalhas com a saúde mental.",
+            link: "https://pt.wikipedia.org/wiki/Andrew_DeLuca",
+            tags: "deluca",
+            imagem: "imagens/luca.webp"
+        },
+        {
+            titulo: "Taryn Helm",
+            descricao: "Taryn Helm é uma residente do Grey Sloan Memorial, conhecida por sua dedicação e por sua admiração por Meredith Grey. Ela se destaca pela ética de trabalho e personalidade única.",
+            link: "https://pt.wikipedia.org/wiki/Taryn_Helm",
+            tags: "helm",
+            imagem: "imagens/helm.jpg"
+        },
+        {
+            titulo: "Atticus 'Link' Lincoln",
+            descricao: "Link é um cirurgião ortopédico com uma personalidade descontraída e um grande coração. Ele traz um toque de leveza ao hospital e rapidamente se torna uma figura importante na equipe.",
+            link: "https://pt.wikipedia.org/wiki/Atticus_Lincoln",
+            tags: "link",
+            imagem: "imagens/link.jpeg"
+        },
+        {
+            titulo: "Tom Koracick",
+            descricao: "Tom Koracick é um neurocirurgião brilhante, mas arrogante, que passa a integrar a equipe do Grey Sloan. Embora tenha um comportamento difícil, ele demonstra grande habilidade na medicina.",
+            link: "https://pt.wikipedia.org/wiki/Tom_Koracick",
+            tags: "koracick",
+            imagem: "imagens/tom.jpeg"
+        },
+        {
+            titulo: "Lexie Grey",
+            descricao: "Lexie Grey, meia-irmã mais nova de Meredith Grey, é uma cirurgiã extremamente inteligente e dedicada. Sua memória fotográfica a torna uma médica excepcionalmente talentosa.",
+            link: "https://pt.wikipedia.org/wiki/Lexie_Grey",
+            tags: "lexie",
+            imagem: "imagens/lexie.jpeg"
+        },
+        {
+            titulo: "Mark Sloan",
+            descricao: "Mark Sloan, também conhecido como 'McSteamy', é um cirurgião plástico carismático e confiante. Sua relação com Derek Shepherd e Lexie Grey desempenha um papel importante em sua história.",
+            link: "https://pt.wikipedia.org/wiki/Mark_Sloan",
+            tags: "sloan",
+            imagem: "imagens/mark.jpg"
+        },
+        {
+            titulo: "Sadie Harris",
+            descricao: "Sadie Harris é uma amiga de longa data de Meredith Grey e entra como interna no hospital. Ela é aventureira e disposta a correr riscos, mas sua abordagem impulsiva à medicina gera conflitos.",
+            link: "https://pt.wikipedia.org/wiki/Sadie_Harris",
+            tags: "harris",
+            imagem: "imagens/harri.jpg"
+        },
+        {
+            titulo: "Stephanie Edwards",
+            descricao: "Stephanie Edwards é uma residente brilhante e ambiciosa. Ela tem uma forte ética de trabalho e luta para se destacar em meio a desafios pessoais e profissionais.",
+            link: "https://pt.wikipedia.org/wiki/Stephanie_Edwards",
+            tags: "edwards",
+            imagem: "imagens/edu.jpg"
+        },
+        {
+            titulo: "Nathan Riggs",
+            descricao: "Nathan Riggs é um cirurgião cardiotorácico que entra na série com uma história complicada envolvendo Owen Hunt e uma antiga noiva desaparecida. Ele desenvolve um relacionamento com Meredith Grey.",
+            link: "https://pt.wikipedia.org/wiki/Nathan_Riggs",
+            tags: "riggs",
+            imagem: "imagens/rigs.jpeg"
         }
+        
+        
+        
     ];
     
     console.log(personagens);
