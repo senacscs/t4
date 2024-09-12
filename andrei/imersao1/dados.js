e7a4276f33bc83fd63007681682c667c4411ebcc
AndreiKohls <167135621+AndreiKohls@users.noreply.github.com>
2024-09-12 10:27:57 -0300

    imersao

--- andrei/imersao1/dados.js
+++ andrei/imersao1/dados.js
@@ -3,30 +3,35 @@ let dados = [
         titulo: "Andrés Nicolás D'Alessandro",
         descricao: "Andrés Nicolás D'Alessandro é um ex-futebolista argentino, naturalizado brasileiro, que se tornou um verdadeiro ídolo do Sport Club Internacional. Sua habilidade, visão de jogo e paixão pelo clube o transformaram em um dos maiores jogadores da história do Inter.",
         link: "https://pt.wikipedia.org/wiki/Andr%C3%A9s_D%27Alessandro",
-        tags: "Inter Idolo Argentino argentina canhoto 10 meia dale dalessandro"
+        tags: "Inter Idolo Argentino argentina canhoto 10 meia dale dalessandro",
+        imagem: "imagens/dale.jpg"
     },
     {
         titulo: "Fernando Lúcio da Costa",
         descricao: "Fernando Lúcio da Costa, mais conhecido como Fernandão, foi um ídolo incontestável do Sport Club Internacional. Sua trajetória no futebol brasileiro, marcada por paixão, garra e títulos, o consagrou como um dos maiores atacantes da história do clube gaúcho.",
         link: "https://pt.wikipedia.org/wiki/Fernand%C3%A3o",
-        tags: "inter idolo destro atacante 9 brasileiro brasil fernandão"
+        tags: "inter idolo destro atacante 9 brasileiro brasil fernandão",
+        imagem: "imagens/fernandao.jpg"
     },
     {
         titulo: "Paulo Roberto Falcão",
         descricao: "Paulo Roberto Falcão é um dos maiores jogadores de futebol da história do Brasil. Conhecido por sua elegância, visão de jogo e inteligência tática, Falcão deixou sua marca tanto no Sport Club Internacional quanto na Seleção Brasileira.",
         link: "https://pt.wikipedia.org/wiki/Paulo_Roberto_Falc%C3%A3o",
-        tags: "inter idolo volante meia brasileiro brasil destro falcão"
+        tags: "inter idolo volante meia brasileiro brasil destro falcão",
+        imagem: "imagens/falcao.jpg"
     },
     {
         titulo: "Elías Ricardo Figueroa Brander",
         descricao: "Elías Ricardo Figueroa Brander é considerado um dos maiores zagueiros da história do futebol, e um dos maiores atletas chilenos de todos os tempos. Sua classe, elegância e liderança em campo o transformaram em um verdadeiro ídolo, tanto no Chile quanto no Brasil.",
         link: "https://pt.wikipedia.org/wiki/El%C3%ADas_Figueroa",
-        tags: "inter idolo zagueiro chileno chile figueroa"
+        tags: "inter idolo zagueiro chileno chile figueroa",
+        imagem: "imagens/figueroa.jpg"
     },
     {
         titulo: "Marcos Antônio de Lima",
         descricao: "Marcos Antônio de Lima, mais conhecido como Índio, é um dos maiores ídolos da história do Internacional. Sua trajetória no clube gaúcho foi marcada por títulos, paixão e uma identificação única com a torcida colorada.",
         link: "https://pt.wikipedia.org/wiki/Marcos_Ant%C3%B4nio_de_Lima",
-        tags: "inter idolo zagueiro brasileiro brasil indio"
+        tags: "inter idolo zagueiro brasileiro brasil indio",
+        imagem: "imagens/indio.jpg"
     },
 ];
