2b4cc46c7d647d927e125576bcf49e518f07f88b
davie500 <167103798+davie500@users.noreply.github.com>
2024-09-12 11:14:55 -0300

    atualizando index e imersao1

--- davie/imersao1/dados.js
+++ davie/imersao1/dados.js
@@ -82,5 +82,159 @@ let dados = [
         link: "https://pt.wikipedia.org/wiki/J._Cole",
         imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjGIqJUSg-Iiu3rl1KuqUvzli6Fmum43jaGg&s",
         tags: "branco rap afiado"
+    },
+    {
+        titulo: "Drake",
+        descricao: "Aubrey Drake Graham é um rapper, cantor, compositor, produtor musical, ator e empresário canadense. Drake inicialmente ganhou reconhecimento como ator na série de televisão de drama adolescente Degrassi: The Next Generation, no início dos anos 2000, iniciou sua carreira músical de forma independente em 2006, e após reconhecimento, assinou com a Young Money Entertainment em 2009.",
+        link: "https://pt.wikipedia.org/wiki/Drake",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpi1v1tteNlNWiqcj9pIhiVtrmFJKPBTgGlQ&s",
+        tags: "outubro rap god"
+    },
+    {
+        titulo: "Ludacris",
+        descricao: "Ludacris, nome artístico de Christopher Brian Bridges, é um rapper, ator e empreendedor americano. Estreou-se em 1999 com Incognegro, um álbum lançado a título independente pela sua editora, a Disturbing tha Peace. Depois, com a criação da Def Jam South, subsidiária da Def Jam, Ludacris é contratado e torna-se na principal face da mesma.",
+        link: "https://pt.wikipedia.org/wiki/Ludacris",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRYxKV1HZ4wg_T1nmtkfDUL-eO-3E7DnF07g&s",
+        tags: "negro rap ator"
+    },
+    {
+        titulo: "Travis Scott",
+        descricao: "Jacques Berman Webster II, mais conhecido por seu nome artístico Travis Scott, é um rapper, cantor, compositor e produtor musical norte-americano. Em 2012, Scott assinou seu primeiro grande contrato com a gravadora Epic Records.",
+        link: "https://pt.wikipedia.org/wiki/Travis_Scott",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWlWHVM4GTuL247FsAGH3aLN8Ohprpkwhezg&s",
+        tags: "negro rap tatuado famoso"
+    },
+    {
+        titulo: "Nicki Minaj",
+        descricao: "Onika Tanya Maraj, conhecida por seu nome artístico Nicki Minaj, é uma rapper, cantora, compositora, modelo e atriz trinidiana radicada nos Estados Unidos. Minaj nasceu no país caribenho e mudou-se para o bairro nova-iorquino do Queens quando tinha cinco anos de idade, se formando em escolas artísticas mais tarde.",
+        link: "https://pt.wikipedia.org/wiki/Nicki_Minaj",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7BDKG7R0eYoi0jCLnfHu8NmsUPDcXikLHuA&s",
+        tags: "mulher negro rap"
+    },
+    {
+        titulo: "Cardi B",
+        descricao: "Belcalis Marlenis Almánzar, conhecida profissionalmente como Cardi B, é uma rapper, compositora, atriz e personalidade de televisão americana.",
+        link: "https://pt.wikipedia.org/wiki/Cardi_B",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRsnv-JXnNDhGXqXfx9moOIHKQSyllMMPwzg&s",
+        tags: "mulher negro rap"
+    },
+    {
+        titulo: "Post Malone",
+        descricao: "Austin Richard Post, conhecido profissionalmente como Post Malone, é um cantor, rapper, compositor, produtor de discos e guitarrista norte-americano.",
+        link: "https://pt.wikipedia.org/wiki/Post_Malone",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkUov8HnNR-QSwlsFpVezGViGOpkv_6smO4g&s",
+        tags: "rap foda tatuado"
+    },
+    {
+        titulo: "DMX",
+        descricao: "Earl Simmons, conhecido como DMX, foi um rapper, compositor e ator americano. DMX já vendeu trinta milhões de álbuns até hoje e é considerado um dos maiores rappers de todos os tempos. Simmons morreu aos 50 anos, após sofrer um ataque de coração causado por uma overdose.",
+        link: "https://pt.wikipedia.org/wiki/DMX",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6Q2DpKBqLvHs33zU6HZzVgMN0x_W_7z8jyw&s",
+        tags: "negro rap morto x"
+    },
+    {
+        titulo: "Wiz Khalifa",
+        descricao: "Cameron Jibril Thomaz, mais conhecido como Wiz Khalifa, é um rapper, cantor, compositor e ator norte-americano. Em 2006, Khalifa lançou seu álbum de estreia, Show and Prove, em 2007 assinou pela Warner Bros Records Sua canção Say Yeah ficou bem colocada nas paradas musicais de 2008, como na Rhythmic Top 40 e na Billboard Rap Songs, recebendo airplay nas rádios.",
+        link: "https://pt.wikipedia.org/wiki/Wiz_Khalifa",
+        imagem: "https://9b16f79ca967fd0708d1-2713572fef44aa49ec323e813b06d2d9.ssl.cf2.rackcdn.com/1140x_a10-7_cTC/Jimmy-Fontaine-jpg-1587050066.jpg",
+        tags: "rap again rico"
+    },
+    {
+        titulo: "Pop Smoke",
+        descricao: "Bashar Barakah Jackson, mais conhecido pelo nome artístico Pop Smoke, foi um rapper, cantor e compositor estadunidense. Ele colaborou com o rapper Travis Scott, entre outros.",
+        link: "https://pt.wikipedia.org/wiki/Pop_Smoke",
+        imagem: "https://media.npr.org/assets/img/2020/02/19/gettyimages-1199920197-266f39a36e90bbb2b8b315d9041093ef7ba4d679.jpg?s=1100&c=85&f=jpeg",
+        tags: "morto tatuado rap smoke"
+    },
+    {
+        titulo: "Chris Brown",
+        descricao: "Christopher Maurice Brown é um cantor, rapper, compositor, dançarino, ator, produtor musical, empreendedor, coreógrafo e grafiteiro norte-americano. É um dos principais artistas do R&B contemporâneo, sendo considerado por muitos como o Rei do R&B e um dos artistas mais influentes do século XXI.",
+        link: "https://pt.wikipedia.org/wiki/Chris_Brown",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdRA4QWyN31Y80gpMtTBbaFR_IVaZC_WfyHg&s",
+        tags: "playboy pop rico pegador rap"
+    },
+    {
+        titulo: "Pitbull",
+        descricao: "Armando Christian Pérez, mais conhecido pelo nome artístico Pitbull é um rapper, cantor, compositor e actor americano de ascendência cubana.",
+        link: "https://pt.wikipedia.org/wiki/Pitbull_(rapper)",
+        imagem: "https://media.cnn.com/api/v1/images/stellar/prod/140619211341-pitbull-rapper-and-artist-1.jpg?q=x_0,y_402,h_1278,w_2273,c_crop/w_1280",
+        tags: "cuba rap pop"
+    },
+    {
+        titulo: "Lil Jon",
+        descricao: "Lil Jon, nome artístico de Jonathan Mortimer Smith, é um rapper, produtor, DJ e ator americano, além de ser fundador da gravadora BME Recordings. Criador do estilo Crunk, que consiste num estilo pesado, variando do Hip Hop, mas possui batidas muito mais dançantes e fortes, que animam a música.",
+        link: "https://pt.wikipedia.org/wiki/Lil_Jon",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTft5ZdwWWUYjathTFyNF-ghpnC3EwVvoU9A&s",
+        tags: "rap town"
+    },
+    {
+        titulo: "Usher",
+        descricao: "Usher Raymond IV, mais conhecido como Usher, é um cantor, dançarino e ator norte-americano. Considerado como um artista influente nos cenários do R&B contemporâneo e da pop, vários meios de comunicação social o descrevem como sendo o 'Rei do R&B'.",
+        link: "https://pt.wikipedia.org/wiki/Usher",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSy8qJDH3yoO_lOhXnopjoo41OYIJfaSNyNQ&s",
+        tags: "yeah rap negro"
+    },
+    {
+        titulo: "Future",
+        descricao: "Nayvadius DeMun Wilburn, nascido Nayvadius D. Wilburn e mais conhecido como Future, é um rapper, cantor, compositor e produtor musical norte-americano. Future é um dos principais pioneiros e influenciadores do gênero trap music.",
+        link: "https://pt.wikipedia.org/wiki/Future_(rapper)",
+        imagem: "https://portalrapmais.com/wp-content/uploads/2022/11/Future-2.jpg",
+        tags: "tatuado rap rico trap"
+    },
+    {
+        titulo: "Pharrell Williams",
+        descricao: "Pharrell Lanscilo Williams também conhecido apenas por Pharrell, é um cantor, compositor, rapper, e produtor musical norte-americano. Williams e Chad Hugo formam a dupla de produção The Neptunes, produzindo música soul, hip hop e música de R&B.",
+        link: "https://pt.wikipedia.org/wiki/Pharrell_Williams",
+        imagem: "https://conteudo.imguol.com.br/c/entretenimento/58/2020/06/25/pharrell-williams-1593098250059_v2_900x506.jpg",
+        tags: "pop negro rap famoso rico"
+    },
+    {
+        titulo: "Ice Spice",
+        descricao: "Isis Naija Gaston, mais conhecida pelo nome artístico Ice Spice, é uma rapper estadunidense. Ela cresceu em Bronx, na cidade de Nova Iorque, e começou sua carreira em 2021, depois de conhecer o produtor musical RiotUSA, enquanto estudava na State University of New York em Purchase.",
+        link: "https://pt.wikipedia.org/wiki/Ice_Spice",
+        imagem: "https://akamai.sscdn.co/uploadfile/letras/fotos/1/c/b/f/1cbfbaf3d620a2571af23963f8899867.jpg",
+        tags: "ruiva rap louca"
+    },
+    {
+        titulo: "Doja Cat",
+        descricao: "Amala Ratna Zandile Dlamini, conhecida profissionalmente como Doja Cat, é uma rapper, cantora, compositora, produtora musical e pintora americana. Nascida e criada em Los Angeles, ela começou a fazer e a lançar música no SoundCloud quando era adolescente.",
+        link: "https://pt.wikipedia.org/wiki/Doja_Cat",
+        imagem: "https://metropolitanafm.com.br/wp-content/uploads/2024/03/img_doja.jpg",
+        tags: "feia rap calva"
+    },
+    {
+        titulo: "Taylor Swift",
+        descricao: "Taylor Alison Swift é uma cantora, compositora, produtora musical, diretora e empresária estadunidense. Conhecida por suas composições narrativas que geralmente concentram-se em sua vida pessoal, ao qual recebem ampla cobertura mediática.",
+        link: "https://pt.wikipedia.org/wiki/Taylor_Swift",
+        imagem: "https://s2-quem.glbimg.com/kfU9Roecl7OYuSoQQ-cgdusal8I=/0x0:1400x950/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_b0f0e84207c948ab8b8777be5a6a4395/internal_photos/bs/2024/G/A/MgCwlMSo6yiM68KEvRGQ/design-sem-nome-2024-09-11t210146.737.jpg",
+        tags: "pop loira famosa nfl"
+    },
+    {
+        titulo: "Bruno Mars",
+        descricao: "Peter Gene Hernandez, mais conhecido pelo nome artístico Bruno Mars, apelidado de 'Novo Rei do Pop', é um cantor, compositor, produtor musical, dançarino e multi-instrumentista americano, nascido e criado no Havaí.",
+        link: "https://pt.wikipedia.org/wiki/Bruno_Mars",
+        imagem: "https://i.scdn.co/image/aa32d6d4ca2467974403518dd3ebfe8831c5ced1",
+        tags: "pop rap famoso playboy rico"
+    },
+    {
+        titulo: "Olivia Rodrigo",
+        descricao: "Olivia Isabel Rodrigo é uma cantora, compositora e atriz filipino-americana. Ela alcançou a fama pela primeira vez como atriz infantil nos programas de televisão da Disney Bizaardvark e High School Musical: The Musical: The Series.",
+        link: "https://pt.wikipedia.org/wiki/Olivia_Rodrigo",
+        imagem: "https://www.hollywoodreporter.com/wp-content/uploads/2022/02/GettyImages-1369736546.jpg?w=1296&h=730&crop=1",
+        tags: "pop morena famosa"
+    },
+    {
+        titulo: "Camila Cabello",
+        descricao: "Karla Camila Cabello Estrabao, mais conhecida como Camila Cabello, é uma cantora, compositora e atriz cubana-mexicana naturalizada norte-americana. Ela ganhou destaque como integrante do grupo feminino Fifth Harmony, formado no reality show The X Factor em 2012, assinando um contrato com as gravadoras Syco e Epic Records. Enquanto ainda fazia parte do grupo, Cabello começou a se estabelecer como artista solo, lançando as colaborações 'I Know What You Did Last Summer', com Shawn Mendes, e a 'Bad Things', com Machine Gun Kelly",
+        link: "https://pt.wikipedia.org/wiki/Camila_Cabello",
+        imagem: "https://s2-quem.glbimg.com/0BavpH55bL_hKzYVY-xhU9LMsXg=/0x0:1400x950/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_b0f0e84207c948ab8b8777be5a6a4395/internal_photos/bs/2024/R/L/qpBfOJR0qvjBxVyMgBng/design-sem-nome-2024-09-11t205529.822.jpg",
+        tags: "morena pop legal"
+    },
+    {
+        titulo: "Machine Gun Kelly (MGK)",
+        descricao: "Colson Baker, mais conhecido por seu nome artístico Machine Gun Kelly ou MGK é um rapper, cantor-compositor, músico e ator norte-americano. O álbum de estreia de Baker, Lace Up, foi lançado em 2012 e foi seguido por quatro subsequentes lançamentos de estúdio.",
+        link: "https://pt.wikipedia.org/wiki/Machine_Gun_Kelly",
+        imagem: "https://media.9news.com/assets/ETONLINE/images/59f3c5d3-0160-417d-8e2f-3b211cf6a737/59f3c5d3-0160-417d-8e2f-3b211cf6a737_1140x641.jpg",
+        tags: "rock tatuado famoso pegador rap"
     }
 ];
