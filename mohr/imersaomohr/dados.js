6ada32a34dd1c19ee24ce950d90019c5039c67e2
Manu Mohr <167103609+manumohrr@users.noreply.github.com>
2024-09-12 11:46:09 -0300

    imersão dois

---
+++ mohr/imersaomohr/dados.js
@@ -0,0 +1,135 @@
+let dados = [
+    {
+        titulo: "Thom",
+        descricao: "Thom é um ser humano gracioso, inteligente, com unhas e cabelos perfeitos. Ele ajuda os coleguinhas e todo mundo gosta dele",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "thom"
+    },
+    {
+        titulo: "Manu Mohr",
+        descricao: "Muito animada, criativa, gente boa, inteligente, louca e divertida",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "mohr"
+    },
+    {
+        titulo: "Nicole",
+        descricao: "Muito gentil, animada quando quer, ama um volei, inteligente e todo mundo gosta dela",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "nick"
+    },
+    {
+        titulo: "Clara Dick",
+        descricao: "Gente boa, desenha bem, criativa, sonolenta e vaidosa",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "clara"
+    },
+    {
+        titulo: "Analice",
+        descricao: "Tem bom gosto musical, divertida, gentil, engraçada e sempre está rindo",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "analice"
+    },
+    {
+        titulo: "Ana Elisa",
+        descricao: "Baixinha",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "ana"
+    },
+    {
+        titulo: "Andrielen",
+        descricao: "Baixinha, barulhenta, briguenta, criativa, fotógrafa",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "andri"
+    },
+    {
+        titulo: "Larissa",
+        descricao: "Criativa, animada, amorosa",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "lari"
+    },
+    {
+        titulo: "Rafa Mitt",
+        descricao: "Insuportável, briguento, divertido, inteligente, brincalhão",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "mitt"
+    },
+    {
+        titulo: "Be Tongo",
+        descricao: "Engraçado, divertido, meio estranho, se esforça quando quer",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "be"
+    },
+    {
+        titulo: "Guilherme Dalke",
+        descricao: "Alto pra caramba, gente boa, legal, divertido",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: ""
+    },
+    {
+        titulo: "Attottao",
+        descricao: "brincalhão, inteligente quando quer, briguento, competitivo, campeão do valorant, gosta de academia, austugo",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "kirst"
+    },
+    {
+        titulo: "Enzo",
+        descricao: "Briguento, engraçado, comilão, gente boa e enche o saco dos professores",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "enzo"
+    },
+    {
+        titulo: "Joaquim",
+        descricao: "Inteligente, generoso, querido, joga bem valorant",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "joaquim"
+    },
+    {
+        titulo: "Johann",
+        descricao: "Querido, atleta, gente boa, divertido, engraçado",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "joaquim"
+    },
+    {
+        titulo: "Luís",
+        descricao: "Inteligente, enche o saco, esportivo, dorminhoco",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "luis"
+    },
+    {
+        titulo: "Lucas",
+        descricao: "Inteligente, bom gosto musical, fan da lana, brat",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "joaquim"
+    },
+    {
+        titulo: "Backes",
+        descricao: "Dorme a aula inteira",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "backes"
+    },
+    {
+        titulo: "",
+        descricao: "Inteligente, generoso, querido, joga bem valorant",
+        link: "https://senacscs.github.io/t4/",
+        imagem: "",
+        tags: "joaquim"
+    }
+];
