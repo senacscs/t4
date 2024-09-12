0a1c0e0a18602b9e51d32b89dc18f6f736aca168
lariii rg <167103378+lariiirg@users.noreply.github.com>
2024-09-12 11:37:58 -0300

    mandando nova index e site da imerção

---
+++ larii/imercao/dados.js
@@ -0,0 +1,64 @@
+let dados = [
+    {
+        titulo: "Once Upon A Time",
+        descricao: "O MBTI dessa série é INFP.",
+        link: "https://www.personality-database.com/profile?pid=2&cid=2&sub_cat_id=123",
+        imagem: "ouat.jpg",
+        tags: " A melhor série de contos de fadas ja feita, ouat. séries "
+    },
+    {
+        titulo: "Grey's Anatomy",
+        descricao: "O MBTI dessa série é ESFP.",
+        link: "https://www.personality-database.com/profile?pid=2&cid=2&sub_cat_id=215",
+        imagem: "greys.jpg",
+        tags: " uma série de médicos, 21 temporadas. séries "
+    },
+    {
+        titulo: "Friends",
+        descricao: "O MBTI dessa série é INTJ.",
+        link: "https://www.personality-database.com/profile?pid=2&cid=2&sub_cat_id=2",
+        imagem: "friends.jpg",
+        tags: " 6 amigos, sitcom mais famosa dos anos 90 e 2000. séries "
+    },
+    {
+        titulo: "WandaVision",
+        descricao: "O MBTI dessa série é INFP.",
+        link: "https://www.personality-database.com/profile?pid=2&cid=27&sub_cat_id=15446",
+        imagem: "wanda.jpg",
+        tags: " A melhor série da Marvel. séries "
+    },
+    {
+        titulo: "Gilmore Girls",
+        descricao: "O MBTI dessa série é ENFP.",
+        link: "https://www.personality-database.com/profile?pid=2&cid=2&sub_cat_id=10",
+        imagem: "gilmore.jpg",
+        tags: " Tal mãe tal filha. séries "
+    },
+    {
+        titulo: "The Vampire Diaries",
+        descricao: "O MBTI dessa série é ESFP.",
+        link: "https://www.personality-database.com/profile?pid=2&cid=2&sub_cat_id=317",
+        imagem: "tvd.jpg",
+        tags: " A série de vampiros queridinha dos fãs. séries  "
+    },
+    {
+        titulo: "Daisy Jones & The Six",
+        descricao: "O MBTI dessa série é ESFP.",
+        link: "https://www.personality-database.com/profile?pid=2&cid=12&sub_cat_id=14849",
+        imagem: "daisyj.jpg",
+        tags: " Série baseada no Best Seller. Melhor banda de Rock. séries  "
+    },
+    {
+        titulo: "New Girl",
+        descricao: "O MBTI dessa série é ESFP.",
+        link: "https://www.personality-database.com/profile?pid=2&cid=2&sub_cat_id=318",
+        imagem: "newgirl.jpg",
+        tags: " Sitcom muito subestimada, pois ela é incrivel e super engraçada. séries "
+    },
+
+
+
+
+
+
+];
