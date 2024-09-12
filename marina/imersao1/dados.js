9547de1508f886b73fe6a16fdaf1f103dbd9552d
MarinaHermany <167104095+MarinaHermany@users.noreply.github.com>
2024-09-12 11:40:38 -0300

    imersao 1

---
+++ marina/imersao1/dados.js
@@ -0,0 +1,35 @@
+let dados = [
+    {
+        titulo: "Lisa",
+        descricao: "Lisa é uma das integrantes do grupo de K-pop BLACKPINK. Seu nome completo é Lalisa Manoban, e ela nasceu em 27 de março de 1997, na Tailândia. Lisa é conhecida por suas habilidades excepcionais em dança e rap, além de seu carisma no palco. Ela é a principal dançarina e uma das rappers do grupo. Além de suas atividades com o BLACKPINK, Lisa também lançou músicas solo e é uma figura influente na moda.",
+        link: "https://pt.wikipedia.org/wiki/Lisa_(rapper)",
+        imagem: "lisa.jpg",
+        tags: "lisa rapper dancer",
+        fundo: "#fdfd00b7"
+    },
+    {
+        titulo: "Rosé",
+        descricao: "Rosé é uma das integrantes do grupo de K-pop BLACKPINK. Seu nome completo é Roseanne Park, e ela nasceu em 11 de fevereiro de 1997, na Nova Zelândia, mas cresceu na Austrália. Rosé é conhecida por sua voz única e poderosa, sendo a vocalista principal do grupo. Além de suas atividades com o BLACKPINK, ela também lançou músicas solo e é reconhecida por seu talento musical e presença de palco.",
+        link: "https://pt.wikipedia.org/wiki/Ros%C3%A9_(cantora)",
+        imagem: "rosé.jpg",
+        tags: "rose rosé vocal",
+        fundo: "#383837a1"
+
+    },
+    {
+        titulo: "Jisoo",
+        descricao: "Jisoo é uma das integrantes do grupo de K-pop BLACKPINK. Seu nome completo é Kim Ji-soo, e ela nasceu em 3 de janeiro de 1995, na Coreia do Sul. Jisoo é conhecida por sua beleza, carisma e habilidades vocais. Ela é a vocalista principal e também atua como visual do grupo, o que significa que ela frequentemente representa a imagem do BLACKPINK. Além de suas atividades musicais, Jisoo também tem experiência como atriz e modelo.",
+        link: "https://pt.wikipedia.org/wiki/Jisoo",
+        imagem: "jisoo.jpg",
+        tags: "jisso jisoo jiso vocal soya ",
+        fundo: "#F7ED7B"
+    },
+    {
+        titulo: "Jennie",
+        descricao: "Jennie é uma das integrantes do grupo de K-pop BLACKPINK. Seu nome completo é Jennie Kim, e ela nasceu em 16 de janeiro de 1996, na Coreia do Sul. Jennie é conhecida por suas habilidades em rap e canto, além de seu estilo e presença de palco marcantes. Ela é a rapper principal e uma das vocalistas do grupo. Jennie também lançou músicas solo e é uma figura influente na moda, frequentemente colaborando com grandes marcas.",
+        link: "https://pt.wikipedia.org/wiki/Jennie",
+        imagem: "jennie.jpg",
+        tags: "jennie jenie raper nini ninni itgirl ",
+        fundo: "#000000"
+    }
+];
