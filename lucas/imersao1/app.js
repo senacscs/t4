b9444927b09ee0567ea5c1403924d64d2420bf4f
luscaszs <167103492+luscaszs@users.noreply.github.com>
2024-09-12 10:44:32 -0300

    mudei tudo

--- lucas/imersao1/app.js
+++ lucas/imersao1/app.js
@@ -1,10 +1,10 @@
 function pesquisar() {
-   
+    // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
     let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-
+    // se campoPesquisa for uma string sem nada
     if (!campoPesquisa) {
         section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
         return 
@@ -12,22 +12,23 @@ function pesquisar() {
 
     campoPesquisa = campoPesquisa.toLowerCase()
 
-   
+    // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
     let titulo = ""; 
     let descricao = "";
     let tags = "";
 
-    
+    // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
         titulo = dado.titulo.toLowerCase()
         descricao = dado.descricao.toLowerCase()
         tags = dado.tags.toLowerCase()
-        
+        // se titulo includes campoPesquisa
         if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
-           
+            // cria um novo elemento
             resultados += `
             <div class="item-resultado">
+            <img src=${dado.imagem} alt=${dado.titulo}>
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
                 </h2>
@@ -42,6 +43,6 @@ function pesquisar() {
         resultados = "<p>Nada foi encontrado</p>"
     }
 
-    
+    // Atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
 }
--- lucas/imersao1/dados.js
+++ lucas/imersao1/dados.js
@@ -7,69 +7,79 @@ let dados = [
         titulo: "A.K.A Lizzy Grant (2010)",
         descricao: "Este é o álbum de estreia de Lana, lançado quando ela ainda era conhecida como Lizzy Grant. Ele apresenta um som mais indie pop e baroque pop com letras que exploram temas de amor e desilusão.",
         link: "https://www.letras.mus.br/lana-del-rey/discografia/lana-del-ray-aka-lizzy-grant-2010/",
-        tags: "flop"
+        tags: "flop",
+        imagem: "imagens/aka.jpg",
     },
 
     {
         titulo: "Born to Die (2012)",
         descricao: "Este álbum é o que trouxe Lana Del Rey ao grande público. Com uma mistura de pop, baroque pop e hip-hop, o álbum inclui sucessos como 'Video Games' e 'Born to Die'. A produção é rica em orquestrações e temas melancólicos.",
         link: "https://open.spotify.com/intl-pt/album/4vXt6IpMcSnqonljffWlMI",
-        tags: "bom"
+        tags: "bom",
+        imagem: "imagens/born.jpg",
     },
 
     {
         titulo: "Paradise (2012)",
         descricao: "Um EP que segue a linha do Born to Die, mas com um foco ainda maior em uma atmosfera cinematográfica e sombria. Inclui faixas como 'Ride' e 'Blue Jeans'.",
         link: "https://open.spotify.com/intl-pt/album/5VoeRuTrGhTbKelUfwymwu",
-        tags: "miley flop"
+        tags: "miley flop",
+        imagem: "imagens/paradise.jpeg",
     },
 
     {
         titulo: "Ultraviolence (2014)",
         descricao: "O álbum explora um som mais sombrio e psicodélico, com uma produção que destaca uma colaboração com o produtor Dan Auerbach do The Black Keys. Tem uma abordagem mais raw e crua em comparação com os lançamentos anteriores.",
         link: "https://open.spotify.com/intl-pt/album/1ORxRsK3MrSLvh7VQTF01F",
-        tags: "aaaa"
+        tags: "aaaa",
+        imagem: "imagens/eb300e3afe470b74b4384b169b96dd56.webp",
     },
 
     {
         titulo: "Honeymoon (2015)",
         descricao: "Este álbum mantém o estilo cinematográfico e melancólico de Lana, mas com uma sonoridade mais suave e sonhadora. Faixas como 'High by the Beach' e 'Music to Watch Boys To' são marcantes.",
         link: "https://open.spotify.com/intl-pt/album/2DpEBrjCur1ythIZ10gJWw",
-        tags: "llll"
+        tags: "llll",
+        imagem: "imagens/honeymoon.jpg",
     },
 
     {
         titulo: "Lust for Life (2017)",
         descricao: "Uma abordagem mais otimista e influenciada pelo pop dos anos 60, com colaborações de artistas como The Weeknd. O álbum tem um tom mais leve e inclui hits como 'Love' e 'Lust for Life'.",
         link: "https://open.spotify.com/intl-pt/album/7xYiTrbTL57QO0bb4hXIKo",
-        tags: "dddd"
+        tags: "dddd",
+        imagem: "imagens/lust.png"
     },
 
     {
         titulo: "Norman Fucking Rockwell (2019)",
         descricao: "Este é um dos álbuns mais aclamados da carreira de Lana. Ele combina elementos de folk e rock com uma escrita lírica profunda e introspectiva. Faixas notáveis incluem 'Doin’ Time' e 'Venice Bitch'.",
         link: "https://open.spotify.com/intl-pt/album/5XpEKORZ4y6OrCZSKsi46A",
-        tags: "kkkkkk"
+        tags: "kkkkkk",
+        imagem: "imagens/nfr.png"
     },
 
     {
         titulo: "Chemtrails Over the Country Club (2021)",
         descricao: "O álbum segue uma linha mais folk e acústica, com uma sensação mais íntima e pessoal. Inclui faixas como 'White Dress' e 'Chemtrails over the Country Club'.",
         link: "https://open.spotify.com/intl-pt/album/6QeosPQpJckkW0Obir5RT8",
-        tags: "ppppp"
+        tags: "ppppp",
+        imagem: "imagens/chem.png"
     },
 
     {
         titulo: "Blue Banisters (2021)",
         descricao: "O álbum segue uma linha mais folk e acústica, com uma sensação mais íntima e pessoal. Inclui faixas como 'White Dress' e 'Chemtrails over the Country Club'.",
         link: "https://open.spotify.com/intl-pt/album/2wwCc6fcyhp1tfY3J6Javr",
-        tags: "oooo"
+        tags: "oooo",
+        imagem: "imagens/blue.jpg"
     },
 
     {
         titulo: "Did You Know That There's a Tunnel Under Ocean Blvd (2023)",
         descricao: "Este álbum mantém o estilo introspectivo e cinematográfico, mas explora novos territórios musicais e líricos. É um trabalho ambicioso e expansivo que continua a refletir o estilo distintivo de Lana Del Rey.",
         link: "https://open.spotify.com/intl-pt/album/5HOHne1wzItQlIYmLXLYfZ",
-        tags: "vvv"
+        tags: "vvv",
+        imagem: "imagens/did.jpeg"
     }
 ];
