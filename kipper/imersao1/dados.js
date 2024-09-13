cdf7b35bc90347040a417d8e2de3fe7094ee5e82
gjohannk <167103316+gjohannk@users.noreply.github.com>
2024-09-13 09:16:31 -0300

    mais cantores

--- kipper/imersao1/dados.js
+++ kipper/imersao1/dados.js
@@ -4,81 +4,116 @@ let dados = [
         descricao: "Matuê - O Rei da Sofrência Trap. Matheus Brasileiro Aguiar (Fortaleza, 11 de outubro de 1993), mais conhecido pelo nome artístico Matuê, é um rapper, cantor, compositor e guitarrista brasileiro. Ficou conhecido com o single Anos Luz, lançado em 2017 e pelo álbum Máquina do Tempo lançado em 2020. É considerado um dos símbolos do trap brasileiro.",
         link: "https://pt.wikipedia.org/wiki/Matuê",
         imagem: "matue.png",
-        tags: "trap rap matue 30praum"
+        tags: "hip hop trap reggae pop rap musica psicodelica 30praum"
     },
     {
         titulo: "Teto",
         descricao: "Teto - O Fenômeno da Internet. Clériton Sávio Santos Silva (Jacobina, 19 de outubro de 2001) mais conhecido pelo seu nome artístico Teto é um rapper, trapper, cantor e compositor brasileiro que ficou conhecido por meio das prévias de suas músicas que foram lançadas não oficialmente em vários canais do YouTube, alcançando milhões de visualizações e ganhando fama em redes sociais como o TikTok e Instagram.",
         link: "https://pt.wikipedia.org/wiki/Teto_(rapper)",
         imagem: "teto.jpg",
-        tags: "trap rap teto 30praum"
+        tags: "trap hip hop teto 30praum"
     },
     {
         titulo: "Wiu",
         descricao: "WIU - A Voz da Rua. Vinicius William Sales de Lima, (Fortaleza, 16 de fevereiro de 2002) mais conhecido pelo nome artístico WIU, é um rapper, cantor, compositor e produtor musical brasileiro. Ficou conhecido com os singles Felina, Horas Iguais, Coração de Gelo e pelo álbum Manual de Como Amar Errado, lançado em 2022.",
         link: "https://pt.wikipedia.org/wiki/WIU",
        imagem: "wiu.png",
-        tags: "trap rap wiu 30praum"
+        tags: "eletronica funk brasileiro hip hop trap wiu 30praum"
     },
     {
         titulo: "Veigh",
         descricao: "Thiago Veigh da Silva (Itapevi, 12 de setembro de 2000), mais conhecido por seu nome artístico Veigh, é um rapper, cantor e compositor brasileiro. em 2023 lançou seu álbum Prédios Deluxe alcançou recorde na estreia ficando em top global de streaming.",
         link: "https://pt.wikipedia.org/wiki/Veigh",
         imagem: "veigh.png",
-        tags: "trap hip-hop Veigh supernova super nova"
+        tags: "hip hop rap funk brasileiro pop eletronica musica regional brasileira veigh supernova super nova"
     },
     {
         titulo: "TZ da Coronel",
         descricao:"Matheus de Araújo Santos (Cabo Frio, Rio de Janeiro, 6 de julho de 2001) Também conhecido como TZ da Coronel, é um dos nomes em ascensão no trap nacional. Oriundo do Rio de Janeiro, ele se destaca pelo seu estilo cru e autêntico, com letras que retratam a realidade da periferia e suas vivências pessoais. Sua música tem forte apelo nas plataformas digitais, conquistando uma legião de fãs.",
         link: "https://pt.wikipedia.org/wiki/TZ_da_Coronel",
         imagem: "tz.png",
-        tags: "trap rap TZ TZ da coronel"
+        tags: "hip hop rap trap TZ da coronel"
     },
     {
         titulo: "Yunk Vino",
-        descricao: "Marcos Vinícius Albano(Carapicuíba, São Paulo, 7 de junho de 1997) Yunk Vino é um dos representantes da nova geração do trap brasileiro, conhecido por seu estilo irreverente e suas músicas que combinam beats modernos com letras que abordam festas, estilo de vida e conquistas. Sua personalidade vibrante e flow único o destacam na cena musical urbana do Brasil.",
+        descricao: "Marcos Vinícius Albano (Carapicuíba, São Paulo, 7 de junho de 1997) Yunk Vino é um dos representantes da nova geração do trap brasileiro, conhecido por seu estilo irreverente e suas músicas que combinam beats modernos com letras que abordam festas, estilo de vida e conquistas. Sua personalidade vibrante e flow único o destacam na cena musical urbana do Brasil.",
         link: "https://www.last.fm/pt/music/Yunk+Vino/+wiki",
         imagem:"yunkvino.png",
-        tags: "rap trap yunk vino"
+        tags: "hip hop rap trap yunk vino"
     },
     {
         titulo: "Derek",
-        descricao: "Derek Luccas da Silva(São Paulo, 13 de março de 1998) Derek é um dos maiores expoentes do Trap Nacional. Fundador da Label TRAP THE FATO e um dos principais líderes do coletivo Recayd Mob, juntos somam mais de 140 milhões de views em um único clipe no Youtube.",
+        descricao: "Derek Luccas da Silva (São Paulo, 13 de março de 1998) Derek é um dos maiores expoentes do Trap Nacional. Fundador da Label TRAP THE FATO e um dos principais líderes do coletivo Recayd Mob, juntos somam mais de 140 milhões de views em um único clipe no Youtube.",
         link: "https://genius.com/artists/Aka-rasta",
         imagem:"derek.png",
-        tags: "rap trap Derek"
+        tags: "hip hop rap trap Derek"
     },
     {
         titulo: "Aka Rasta",
         descricao: "Guilherme Ramos, ou Aka Rasta, é um artista original de Curitiba e criador do coletivo Astro Gang. Seu primeiro trabalho independente foi em 2017, com a Astronauta: Mixtape. Lançada sob o selo Astro Gang.",
         link: "https://genius.com/artists/Aka-rasta",
         imagem:"akarasta.png",
-        tags: "rap trap Aka Rasta"
+        tags: "hip hop rap pop r&b soul reggae funk brasileiro eletronica musica regional brasileira trap Aka Rasta"
     },    {
-        titulo: "Matheus Barros(Teteu)",
+        titulo: "Matheus Barros (Teteu)",
         descricao: "Matheus Barros, tem um ranço enorme pelo mano chico, e seu coração pertence a varias mulheres, as martina, as maria, as gabriela. Seu passatempo predileto é resenhar com os guris e pensar nas suas amadas.",
         imagem:"teteu.png",
         tags: "teteu"
     },
     {
         titulo: "Ryu, The Runner",
-        descricao: "Ryan Gonçalves(Vitória, Bahia, 22 de novembro de 2004). Ryu foi pra São Paulo ainda novo, tendo muito contato com o Rap por influencia de seu Pai e seu tio, sempre consumiu muito desde muito cedo, isso fez com que despertasse o interesse dele pela música, pelo rap e pelos artistas da epoca.",
+        descricao: "Ryan Gonçalves (Vitória, Bahia, 22 de novembro de 2004). Ryu foi pra São Paulo ainda novo, tendo muito contato com o Rap por influencia de seu Pai e seu tio, sempre consumiu muito desde muito cedo, isso fez com que despertasse o interesse dele pela música, pelo rap e pelos artistas da epoca.",
         link: "https://www.last.fm/pt/music/Ryu,+the+Runner/+wiki",
         imagem:"ryu.png",
-        tags: "rap trap ryu the runner "
+        tags: "hip hop rap trap ryu the runner "
     },
     {
         titulo: "MC Igu",
         descricao: "Igor Kuwahara, conhecido artisticamente como Mc Igu, é um rapper brasileiro que possui raízes japonesas. Igu mistura diferentes estilos e influências nas suas composições, trazendo uma roupagem totalmente única para o trap que vem se popularizando no Brasil.",
         link: "https://aminoapps.com/c/rapislife/page/blog/mc-igu-biografia/zY4N_w8Uxu8MG5ePmVob0ZoBrMoQnZPnWz",
         imagem:"igu.png",
-        tags: "rap trap mc igu"
+        tags: "hip hop gospel eletronica pop rap trap mc igu"
     },
     {
         titulo: "Nagalli",
-        descricao: "André Nagalli(Piracicaba, São Paulo, 21 de dezembro de 1995). André Nagalli era apenas uma criança, em Piracicaba (SP), quando começou a tocar bateria. Ganhou um disco do Charlie Brown Jr., se interessou por rock, teve uma banda na adolescência, mas foi ao ganhar um programa de produção musical do pai que descobriu um talento. No dia seguinte, já tinha composto 20 beats, as batidas que são a base para a criação de músicas de rap e trap. Não demorou e ele estava improvisando um estúdio em casa para produzir fenômenos do rap e trap nacional.",
+        descricao: "André Nagalli (Piracicaba, São Paulo, 21 de dezembro de 1995). Quando criança tocava bateria. Ganhou um disco do CBJR., se interessou por rock, teve uma banda na adolescência, mas foi ao ganhar um programa de produção musical do pai que descobriu um talento. No dia seguinte, já tinha composto 20 beats, as batidas que são a base para a criação de músicas de rap e trap. Não demorou e ele estava improvisando um estúdio em casa para produzir fenômenos do rap e trap nacional.",
         link: "https://g1.globo.com/sp/piracicaba-regiao/noticia/2024/05/18/quem-e-nagalli-fenomeno-do-rap-que-emplacou-album-na-lista-de-estreias-mais-ouvidas-do-mundo.ghtml",
         imagem:"nagalli.png",
-        tags: "rap trap nagalli"
+        tags: "hip hop rap trap nagalli"
+    },
+    {
+        titulo: "Raffa Moreira",
+        descricao: "Rafael Fernando Moreira (Guarulhos, 10 de fevereiro de 1988), mais conhecido pelo nome artístico de Raffa Moreira ou BC Raff, é um rapper e produtor musical brasileiro. Ficou conhecido com o single Bro, lançado em outubro de 2017, e com sua participação em Poetas no Topo 2. É considerado o principal pioneiro do trap no Brasil, e um dos principais artistas da cena do trap nacional.",
+        link: "https://pt.wikipedia.org/wiki/Raffa_Moreira",
+        imagem:"Raffamoreira.png",
+        tags: "hip hop rap trap raffa moreira rafael"
+    },
+    {
+        titulo: "L7NNON ",
+        descricao: "Lennon dos Santos Barbosa Frassetti (Rio de Janeiro, 30 de março de 1994), mais conhecido artisticamente pelo seu prenome estilizado L7nnon, é um rapper e compositor brasileiro. Ficou conhecido nacionalmente e internacionalmente pelas canções Freio da Blazer e Perdição",
+        link: "https://pt.wikipedia.org/wiki/L7nnon",
+        imagem:"l7.png",
+        tags: "hip hop funk brasileiro rap trap l7 l7nnon lennon"
+    },
+    {
+        titulo: "Xamã",
+        descricao: "Geizon Carlos da Cruz Fernandes (Rio de Janeiro, 30 de outubro de 1989), mais conhecido pelo nome artístico Xamã, é um rapper, poeta, cantor e ator brasileiro. Em 2016 começou a carreira musical nas rimas. O cantor tem quatro álbuns, Pecado Capital (2018), O Iluminado (2019), Zodíaco (2020) e O Último Romântico Online (2023)",
+        link: "https://pt.wikipedia.org/wiki/Xamã_(rapper)",
+        imagem:"xama.png",
+        tags: "eletronica forro trap xama xamã"
+    },
+    {
+        titulo: "Sidoka",
+        descricao: "Nicolas Paolinelli Gino (Belo Horizonte, 19 de junho de 1998), mais conhecido pelo nome artístico Sidoka, é um rapper e compositor brasileiro. O artista é considerado um expoente do trap no Brasil, tendo ganhado o prêmio MTV Millenial Awards 2021, na categoria Trap na cena e participado do documentário Música pelo Brasil: Trap.",
+        link: "https://pt.wikipedia.org/wiki/Sidoka",
+        imagem:"sidoka.png",
+        tags: "hip hop funk brasileiro pop eletronica rap trap sidoka"
+    },
+    {
+        titulo: "Djonga",
+        descricao: "Gustavo Pereira Marques (Belo Horizonte, 4 de junho de 1994), mais conhecido pelo nome artístico Djonga, é um rapper, escritor e compositor brasileiro. Considerado um dos nomes mais influentes do rap na atualidade, o artista chama a atenção por suas letras diretas e agressivas, com fortes críticas sociais.",
+        link: "https://pt.wikipedia.org/wiki/Djonga",
+        imagem:"djonga.png",
+        tags: "hip hop rap trap Djonga"
     },
 ];
