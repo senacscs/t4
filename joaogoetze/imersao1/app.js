50ceda170bab54dbd6cc61888a6eeb12b1f0334b
joaogoetzedasilva <167103392+joaogoetzedasilva@users.noreply.github.com>
2024-09-16 07:38:59 -0300

    atualiznado o imersao 1

--- joaogoetze/imersao1/app.js
+++ joaogoetze/imersao1/app.js
@@ -6,7 +6,7 @@ function pesquisar() {
 
     // se campoPesquisa for uma string sem nada
     if (!campoPesquisa) {
-        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte. Se quer ver todos digite -todos- </p>";
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar a raça do gato ou só gato </p>";
         return;
     }
 
@@ -14,34 +14,30 @@ function pesquisar() {
 
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
-    let titulo = ""; 
-    let descricao = "";
-    let tags = "";
-    let foto = "";
 
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-        titulo = dado.titulo.toLowerCase();
-        descricao = dado.descricao.toLowerCase();
-        tags = dado.tags.toLowerCase();
-        // se titulo includes campoPesquisa
-        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        let titulo = dado.titulo ? dado.titulo.toLowerCase() : "";
+        let descricao = dado.descricao ? dado.descricao.toLowerCase() : "";
+        let tags = dado.tags ? dado.tags.toLowerCase() : "";
+
+        // se tags includes campoPesquisa
+        if (tags.includes(campoPesquisa)) {
             // cria um novo elemento
             resultados += `
             <div class="item-resultado">
                 <h2>
-                    <a href="#" target="_blank">${dado.titulo}</a>
+                <a href="${dado.link}" target="_blank">${dado.titulo}</a>
                 </h2>
-                <img src=${dado.foto}>
+                <img src="${dado.foto}" alt="Imagem de ${dado.titulo}"> 
                 <p class="descricao-meta">${dado.descricao}</p>
-                <a href=${dado.link} target="_blank">Mais informações</a>
-            </div>
-        `;
+                <p><a href="${dado.link}" target="_blank">Mais informações</a></p>
+            </div>`;
         }
     }
 
     if (!resultados) {
-        resultados = "<p>Nada foi encontrado ou voce digitou errado</p>";
+        resultados = "<p>Nada foi encontrado ou você digitou errado</p>";
     }
 
     // Atribui os resultados gerados à seção HTML
--- joaogoetze/imersao1/dados.js
+++ joaogoetze/imersao1/dados.js
@@ -4,21 +4,21 @@ let dados = [
         descricao: "Conhecido por seus olhos azuis intensos e pelagem clara com extremidades escuras. São elegantes e têm um temperamento imprevisível, podendo ser tanto tranquilos quanto ativos.",
         link: "https://pt.wikipedia.org/wiki/Siamês",
         tags: "Gato Siamês Olhos Azuis Pelagem Clara Tranquilo Ativo",
-        foto: "OIP.jpg",
+        foto: "siames.jpeg",
     },
     {
         titulo: "Persa",
         descricao: "Famoso por sua pelagem longa e focinho achatado. São gatos tranquilos e amáveis, ideais para quem tem outros pets ou crianças.",
         link: "https://pt.wikipedia.org/wiki/Persa",
         tags: "Gato Persa Pelagem Longa Focinho Achatado Tranquilo Amável",
-        foto: "baixados.jpg",
+        foto: "https://th.bing.com/th/id/OIP.DpQgAM1s8e0qGrdr7Q-iDQHaE6?w=310&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
     },
     {
         titulo: "Maine Coon",
         descricao: "Uma das maiores raças de gatos domésticos. São conhecidos por sua pelagem densa e personalidade amigável. São ótimos companheiros e se dão bem com outros animais.",
         link: "https://pt.wikipedia.org/wiki/Maine_Coon",
         tags: "Gato Maine Coon Pelagem Densa Amigável Companheiro",
-        foto: "baixados (1).jpg",
+        foto: "https://th.bing.com/th/id/OIP.y_R6Re3yyG24LoqFbIhJ_gHaGW?w=233&h=199&c=7&r=0&o=5&dpr=1.5&pid=1.7",
     },
     {
         titulo: "Scottish Fold",
@@ -28,31 +28,52 @@ let dados = [
         foto: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Scottish_Fold_-_CFF_cat_show_Heinola_2008-05-03_IMG_7882.JPG/300px-Scottish_Fold_-_CFF_cat_show_Heinola_2008-05-03_IMG_7882.JPG"
     },
     {
-        titulo: "Sphynx",
+        tituloo: "Sphynx",
         descricao: "Conhecido por sua falta de pelo, o que lhe dá uma aparência única. São gatos muito afetuosos e adoram a companhia humana.",
         link: "https://pt.wikipedia.org/wiki/Sphynx",
         tags: "Gato Sphynx Sem Pelo Afetuoso Companhia Humana",
-        foto: "OIP (1).jpg",
+        foto: "https://th.bing.com/th/id/OIP.N41EYGItXthtrg9r6MmR6QHaEo?w=315&h=196&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+    },
+    {
+        "titulo": "Gato de Botas",
+        "descricao": "Famoso personagem de contos de fadas e filmes, conhecido por sua inteligência, astúcia e botas icônicas. Ele é um gato aventureiro e carismático, frequentemente ajudando seus amigos com sua esperteza.",
+        "link": "https://pt.wikipedia.org/wiki/Gato_de_Botas",
+        "tags": "Botas",
+        "foto": "https://th.bing.com/th/id/OIP.CPAnFHs1hFS-xjloh0KLfwHaDp?rs=1&pid=ImgDetMain",
     },
     {
         titulo: "Bengal",
         descricao: "Tem uma aparência selvagem com manchas que lembram um leopardo. São ativos e inteligentes, adoram brincar e explorar.",
         link: "https://pt.wikipedia.org/wiki/Bengal",
         tags: "Gato Bengal Aparência Selvagem Manchas Ativo Inteligente",
-        foto: "OIP (2).jpg"
+        foto: "Bengal.jpeg"
     },
     {
         titulo: "Ragdoll",
         descricao: "Conhecido por sua natureza dócil e pelagem macia. Eles tendem a relaxar completamente quando são pegos no colo, daí o nome 'Ragdoll' (boneca de pano).",
         link: "https://pt.wikipedia.org/wiki/Ragdoll",
         tags: "Gato Ragdoll Dócil Pelagem Macia Relaxado",
-        foto: "OIP (3).jpg"
+        foto: "Ragdoll.jpeg",
     },
     {
         titulo: "Devon Rex",
         descricao: "Tem uma pelagem curta e encaracolada e grandes orelhas. São gatos brincalhões e adoram a companhia de seus tutores.",
         link: "https://pt.wikipedia.org/wiki/Devon_Rex",
         tags: "Gato Devon Rex Pelagem Curta Encaracolada Brincalhão Companhia",
-        foto: "OIP (4).jpg"
+        foto: "https://cattime.com/wp-content/uploads/sites/14/2011/12/GettyImages-611084284-e1688357436130.jpg?w=1024",
+    },
+    {
+        titulo: "British Shorthair",
+        descricao: "Conhecido por sua pelagem densa e rosto arredondado. São gatos tranquilos e afetuosos, ideais para a vida em apartamento.",
+        link: "https://pt.wikipedia.org/wiki/British_Shorthair",
+        tags: "Gato British Shorthair Pelagem Densa Rosto Arredondado Tranquilo Afetuoso",
+        foto: "british-shorthair-750x419.jpg",
+    },
+    {
+        titulo: "Abyssinian",
+        descricao: "Uma das raças mais antigas, conhecida por sua pelagem curta e aparência elegante. São gatos ativos e curiosos.",
+        link: "https://pt.wikipedia.org/wiki/Abyssinian",
+        tags: "Gato Abyssinian Pelagem Curta Aparência Elegante Ativo Curioso",
+        foto: "OIP.jpg",
     }
-];
+]
