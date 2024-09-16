26155de08bec8965734728431b56bf55252246c6
Ana Sprenger <167103067+anasprenger@users.noreply.github.com>
2024-09-16 11:43:07 -0300

    nova index (e um aleluia para dar certo

---
+++ elisa/efeitos.js
@@ -0,0 +1,30 @@
+const intro = document.querySelector('.intro');
+const title = document.querySelector('.intro h1');
+
+// Evento de clique no h1 para ocultar a tela de introdução
+title.addEventListener('click', () => {
+    intro.classList.add('hidden');
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector('.interactive');
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+        curX += (tgX - curX) / 20;
+        curY += (tgY - curY) / 20;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(() => {
+            move();
+        });
+    }
+
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
+
+    move();
+});
