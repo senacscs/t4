0b434898aa6838fae9c70ea6a324e0fc4cb7b709
enzo <167103155+EnzoBossardi208@users.noreply.github.com>
2024-09-16 22:54:06 -0300

    atualizando imersao

--- enzo/imersao1/app.js
+++ enzo/imersao1/app.js
@@ -2,15 +2,15 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
-    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-    // se campoPesquisa for uma string sem nada
+    // Se campoPesquisa for uma string vazia
     if (!campoPesquisa) {
-        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um jogo ou de algo relacionado.</p>"
-        return 
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um jogo ou de algo relacionado.</p>";
+        return;
     }
 
-    campoPesquisa = campoPesquisa.toLowerCase()
+    campoPesquisa = campoPesquisa.toLowerCase();
 
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
@@ -18,14 +18,9 @@ function pesquisar() {
     let descricao = "";
     let tags = "";
 
-    // Itera sobre cada dado da lista de dados
-    for (let dado of dados) {
-        titulo = dado.titulo.toLowerCase()
-        descricao = dado.descricao.toLowerCase()
-        tags = dado.tags.toLowerCase()
-        // se titulo includes campoPesquisa
-        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
-            // cria um novo elemento
+    // Se a palavra "lista" for digitada, mostra todos os resultados
+    if (campoPesquisa === "lista") {
+        for (let dado of dados) {
             resultados += `
             <div class="item-resultado">
                 <h2>
@@ -36,9 +31,29 @@ function pesquisar() {
             </div>
         `;
         }
+    } else {
+        // Itera sobre cada dado da lista de dados
+        for (let dado of dados) {
+            titulo = dado.titulo.toLowerCase();
+            descricao = dado.descricao.toLowerCase();
+            tags = dado.tags.toLowerCase();
+            // Verifica se o campo de pesquisa está presente no título, descrição ou tags
+            if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+                resultados += `
+                <div class="item-resultado">
+                    <h2>
+                        <a href="#" target="_blank">${dado.titulo}</a>
+                    </h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href=${dado.link} target="_blank">Mais informações</a>
+                </div>
+            `;
+            }
+        }
     }
+
     if (!resultados) {
-        resultados = "<p>Nada foi encontrado</p>"
+        resultados = "<p>Nada foi encontrado</p>";
     }
 
     // Atribui os resultados gerados à seção HTML
--- enzo/imersao1/dados.js
+++ enzo/imersao1/dados.js
@@ -2,25 +2,68 @@ let dados = [
     {
         titulo: "BloodBorne",
         descricao: "Bloodborne se passa na cidade gótica de Yharnam, onde uma antiga e poderosa doença transformou seus habitantes em criaturas bestiais. O jogador assume o papel de um caçador que chega à cidade em busca de um remédio milagroso, mas logo se vê preso em um ciclo de pesadelos e horrores. O mistério da doença está ligado a uma civilização antiga e à busca pelo sangue sagrado, uma substância que promete poder e cura, mas também atrai deuses alienígenas e entidades cósmicas. À medida que o caçador explora Yharnam e seus segredos, descobre verdades perturbadoras sobre a natureza da humanidade, a loucura e o desejo de transcendência.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
-        tags: "Souls like, difícil, BloodBorne, Terror, Ação"
+        link: "https://pt.wikipedia.org/wiki/Bloodborne",
+        tags: "Souls like, difícil, BloodBorne, Terror, Ação, acao, terror, bloodborne, souls, souls like, dificil"
     },
     {
         titulo: "Ghost of Tsushima",
         descricao: "Ghost of Tsushima se passa no final do século XIII, durante a invasão mongol à ilha de Tsushima, no Japão. O jogador controla Jin Sakai, um samurai que sobrevive ao massacre inicial dos mongóis e se vê forçado a adotar táticas furtivas e não convencionais para derrotar o invasor, questionando o rígido código de honra samurai. Conforme Jin luta para libertar sua terra, ele enfrenta não apenas os mongóis liderados pelo astuto Khotun Khan, mas também o conflito interno entre preservar suas tradições e se tornar o (Fantasma), um guerreiro sombrio que utiliza qualquer meio necessário para salvar Tsushima.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        link: "https://pt.wikipedia.org/wiki/Ghost_of_Tsushima",
         tags: "Samurai, Espada, Ghost of Tsushima, ghost of tsushima, japão, mongol"
     },
     {
         titulo: "Minecraft",
         descricao: " Minecraft é vaga e aberta, permitindo que os jogadores criem suas próprias histórias em um mundo vasto e gerado proceduralmente. No entanto, o jogo possui elementos ocultos que sugerem uma narrativa. O jogador começa sozinho em um mundo cheio de biomas diversificados e criaturas, coletando recursos para sobreviver e construir. À medida que explora, descobre templos antigos, ruínas e portais para outras dimensões, como o Nether e o End. No End, o jogador enfrenta o Ender Dragon, uma criatura misteriosa que parece ser o guardião desse reino. A lore implícita sugere que civilizações antigas podem ter habitado essas terras antes, deixando vestígios de sua existência, mas cabe ao jogador interpretar esses mistérios enquanto molda sua própria aventura.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        link: "https://pt.wikipedia.org/wiki/Minecraft",
         tags: "bloco, Bloco, minecraft, Minecraft, Exploração"
     },
     {
         titulo: "Skate 3",
         descricao: "Skate 3 é ambientada na cidade fictícia de Port Carverton, onde o jogador assume o papel de um skatista profissional que busca construir sua própria marca de skate. Após um acidente em uma sessão de fotos, o protagonista decide formar uma equipe de skatistas, expandindo seu império no mundo do skate. O jogo é focado no aspecto colaborativo e na criação de uma comunidade, permitindo que o jogador complete desafios, realize manobras icônicas e desenvolva seu próprio estilo enquanto promove a cultura do skate. A cidade de Port Carverton é o cenário perfeito para explorar diferentes terrenos, como parques, praças e ruas, refletindo a liberdade e a expressão do skateboarding.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
-        tags: "Skate, skate, skate 3, Skate 3, manobra, Manobra, "
+        link: "https://pt.wikipedia.org/wiki/Skate_3",
+        tags: "Skate, skate, skate 3, Skate 3, manobra, Manobra, esporte, Esporte "
     },
+    {
+        titulo: "Fifa",
+        descricao: "FIFA é uma série de jogos de futebol que coloca o jogador no papel de gerenciar, jogar e liderar times profissionais do esporte mais popular do mundo. A cada edição, o jogo traz competições internacionais e ligas renomadas, permitindo que o jogador construa sua equipe dos sonhos, treine suas habilidades e dispute campeonatos globais. A franquia é marcada por sua fidelidade à realidade, trazendo jogadores, times e estádios licenciados, o que oferece uma imersão completa no mundo do futebol. O modo carreira coloca o jogador no controle de um clube, tomando decisões estratégicas como transferências, táticas e treinos, visando construir um legado no esporte. Além disso, o modo Ultimate Team permite que os jogadores criem e evoluam suas equipes personalizadas, combinando estrelas do futebol em um cenário competitivo. FIFA é, assim, uma celebração da paixão pelo futebol, permitindo que os jogadores experimentem a emoção dos gramados de forma autêntica e envolvente.",
+        link: "https://pt.wikipedia.org/wiki/Fifa",
+        tags: "Futebol, bola, fifa, Fifa, esporte, Esporte "
+    },
+    {
+        titulo: "Red dead Redemption 2",
+        descricao: "Red Dead Redemption 2 é ambientado no final do século XIX, em um vasto e detalhado mundo aberto que retrata o fim da era dos foras-da-lei no Velho Oeste americano. O jogador assume o papel de Arthur Morgan, um membro veterano da gangue Van der Linde, liderada pelo carismático Dutch Van der Linde. À medida que as autoridades e caçadores de recompensas apertam o cerco, a gangue luta para sobreviver, ao mesmo tempo que lida com conflitos internos e mudanças no cenário social e econômico da época.",
+        link: "https://pt.wikipedia.org/wiki/Red_Dead_Redemption_2",
+        tags: "cavalo, bagual, Cavalo, Bagual, revólver, revolver, Revólver, revolver, tiro, acao, Tiro, Ação, faroeste, Faroeste, "
+    },
+    {
+        titulo: "Red Dead Redemption 1",
+        descricao: "Red Dead Redemption é ambientado em 1911, durante os últimos dias do Velho Oeste, quando a era dos foras-da-lei está chegando ao fim. O jogador controla John Marston, um ex-criminoso que tenta deixar para trás sua vida de crime em troca de um futuro pacífico para sua família. No entanto, o governo dos Estados Unidos o força a caçar antigos membros de sua gangue, sequestrando sua esposa e filho como forma de pressão. Marston embarca em uma jornada por regiões selvagens, desoladas e fronteiriças do oeste, incluindo o México, em busca de seus ex-companheiros.",
+        link: "https://pt.wikipedia.org/wiki/Red_Dead_Redemption_1",
+        tags: "cavalo, bagual, Cavalo, Bagual, revólver, revolver, Revólver, revolver, tiro, acao, Tiro, Ação, faroeste, Faroeste, "
+    },
+    {
+        titulo: "Super Mario Bros",
+        descricao: "Super Mario Bros. é ambientado no Reino dos Cogumelos, um mundo vibrante e repleto de criaturas fantásticas. O jogador assume o papel de Mario, um encanador que, junto com seu irmão Luigi, precisa resgatar a Princesa Peach, que foi raptada pelo vilão Bowser, o rei dos Koopas. Bowser lançou seu exército de criaturas perigosas, como Goombas e Koopa Troopas, para conquistar o reino e aprisionar a princesa em seu castelo. Para salvar Peach e restaurar a paz no Reino dos Cogumelos, Mario deve atravessar diferentes mundos, enfrentando uma série de desafios.",
+        link: "https://pt.wikipedia.org/wiki/Super_Mario_Bros",
+        tags: "Mario, super, super mario, Super Mario, mario, cogumelo, encanador  "
+    },
+    {
+        titulo: "Valorant",
+        descricao: "Valorant é ambientado em um futuro próximo, onde uma misteriosa força conhecida como Radianite gerou pessoas com habilidades extraordinárias, chamadas Radiantes. O jogador assume o papel de um agente de elite, cada um com suas próprias habilidades únicas e histórias de fundo. Esses agentes vêm de diversas partes do mundo, e são recrutados pela organização Valorant para combater ameaças globais, incluindo uma facção oposta que usa a Radianite de forma perigosa. Com uma combinação de tiroteios precisos e o uso estratégico de habilidades especiais, Valorant mistura ação tática com poderes sobrenaturais, exigindo cooperação em equipe e planejamento para dominar os campos de batalha.",
+        link: "https://pt.wikipedia.org/wiki/Valorant",
+        tags: "Valorant, tiro, valorant, Tiro, arma, ação, acao, Acao, Ação, Tiro  "
+    },
+    {
+        titulo: "Counter-Strike 2",
+        descricao: "Counter-Strike 2 é ambientado em um mundo de operações táticas, onde duas facções rivais, os Terroristas e os Contra-Terroristas, lutam pelo controle de territórios e pela execução de missões estratégicas. O jogo se foca em batalhas multijogador intensas, onde os Terroristas tentam plantar bombas ou capturar reféns, enquanto os Contra-Terroristas têm a tarefa de desarmar os explosivos ou resgatar os reféns. Com mecânicas de tiro precisas e uma ênfase no trabalho em equipe e nas táticas, Counter-Strike 2 refina a jogabilidade clássica da série com melhorias gráficas e ajustes técnicos, oferecendo uma experiência competitiva moderna e imersiva.",
+        link: "https://pt.wikipedia.org/wiki/Counter_Strike_2",
+        tags: "Counter Strike, tiro, counter strike, Tiro, arma, ação, acao, Acao, Ação, Tiro, cs, Cs, CS  "
+    },
+    {
+        titulo: "Counter-Strike 1.8",
+        descricao: "Counter-Strike 1.8 é ambientado em um mundo de operações táticas, onde duas facções rivais, os Terroristas e os Contra-Terroristas, lutam pelo controle de territórios e pela execução de missões estratégicas. O jogo se foca em batalhas multijogador intensas, onde os Terroristas tentam plantar bombas ou capturar reféns, enquanto os Contra-Terroristas têm a tarefa de desarmar os explosivos ou resgatar os reféns. Com mecânicas de tiro precisas e uma ênfase no trabalho em equipe e nas táticas, Counter-Strike 2 refina a jogabilidade clássica da série com melhorias gráficas e ajustes técnicos, oferecendo uma experiência competitiva moderna e imersiva.",
+        link: "https://pt.wikipedia.org/wiki/Counter_Strike_1.8",
+        tags: "Counter Strike, tiro, counter strike, Tiro, arma, ação, acao, Acao, Ação, Tiro, cs, Cs, CS  "
+    },
+    
 ];
