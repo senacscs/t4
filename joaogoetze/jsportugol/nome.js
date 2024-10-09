f176d4e89ae0308ef5e894262fc55df43decf029
joaogoetzedasilva <167103392+joaogoetzedasilva@users.noreply.github.com>
2024-10-09 09:21:26 -0300

    fazendo commit do jspotugol

---
+++ joaogoetze/jsportugol/nome.js
@@ -0,0 +1,19 @@
+// programa
+// {
+// 	funcao inicio ()
+// 	{
+// 		cadeia nome
+
+// 		escreva("Digite seu nome: ")
+// 		leia(nome)
+// 	}
+// }
+
+function nome(){
+// 		cadeia nome
+let texto;
+    // 		leia(numero)
+    texto = document.getElementById("texto").value
+// 		escreva("Digite seu nome: ")
+document.write("O seu nome é:",texto)		
+}
