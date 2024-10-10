d15508f8908cbe5cbac2d938fc05e1c8c27091de
Edu <167102925+eduardosoaresdure@users.noreply.github.com>
2024-10-10 08:55:45 -0300

    Colocada a index

---
+++ edu/portugoljs/operacoessimples.js
@@ -0,0 +1,23 @@
+/*programa
+{
+	funcao inicio()
+	{
+		real a, b, soma, sub, mult, div
+		
+		escreva("Digite o primeiro número: ")
+		leia(a)
+
+		escreva("Digite o segundo número: ")
+		leia(b)
+
+		soma = a + b // Soma os dois valores
+		sub  = a - b // Subtrai os dois valores
+		mult = a * b // Multiplica os dois valores
+		div  = a / b // Divide os dois valores
+
+		escreva("\nA soma dos números é igual a: ", soma) 		// Exibe o resultado da soma
+		escreva("\nA subtração dos números é igual a: ", sub) 		// Exibe o resultado da subtração
+		escreva("\nA multiplicação dos números é igual a: ", mult) 	// Exibe o resultado da multiplicação
+		escreva("\nA divisão dos números é igual a: ", div, "\n") 	// Exibe o resultado da divisão
+	} 
+} */
