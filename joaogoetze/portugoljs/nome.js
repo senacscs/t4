d2ff9c4f763a01dd7c7c77375aa1fb06e2b43308
joaogoetzedasilva <167103392+joaogoetzedasilva@users.noreply.github.com>
2024-10-10 09:16:40 -0300

    fazendo comit do "site"  portugol

---
+++ joaogoetze/portugoljs/nome.js
@@ -0,0 +1,20 @@
+// programa
+// {
+// 	funcao inicio ()
+// 	{
+// 		cadeia nome
+
+// 		escreva("Digite seu nome: ")
+// 		leia(nome)
+// 	}
+// }
+
+let campoPesquisa = document.getElementById("texto").value
+function nome(){
+// 		cadeia nome
+let texto;
+    // 		leia(numero)
+    texto = document.getElementById("texto").value
+// 		escreva("Digite seu nome: ")
+document.write("O seu nome é:",texto)		
+}
---
+++ joaogoetze/portugoljs/numerodigitado.js
@@ -0,0 +1,24 @@
+// programa 
+// { 
+// 	funcao inicio () 
+// 	{ 
+// 		inteiro numero
+		
+// 		escreva("Digite um número inteiro: ")
+// 		leia(numero)
+		
+// 		escreva("O número digitado foi: ", numero, "\n")
+// 	} 
+// }
+
+let campoPesquisa = document.getElementById("numero").value
+function numeroDigitado(){
+    // 		inteiro numero
+		let numero
+    // 		leia(numero)
+    numero = document.getElementById("numero").value
+// 		escreva("Digite um número inteiro: ")
+document.write("O numero digitado foi:",numero)		
+}
+
+
