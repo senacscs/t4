659ae216e38379048a9d1aa59b4a437da5d4da37
pedrofa07 <167103014+pedrofa07@users.noreply.github.com>
2024-10-10 09:08:49 -0300

    portugol

---
+++ pedrofa/portugoljs/prioridades.js
@@ -0,0 +1,32 @@
+// /* programa
+// {
+// 	funcao inicio()
+// 	{
+// 		real resultado
+
+// 		// Neste exemplo, a operação de multiplicação (*) será executada primeiro
+// 		resultado = 5.0 + 4.0 * 2.0
+// 		escreva("Operação: 5 + 4 * 2 = ", resultado) 
+
+
+// 		// Neste exemplo, a operação de soma (+) será executada primeiro
+// 		resultado = (5.0 + 4.0) * 2.0
+// 		escreva("\nOperação: (5 + 4) * 2 = ", resultado)		
+
+// 		/*
+// 		 * Neste exemplo, a operação de divisão (/) será executada primeiro, 
+// 		 * seguida pela operação de multiplicação (*). Por último, será 
+// 		 * executada a operação de soma (+)
+// 		 */
+// 		resultado = 1.0 + 2.0 / 3.0 * 4.0 
+// 		escreva("\nOperação: 1 + 2 / 3 * 4 = ", resultado)
+
+// 		/*
+// 		 * Neste exemplo, a operação de soma (+) será executada primeiro, 
+// 		 * seguida pela operação de multiplicação (*). Por último, será 
+// 		 * executada a operação de divisão (/).
+// 		 */
+// 		resultado = (1.0 + 2.0) / (3.0 * 4.0)
+// 		escreva("\nOperação: (1 + 2) / (3 * 4) = ", resultado, "\n")
+// 	}
+// } 
