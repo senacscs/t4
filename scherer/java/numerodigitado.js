9cb2641b555ff2be9447270caf9703e37e417b50
schererarthur <167102905+schererarthur@users.noreply.github.com>
2024-10-10 09:22:50 -0300

    comitei

---
+++ scherer/java/numerodigitado.js
@@ -0,0 +1,9 @@
+let campoPesquisa = document.getElementById("numero").value
+function numeroDigitado(){
+    // 		inteiro numero
+		let numero
+    // 		leia(numero)
+    numero = document.getElementById("numero").value
+// 		escreva("Digite um número inteiro: ")
+document.write("O numero digitado foi:",numero)	
+}
