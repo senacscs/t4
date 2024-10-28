a1fbafef653aed64c5f2f435474056aaa40b453f
joaquimthome <167103266+joaquimthome@users.noreply.github.com>
2024-10-28 10:30:04 -0300

    index e sites que estavam faltando

---
+++ joaquim/portugoljs/olamundo.js
@@ -0,0 +1,11 @@
+// programa 
+// { 
+// 	funcao inicio () 
+// 	{
+// 		escreva("Olá Mundo!\n")
+// 	} 
+// }
+
+console.log("Olá Mundo!\n");
+alert("Olá Mundo!\n");
+document.write("Olá Mundo!\n");
---
+++ joaquim/portugoljs/seunome.js
@@ -0,0 +1,17 @@
+// programa
+// {
+// 	funcao inicio ()
+// 	{
+// 		cadeia nome
+
+// 		escreva("Digite seu nome: ")
+// 		leia(nome)
+// 	}
+// }
+
+function nomeDigitado() {
+    let nome;
+    nome = document.getElementById("nome").value
+    alert("O nome digitado foi: ", nome);
+    document.write("O nome digitado foi: ", nome);
+}
