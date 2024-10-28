80860dbc855584911a2ebf7fa6fcb72b2a3de24a
gabrielludwig <167104168+gabrielludwig123@users.noreply.github.com>
2024-10-28 10:55:45 -0300

    subindo meus sites

---
+++ ludwig/imersao1/dados.js
@@ -0,0 +1,57 @@
+let dados = [
+    {
+        titulo: "Porsche 911 ",
+        descricao: "O 911 é conhecido por seu perfil inconfundível, com uma silhueta baixa e um design de corpo que evoluiu ao longo das décadas, mas manteve elementos clássicos como as linhas arredondadas e os faróis dianteiros circulares. O carro é tradicionalmente um coupé de duas portas, mas também está disponível em versões cabriolet (conversível) e Targa (com uma seção removível do teto e uma estrutura de barra de proteção).",
+        link: "https://www.porsche.com/brazil/pt/models/911/carrera-models/911-carrera/",
+        tags: "Porsche 911 "
+    },
+    {
+        titulo: "Chevrolet corvette ",
+        descricao: "O Chevrolet Corvette Stingray C8 é alimentado por um motor V8 de 6,2 litros que produz 490 cavalos de potência e 64.28 kgfm de torque. Ele pode acelerar de 0 a 100 km/h em menos de 3 segundos e tem uma velocidade máxima de 312 km/h. O carro também possui uma transmissão automática de dupla embreagem de oito velocidades.",
+        link: "https://directimports.com.br/importacao/chevrolet-corvette/#:~:text=O%20Chevrolet%20Corvette%20Stingray%20C8,máxima%20de%20312%20km%2Fh.",
+        tags: "chevrolet corvette "
+    },
+    {
+        titulo: "Lamborghini Huracán",
+        descricao: "O Lamborghini Huracán é a fusão perfeita de tecnologia e design. Com suas linhas nítidas e aerodinâmicas, projetadas para cortar o ar e domar a estrada, você sentirá uma emoção só de olhar para ele. A única coisa melhor do que apreciar esta beleza à distância é realmente tocá-la. O melhor artesanato italiano esbanjou acabamentos de prestígio e qualidade sem precedentes.",
+        link: "https://www-lamborghini-com.translate.goog/en-en/models/huracan?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc&_x_tr_hist=true",
+        tags: "Lamborghini Huracán"
+    },
+    {
+        titulo: "Lamborghini Urus ",
+        descricao: "Uma declaração de liberdade, o Urus permite que você descubra qualquer terreno, de pista a areia, gelo, cascalho ou pedras. É o supercarro esportivo completo e muito mais. Ele permite que você explore novos caminhos e novas maneiras de se expressar — ​​aceitar desafios com confiança e viver a vida ao máximo. Você não tem medo de ir longe: é isso que você aspira. Desbloqueie qualquer estrada, desbloqueie sua personalidade.",
+        link: "https://www-lamborghini-com.translate.goog/en-en/models/urus?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc&_x_tr_hist=true",
+        tags: "Lamborghini Urus"
+    },
+    {
+        titulo: "Audi R8",
+        descricao: "Quem senta atrás do volante de um R8 se sente como um piloto de corridas. Todas as funções são pensadas para o conforto do motorista e tornam simples até as condições extremas. Os assentos com apoio de cabeça oferecem ergonomia e conforto para dirigir sem preocupação. Há também um botão para deixar o ronco do motor 5.2 V10 de 610 cavalos de potência ainda mais poderoso e um seletor, que regula o coeficiente de atrito para três diferentes condições: seco, molhado ou neve, tornando única a experiência de dirigir o Audi R8.",
+        link: "https://www.audi.com.br/br/web/pt/models/r8/r8-coupe-v10-performance-quattro.html",
+        tags: "Audi r8"
+    },
+    {
+        titulo: "Ferrari F50",
+        descricao: "Criado para celebrar o 50º aniversário da Ferrari, o F50 foi o mais próximo de um carro de Fórmula 1 de rua que a empresa já havia construído. Dada sua abordagem intransigente e purista para alto desempenho, o F50 era desprovido de direção hidráulica, frenagem assistida e ABS, mas fazia uso extensivo de materiais compostos sofisticados, tecnologia de construção estilo F1 e aerodinâmica.",
+        link: "https://www-ferrari-com.translate.goog/en-EN/auto/f50?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc&_x_tr_hist=true",
+        tags: "Ferrari F50"
+    },
+    {
+        titulo: "Mustang Shelby GT",
+        descricao: "O icônico Shelby GT retornou com mais potência, desempenho e estilo Shelby! O recém-projetado Shelby GT combina manuseio fantástico, forte potência V8, som de escapamento musculoso e aparência atraente. A versão naturalmente aspirada produz 460 cavalos de potência. A potência adicional vem de um novo escapamento, enquanto um supercharger está disponível para aumentar a potência para 750 HP",
+        link: "https://www-shelby-com.translate.goog/Vehicles/Shelby-GT-2023?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc",
+        tags: "Mustang Shelby GT"
+    },
+    {
+        titulo: "Mclaren Senna",
+        descricao: "Pensamos no McLaren Senna como um carro de corrida - projetado e construído para andar nas pistas, acima de tudo. Sua aerodinâmica ativa é pioneira, e a visibilidade sem igual gerada pelas portas revestidas de vidro proporcionam confiança total ao motorista. A engenharia exaustiva e arrojada nos levou a reavaliar e revisar cada componente, não importando se era pequeno ou insignificante, para maximizar o desempenho. Usamos fibra de carbono no monocoque, na carroceria e em onde mais foi necessário ter leveza, rigidez e força - é por isso que o McLaren Senna é carro de estrada mais leve que a McLaren Automotive já construiu.",
+        link: "https://cars.mclaren.com/br-pt/ultimate-series/mclaren-senna",
+        tags: "Mclaren Senna"
+    },
+    {
+        titulo: "",
+        descricao: "",
+        link: "",
+        tags: ""
+    }
+    
+];
