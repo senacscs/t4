00745dfc360d8017d32206a37754d09b0bcd1ef2
pedroarthur22 <167103123+pedroarthur22@users.noreply.github.com>
2024-11-07 09:17:31 -0300

    portugol

---
+++ pedroa/portugoljs/seunome.js
@@ -0,0 +1,16 @@
+/*programa
+{
+	funcao inicio ()
+	{
+		cadeia nome
+
+		escreva("Digite seu nome: ")
+		leia(nome)
+	}
+} */
+
+	function seunome () {
+		let nome = document.getElementById("nome").value 
+		document.write("Oi, " + nome)
+
+	}
