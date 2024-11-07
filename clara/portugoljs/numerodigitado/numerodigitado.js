79cb3423265b05554c8763d859f32fec76a5a9c8
claradick <167199495+claradick@users.noreply.github.com>
2024-11-07 10:57:03 -0300

    subindo trabalho de tecnico js

---
+++ clara/portugoljs/numerodigitado/numerodigitado.js
@@ -0,0 +1,8 @@
+function inicio() {
+    let numero;
+
+    numero = parseInt (prompt("Digite um número inteiro: ") );
+    console.log("O número digitado foi: " + numero + "\n");
+ alert ("o número digitado foi: " + numero)
+}
+
