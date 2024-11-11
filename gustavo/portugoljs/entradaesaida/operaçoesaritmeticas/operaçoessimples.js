f8f1b0d328eefc2d59ace642a06e2e28ff04a5a7
GustavoFlorisbalTatsch <167103391+GustavoFlorisbalTatsch@users.noreply.github.com>
2024-11-11 10:39:24 -0300

    portugoljs mais atualizado

--- gustavo/portugoljs/entradaesaida/operaçoesaritmeticas/operaçoessimples.js
+++ gustavo/portugoljs/entradaesaida/operaçoesaritmeticas/operaçoessimples.js
@@ -1,18 +1,22 @@
 function calcular() {
     // Lê os valores dos inputs
-    var a= parseFloat(document.getElementById("num1").value);
+    var a = parseFloat(document.getElementById("num1").value);
     var b = parseFloat(document.getElementById("num2").value);
 
     // Realiza os cálculos
-    var soma = a + b // Soma os dois valores;
-    var sub = a - b // Subtrai os dois valores;
-    var mult = a * b // Multiplica os dois valores;
-    var div = a / b // Divide os dois valores;
+    var soma = a + b; // Soma os dois valores
+    var sub = a - b; // Subtrai os dois valores
+    var mult = a * b; // Multiplica os dois valores
+    var div = a / b; // Divide os dois valores
+    var logA = Math.log(a); // Logaritmo natural de a
+    var logB = Math.log(b); // Logaritmo natural de b
 
     // Usando document.write para exibir os resultados diretamente no corpo da página
     document.write("<h2>Resultados:</h2>");
-    document.write("A soma dos números é igual a: ", soma) 		// Exibe o resultado da soma"<br>");
-    document.write("A subtração dos números é igual a: ", sub) 		// Exibe o resultado da subtração"<br>");
-    document.write("A multiplicação dos números é igual a: ", mult) 	// Exibe o resultado da multiplicação"  "<br>");
-    document.write("A divisão dos números é igual a: ", div, "\n") 	// Exibe o resultado da divisão"  "<br>");
+    document.write("A soma dos números é igual a: " + soma + "<br>");
+    document.write("A subtração dos números é igual a: " + sub + "<br>");
+    document.write("A multiplicação dos números é igual a: " + mult + "<br>");
+    document.write("A divisão dos números é igual a: " + div + "<br>");
+    document.write("O logaritmo natural de " + a + " é: " + logA + "<br>");
+    document.write("O logaritmo natural de " + b + " é: " + logB + "<br>");
 }
