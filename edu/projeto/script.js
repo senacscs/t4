5353a2c82e2fc196c366774c52f428b68cae40c9
Edu <167102925+eduardosoaresdure@users.noreply.github.com>
2024-11-14 07:39:40 -0300

    Site de PI, com botão para deixa-lo em inglês

---
+++ edu/projeto/script.js
@@ -0,0 +1,32 @@
+let lang = "pt";
+let pt = Array.from(document.getElementsByClassName("pt"));
+let en = Array.from(document.getElementsByClassName("en"));
+
+
+function trocarLingua() {
+    switch (lang) {
+        case "pt":
+            console.log("pt");
+            lang = "en";
+            pt.forEach(element => {
+                element.style.display = "none";
+            });
+            en.forEach(element => {
+                element.style.display = "inline";
+            });
+            break;
+
+        case "en":
+            console.log("en");
+            lang = "pt";
+            en.forEach(element => {
+                element.style.display = "none";
+            });
+            pt.forEach(element => {
+                element.style.display = "inline";
+            });
+            break;
+    }
+}
+
+document.getElementById("bot").addEventListener("click", function () { trocarLingua() });
