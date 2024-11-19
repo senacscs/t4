84fc454db698aa6667e07b9c134a7a646796bb16
joaogoetzedasilva <167103392+joaogoetzedasilva@users.noreply.github.com>
2024-11-19 09:57:15 -0300

    Arrumando erros de portugues e tirando o media e o js

--- joaogoetze/inverter.js
+++
@@ -1,6 +0,0 @@
-function invertSections() {
-    const mainContent = document.getElementById('main-content');
-    const sections = Array.from(mainContent.querySelectorAll('section'));
-    sections.reverse().forEach(section => mainContent.appendChild(section));
- }
- 
