2f880098a15504f3677a77d565cb5182348133fa
JErnesto <167102953+JErnesto42@users.noreply.github.com>
2024-11-25 10:56:58 -0300

    sla tentei arrumar o log da calculadora

--- ernesto/portugoljs/operacao.js
+++ ernesto/portugoljs/operacao.js
@@ -1,40 +1,25 @@
-/*programa
-{
-	funcao inicio()
-	{
-		real a, b, soma, sub, mult, div
-		
-		escreva("Digite o primeiro número: ")
-		leia(a)
-
-		escreva("Digite o segundo número: ")
-		leia(b)
-
-		soma = a + b // Soma os dois valores
-		sub  = a - b // Subtrai os dois valores
-		mult = a * b // Multiplica os dois valores
-		div  = a / b // Divide os dois valores
-
-		escreva("\nA soma dos números é igual a: ", soma) 		// Exibe o resultado da soma
-		escreva("\nA subtração dos números é igual a: ", sub) 		// Exibe o resultado da subtração
-		escreva("\nA multiplicação dos números é igual a: ", mult) 	// Exibe o resultado da multiplicação
-		escreva("\nA divisão dos números é igual a: ", div, "\n") 	// Exibe o resultado da divisão
-	}
+function getBaseLog(x, y) {
+	return Math.log(y) / Math.log(x);
 }
-    */ 
+
+console.log(getBaseLog(2, 8));
+
+console.log(getBaseLog(5, 625));
+
 function calcular() {
-    let a = parseFloat(document.getElementById('num1').value);
-    let b = parseFloat(document.getElementById('num2').value);
+	let a, b, soma, sub, mult, div, log;
+	a = parseInt(document.getElementById("num1").value);
+	b = parseInt(document.getElementById("num2").value);
 
-    let soma = a + b;
-    let sub = a - b;
-    let mult = a * b;
-    let div = a / b;
+	soma = a + b;
+	sub = a - b;
+	mult = a * b;
+	div = a / b;
+	log = getBaseLog(a, b);
 
-    document.getElementById('resultado').innerHTML = `
-        <p>A soma dos números é igual a: ${soma}</p>
-        <p>A subtração dos números é igual a: ${sub}</p>
-        <p>A multiplicação dos números é igual a: ${mult}</p>
-        <p>A divisão dos números é igual a: ${div}</p>
-    `;
+	document.write("A soma dos números é igual a: ", soma);
+	document.write("<br>A subtração dos números é igual a: ", sub);
+	document.write("<br>A multiplicação dos números é igual a: ", mult);
+	document.write("<br>A divisão dos números é igual a: ", div);
+	document.write("<br>O log de base " + a + " do número " + b + " é igual a: ", log);
 }
