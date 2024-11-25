f85480d79d846891b2e2629b756e327008d5d875
GustavoFlorisbalTatsch <167103391+GustavoFlorisbalTatsch@users.noreply.github.com>
2024-11-25 11:02:08 -0300

    portugoljs mais atualizado e codigos corrigidos

--- gustavo/portugoljs/entradaesaida/operaçoesaritmeticas/operaçoessimples.js
+++ gustavo/portugoljs/entradaesaida/operaçoesaritmeticas/operaçoessimples.js
@@ -8,8 +8,7 @@ function calcular() {
     var sub = a - b; // Subtrai os dois valores
     var mult = a * b; // Multiplica os dois valores
     var div = a / b; // Divide os dois valores
-    var logA = Math.log(a); // Logaritmo natural de a
-    var logB = Math.log(b); // Logaritmo natural de b
+    var logBaseB = Math.log(a) / Math.log(b); // Logaritmo de a na base b
 
     // Usando document.write para exibir os resultados diretamente no corpo da página
     document.write("<h2>Resultados:</h2>");
@@ -17,6 +16,5 @@ function calcular() {
     document.write("A subtração dos números é igual a: " + sub + "<br>");
     document.write("A multiplicação dos números é igual a: " + mult + "<br>");
     document.write("A divisão dos números é igual a: " + div + "<br>");
-    document.write("O logaritmo natural de " + a + " é: " + logA + "<br>");
-    document.write("O logaritmo natural de " + b + " é: " + logB + "<br>");
+    document.write("O logaritmo de " + a + " na base " + b + " é: " + logBaseB + "<br>");
 }
