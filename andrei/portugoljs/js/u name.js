a6327aecca04e97dc42efbf20c9863501bbf1775
AndreiKohls <167135621+AndreiKohls@users.noreply.github.com>
2024-11-25 10:37:56 -0300

    INDEX E PORTUGOL

---
+++ andrei/portugoljs/js/u name.js
@@ -0,0 +1,15 @@
+// programa
+// {
+// 	funcao inicio ()
+// 	{
+// 		cadeia nome
+
+// 		escreva("Digite seu nome: ")
+// 		leia(nome)
+// 	}
+// }
+
+function sim(){
+ const nsei = document.getElementById('nome').value;
+ document.getElementById("res").innerText = `Olá ${nsei}`;
+}
