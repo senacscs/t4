046cc9f08b79685a782f606b2aaada1620489813
pedrofa07 <167103014+pedrofa07@users.noreply.github.com>
2024-11-25 10:54:07 -0300

    kkk

--- pedrofa/portugoljs/exercicio2/operacaosimples.js
+++ pedrofa/portugoljs/exercicio2/operacaosimples.js
@@ -23,18 +23,25 @@
 }
 */
 function numeroDigitado() {
-    let a = parseFloat(document.getElementById("number1").value)
-    let b = parseFloat(document.getElementById("number2").value)
+    let a = parseFloat(document.getElementById("number1").value);
+    let b = parseFloat(document.getElementById("number2").value);
 
-    let soma, sub, mult, div
+    let soma, sub, mult, div, logA, logB;
 
     soma = a + b;
     sub = a - b;
     mult = a * b;
     div = a / b;
 
-    document.write("O resultado da soma é:", soma)
-    document.write("O resultado da subtração é:", sub)
-    document.write("O resultado da multiplicação é:", mult)
-    document.write("O resultado da divisão é:", div)
+
+    logA = Math.log10(a);
+    logB = Math.log10(b);
+
+    document.write("O resultado da soma é: ", soma, "<br>");
+    document.write("O resultado da subtração é: ", sub, "<br>");
+    document.write("O resultado da multiplicação é: ", mult, "<br>");
+    document.write("O resultado da divisão é: ", div, "<br>");
+    document.write("O logaritmo de A (base 10) é: ", logA, "<br>");
+    document.write("O logaritmo de B (base 10) é: ", logB, "<br>");
 }
+
