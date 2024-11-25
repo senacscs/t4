c62b4c5b10c79bf5d0bf6dc59b569aa18a660a7f
gabrielludwig <167104168+gabrielludwig123@users.noreply.github.com>
2024-11-25 11:20:12 -0300

    subindo portugol

--- ludwig/portugoljs/operacoes_aritmeticas/operacaosimples.js
+++ ludwig/portugoljs/operacoes_aritmeticas/operacaosimples.js
@@ -1,40 +1,39 @@
-/* programa
-{
-    funcao inicio()
-    {
-        real a, b, soma, sub, mult, div
-    	
-        escreva("Digite o primeiro número: ")
-        leia(a)
+// function numeroDigitado() {
+//     let a = parseFloat(document.getElementById("number1").value)
+//     let b = parseFloat(document.getElementById("number2").value)
 
-        escreva("Digite o segundo número: ")
-        leia(b)
-
-        soma = a + b // Soma os dois valores
-        sub  = a - b // Subtrai os dois valores
-        mult = a * b // Multiplica os dois valores
-        div  = a / b // Divide os dois valores
-
-        escreva("\nA soma dos números é igual a: ", soma) 		// Exibe o resultado da soma
-        escreva("\nA subtração dos números é igual a: ", sub) 		// Exibe o resultado da subtração
-        escreva("\nA multiplicação dos números é igual a: ", mult) 	// Exibe o resultado da multiplicação
-        escreva("\nA divisão dos números é igual a: ", div, "\n") 	// Exibe o resultado da divisão
-    }
-}
-*/
+//     let soma, sub, mult, div, log
+ 
+//     soma = a + b;
+//     sub = a - b;
+//     mult = a * b;
+//     div = a / b;
+ 
+//     document.write("O resultado da soma é:", soma)
+//     document.write("O resultado da subtração é:", sub)
+//     document.write("O resultado da multiplicação é:", mult)
+//     document.write("O resultado da divisão é:", div)
+//     document.write("O resultado do log é:", log)
+// }
+ 
 function numeroDigitado() {
-    let a = parseFloat(document.getElementById("number1").value)
-    let b = parseFloat(document.getElementById("number2").value)
+    let a = parseFloat(document.getElementById("number1").value);
+    let b = parseFloat(document.getElementById("number2").value);
 
-    let soma, sub, mult, div
+    let soma, sub, mult, div, logA, logB;
 
     soma = a + b;
     sub = a - b;
     mult = a * b;
     div = a / b;
 
-    document.write("O resultado da soma é:", soma)
-    document.write("O resultado da subtração é:", sub)
-    document.write("O resultado da multiplicação é:", mult)
-    document.write("O resultado da divisão é:", div)
+    logA = Math.log10(a);
+    logB = Math.log10(b);
+
+    document.write("O resultado da soma é: " + soma + "<br>");
+    document.write("O resultado da subtração é: " + sub + "<br>");
+    document.write("O resultado da multiplicação é: " + mult + "<br>");
+    document.write("O resultado da divisão é: " + div + "<br>");
+    document.write("O logaritmo de " + a + " é: " + logA + "<br>");
+    document.write("O logaritmo de " + b + " é: " + logB + "<br>");
 }
