e5b57ca7cf7da537c1fad37def1b80a45853da61
schererarthur <167102905+schererarthur@users.noreply.github.com>
2024-11-27 09:20:57 -0300

    mais portugol na index

--- scherer/portugoljs/operacoessimples.js
+++ scherer/portugoljs/operacoessimples.js
@@ -1,56 +1,20 @@
-// programa
-// {
-//    funcao inicio()
-//    {
-//       real a, b, soma, sub, mult, div
-     
-//       escreva("Digite o primeiro número: ")
-//       leia(a)
- 
-//       escreva("Digite o segundo número: ")
-//       leia(b)
- 
-//       soma = a + b // Soma os dois valores
-//       sub  = a - b // Subtrai os dois valores
-//       mult = a * b // Multiplica os dois valores
-//       div  = a / b // Divide os dois valores
- 
-//       escreva("\nA soma dos números é igual a: ", soma)     // Exibe o resultado da soma
-//       escreva("\nA subtração dos números é igual a: ", sub)       // Exibe o resultado da subtração
-//       escreva("\nA multiplicação dos números é igual a: ", mult)  // Exibe o resultado da multiplicação
-//       escreva("\nA divisão dos números é igual a: ", div, "\n")   // Exibe o resultado da divisão
-//    }
-// }
- 
-function matematica(){
-    //      cadeia matematica
-     let a, b, soma, sub, mult, div;
-    //      real a, b, soma, sub, mult, div
-        //     leia(numero)
-        a = parseInt(document.getElementById("numero").value)
-        b = parseInt(document.getElementById("numero").value)
+function numeroDigitado() {
+   let a = parseFloat(document.getElementById("number1").value);
+   let b = parseFloat(document.getElementById("number2").value);
+
+   let soma, sub, mult, div, logA, logB;
+
+   soma = a + b;
+   sub = a - b;
+   mult = a * b;
+   div = a / b;
    
-   
-   
-   
-   
-    //      soma = a + b // Soma os dois valores
-       soma = a + b
-    //      sub  = a - b // Subtrai os dois valores
-       sub = a - b
-    //      mult = a * b // Multiplica os dois valores
-       mult = a * b
-    //      div  = a / b // Divide os dois valores
-       div = a / b
-     
-       
-    //      escreva("\nA soma dos números é igual a: ", soma)     // Exibe o resultado da soma
-    document.write("\nA soma dos números é igual a: ", soma)  
-    //      escreva("\nA subtração dos números é igual a: ", sub)       // Exibe o resultado da subtração
-    document.write("\nA subtração dos números é igual a: ", sub)  
-    //      escreva("\nA multiplicação dos números é igual a: ", mult)  // Exibe o resultado da multiplicação
-    document.write("\nA subtração dos números é igual a: ", mult)
-    //      escreva("\nA divisão dos números é igual a: ", div, "\n")   // Exibe o resultado da divisão
-    document.write("\nA divisão dos números é igual a: ", div, "\n")
-   
-    }
+
+   log = Math.log(a) / Math.log(b);
+
+   document.write("O resultado da soma é: " + soma + "<br>");
+   document.write("O resultado da subtração é: " + sub + "<br>");
+   document.write("O resultado da multiplicação é: " + mult + "<br>");
+   document.write("O resultado da divisão é: " + div + "<br>");
+   document.write("O logaritmo de é: ", log, "<br>")
+}
