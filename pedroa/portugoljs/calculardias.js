465b4f9939d899a72afb9e6c3ba248a22a6a4303
pedroarthur22 <167103123+pedroarthur22@users.noreply.github.com>
2024-11-27 09:14:54 -0300

    .

--- pedroa/portugoljs/calculardias.js
+++
@@ -1,9 +0,0 @@
-function calcularDias() {
-    let anoAtual = parseInt(document.getElementById("anoAtual").value) || 0;
-    let qtdAnosBi = Math.floor(anoAtual / 4);
-    let dias = (anoAtual - 1) * 365 + qtdAnosBi;
-
-    document.getElementById("resultado").innerHTML = `
-        <p>Já se passaram ${dias} dias desde 01/01/0001</p>
-    `;
-}
