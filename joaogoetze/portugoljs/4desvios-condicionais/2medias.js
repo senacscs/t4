6ff811ca542fd74fa96c25ec42e3b237fd151433
joaogoetzedasilva <167103392+joaogoetzedasilva@users.noreply.github.com>
2024-11-29 12:19:52 -0300

    botando mais exemplos dos desvios

---
+++ joaogoetze/portugoljs/4desvios-condicionais/2medias.js
@@ -0,0 +1,63 @@
+// inclua biblioteca Matematica --> mat
+	
+// 	funcao inicio ()
+// 	{
+// 		real m1, m2, m3, media
+
+// 		escreva ("Informe a média 1: " )
+// 		leia (m1)
+// 		escreva( "Informe a média 2: ")
+// 		leia (m2)
+// 		escreva ("Informe a média 3: ")
+// 		leia (m3)
+
+// 		media = (m1 + m2 + m3) / 3 
+
+// 		limpa()
+// 		escreva ("A média final é: ", mat.arredondar(media, 2), "\n\n")
+
+
+// 		se (m1 < media)
+// 		{ 
+// 			escreva ("A média 1 é menor que a média final\n") 
+// 		}
+		
+// 		se (m2 < media) 
+// 		{
+// 			escreva ("A média 2 é menor que a média final\n")
+// 		}
+		
+// 		se (m3 < media)
+// 		{
+// 			escreva ("A média 3 é menor que a média final\n")
+// 		}		
+// 	}
+
+function medias() {
+
+	let m1, m2, m3, media
+
+    media = parseInt(document.getElementById("medida1").value)
+    media = parseInt(document.getElementById("medida2").value)
+    media = parseInt(document.getElementById("medida3").value)
+
+ 	 media = (m1 + m2 + m3) / 3 
+
+    document.write("A média final é: ", Math.round(media, 2), "\n\n")
+
+ 		if (m1 < media)
+		{ 
+			document.write ("A média 1 é menor que a média final\n") 
+		}
+		
+		if (m2 < media) 
+		{
+			document.write ("A média 2 é menor que a média final\n")
+		}
+		
+		if (m3 < media)
+		{
+			document.write ("A média 3 é menor que a média final\n")
+		}	
+
+}
