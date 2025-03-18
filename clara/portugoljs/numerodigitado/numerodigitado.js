85cf741f808fb52138d5d79a22a640baed7f8241
claradick <167199495+claradick@users.noreply.github.com>
2025-03-18 10:57:42 -0300

    subindo site

--- clara/portugoljs/numerodigitado/numerodigitado.js
+++
@@ -1,8 +0,0 @@
-function inicio() {
-    let numero;
-
-    numero = parseInt (prompt("Digite um número inteiro: ") );
-    console.log("O número digitado foi: " + numero + "\n");
- alert ("o número digitado foi: " + numero)
-}
-
--- clara/portugoljs/raizepotencia/seunome/seunome.js
+++
@@ -1,5 +0,0 @@
-function sim(){
-    const nsei = document.getElementById('nome').value;
-    document.getElementById("res").innerText = `Olá ${nsei}`;
-
-}
