98608add9ceccc8765164d7345924b69e6ce9d27
claradick <167199495+claradick@users.noreply.github.com>
2025-03-18 11:11:43 -0300

    subindo tudo

---
+++ clara/script.js
@@ -0,0 +1,26 @@
+
+"use strict";
+(function () {
+	window.onload = () => {
+		const obj = document.querySelector("#gallery");
+		const time = 10000;
+		function animStart() {
+			if (obj.classList.contains("active") == false) {
+				obj.classList.add("active");
+				setTimeout(() => {
+					animEnd();
+				}, time);
+			}
+		}
+		function animEnd() {
+			obj.classList.remove("active");
+			obj.offsetWidth;
+		}
+		document.addEventListener("scroll", function () {
+			// scroll or scrollend
+			animStart();
+		});
+		window.addEventListener("resize", animStart);
+		animStart();
+	};
+})();
