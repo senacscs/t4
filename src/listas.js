92fed4bd49293ed7a1ccdba890310d791d528b64
iuriwickert <167103303+iuriwickert@users.noreply.github.com>
2025-03-24 08:05:07 -0300

    Nova Index para turma

---
+++ src/listas.js
@@ -0,0 +1,68 @@
+const alunos = [
+    { "nome": "Ana E.", "pasta": "elisa" },
+    { "nome": "Analice", "pasta": "ana" },
+    { "nome": "Andrei", "pasta": "andrei" },
+    { "nome": "Andri", "pasta": "andri" },
+    { "nome": "Aquino", "pasta": "aquino" },
+    { "nome": "Arthur", "pasta": "scherer" },
+    { "nome": "Augusto", "pasta": "augusto" },
+    { "nome": "Cícero", "pasta": "cicero" },
+    { "nome": "Clara", "pasta": "clara" },
+    { "nome": "Davi", "pasta": "davie" },
+    { "nome": "Eduardo D.", "pasta": "edu" },
+    { "nome": "Eduardo M.", "pasta": "moraes" },
+    { "nome": "Enzo", "pasta": "enzo" },
+    { "nome": "Gabriel Ludwig", "pasta": "Ludwig" },
+    { "nome": "Gracia🧃", "pasta": "garcia" },
+    { "nome": "Guilherme D.", "pasta": "dalke" },
+    { "nome": "Guilherme K.", "pasta": "kipper" },
+    { "nome": "Gustavo", "pasta": "gustavo" },
+    { "nome": "Henrique", "pasta": "henrique" },
+    { "nome": "Iuri🧃", "pasta": "wickert" },
+    { "nome": "João E.", "pasta": "ernesto" },
+    { "nome": "João G.", "pasta": "joaogoetze" },
+    { "nome": "Joaquim", "pasta": "joaquim" },
+    { "nome": "Larissa", "pasta": "larii" },
+    { "nome": "Lucas", "pasta": "lucas" },
+    { "nome": "Mohr🌙", "pasta": "mohr" },
+    { "nome": "Marina", "pasta": "marina" },
+    { "nome": "Matheus B.", "pasta": "teteu" },
+    { "nome": "Matheus F.", "pasta": "" },
+    { "nome": "Nicole", "pasta": "nick" },
+    { "nome": "Pedro A.", "pasta": "pedroa" },
+    { "nome": "Pedro B.", "pasta": "backes" },
+    { "nome": "Pedro F.", "pasta": "pedrofa" },
+    { "nome": "Rafael", "pasta": "rafaelmarmitt" },
+    { "nome": "Sara", "pasta": "sararosa" },
+    { "nome": "Theo", "pasta": "theo" },
+    { "nome": "Thom🍒", "pasta": "brandt" }
+];
+
+
+// ---------------------------
+
+const ProjetosP = [  
+    //? Projetos Primeiro Ano
+    { "nome": "Index", "url": "index.html"},
+    { "nome": "Currículo", "url": "curriculo"},
+    { "nome": "Site de Triângulos", "url": "triangulos"},
+    { "nome": "Poemas", "url": "poema"},
+    { "nome": "Ponte Da Vinci", "url": "pontedavinci"},
+    { "nome": "Imersão", "url": "imersao1"},
+    { "nome": "Autocuidado", "url": "autocuidado"}, 
+    { "nome": "Site de tecnologia", "url": "tecnologiamat"},  
+    { "nome": "Jogo Escape Room", "url": "jogo1"},   
+    { "nome": "Site feito a mão", "url": "teste_ods"},  
+    { "nome": "Oficina da zoeira", "url": "oficina"},  
+    { "nome": "Projeto PI", "url": "projeto"},
+    { "nome": "Site pros responsáveis", "url": "responsaveis"},
+    { "nome": "Indicações CSS", "url": "indicacoescss"},
+    { "nome": "Indicações HTML", "url": "indicacoeshtml"},  
+    { "nome": "Olá Mundo", "url": "olamundo"},
+]
+
+const ProjetosS = [  
+    //? Projetos Segundo Ano
+    { "nome": "Index", "url": "index.html"},
+    { "nome": "Calendario", "url": "calendario"}
+]
---
+++ src/main.js
@@ -0,0 +1,106 @@
+//! Cards, Pesquisa e Filtros
+function atualizarCards(p, f) {   
+    const cD = document.getElementById("cd");
+    cD.innerHTML = "";
+    alunos.sort((a, b) => a.nome.localeCompare(b.nome));
+    if (!f) {
+        let fils = document.getElementsByClassName("fil");
+        for (let i = 0; i < fils.length; i++) {
+            if (fils[i].classList.contains("sele")) {
+                f = fils[i].value;
+                break;
+            }
+        }
+    }
+    for (let i = 0; i < alunos.length; i++) {
+        let elem1 = document.createElement("a");
+        elem1.setAttribute("class", "card");
+        elem1.setAttribute("href", "/" + alunos[i].pasta + "/" + f);
+        let elem2 = document.createElement("div");
+        let elem3 = document.createElement("p");
+        elem3.innerText = alunos[i].nome;
+        cD.appendChild(elem1);
+        elem1.appendChild(elem2);
+        elem2.appendChild(elem3); 
+        if (p) {
+            if (alunos[i].nome.toLowerCase().includes(p.toLowerCase())) {
+                elem1.style.display = "block";
+            } else {
+                elem1.style.display = "none";
+            }
+        } else {
+            elem1.style.display = "block";
+        }
+    }
+} 
+
+//! Filtros
+//? Abrir a aba de filtros
+function abrirFiltros() {
+    let filtros = document.getElementById("filtros");
+    if (filtros.classList.contains("aberto")) {
+        filtros.classList.toggle("aberto2");
+        setTimeout(() => {
+            filtros.classList.toggle("aberto");
+        }, 200);
+    } else {
+        filtros.classList.toggle("aberto");
+        setTimeout(() => {
+            filtros.classList.toggle("aberto2");
+        }, 100);
+    }
+}
+
+//? Responsividade da aba de filtros
+function filtroWidth() {
+    let barraW = document.getElementById("barraDePesquisa").offsetWidth;
+    let filtro = document.getElementById("filtros");
+    filtro.style.setProperty('--w', `${barraW}px`);
+}
+filtroWidth();
+window.addEventListener("resize", filtroWidth);
+//? Criar filtros 
+function filtrosCards(t) {
+    const fC = document.getElementById("filContainer");
+    fC.innerHTML = "";
+    let sp = document.getElementsByClassName("anpalr").length > 0 ? "p" : "s";
+    let proj = sp === "p" ? ProjetosP : ProjetosS;
+    let primeiroB = sp === "p" ? "primeiro/" : "segundo/"
+    for(let i = 0; i < proj.length; i++) {
+        let filbtn = document.createElement("button")
+        filbtn.setAttribute("class", "fil");
+        if(i === 0) {
+            filbtn.classList.add("sele");
+        };
+        filbtn.setAttribute("onclick", "selecionado(this)");
+        filbtn.setAttribute("value", primeiroB + proj[i].url);
+        let filp = document.createElement("p")
+        filp.innerText = proj[i].nome;
+        fC.appendChild(filbtn);
+        filbtn.appendChild(filp);
+    };
+    if (t == "1") {
+        let pesvValue = document.getElementById("pesquisa").value;
+        atualizarCards(pesvValue);
+    }
+};
+filtrosCards()
+//? Selecionar
+function selecionado(a) {
+    let fils = document.getElementsByClassName("fil");
+    for (let i = 0; i < fils.length; i++) {
+        fils[i].classList.remove("sele");
+    };
+    a.classList.add("sele");
+    let pesvValue = document.getElementById("pesquisa").value;
+    atualizarCards(pesvValue, a.value);
+};
+
+function troca() {
+    let anpal = document.getElementById("anpal")
+    anpal.classList.toggle("anpalr")
+    filtrosCards("1")
+}
+
+//! Chamar a função pra spawnar os cards IMPORTANTE ESTAR NO FINAL
+atualizarCards();
