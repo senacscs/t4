6c89210f2c4c08e941633819dc84727b78847ec9
henriquebender <167101931+henriquebender@users.noreply.github.com>
2025-04-17 10:26:09 -0300

    .

--- henrique/primeiro/poema/script.js
+++
@@ -1,5 +0,0 @@
-let scrollStep = 1.2;
-let scrollInterval = setInterval(() => {
-    window.scrollBy(0, scrollStep);
-    
-}, 30);
--- henrique/primeiro/portugoljs/numeros.js
+++
@@ -1,8 +0,0 @@
-function verificarNumerosIguais() {
-    const numeroUsuario = parseInt(document.getElementById('numeroUsuario').value);
-    const numeroSorteado = Math.floor(Math.random() * 7);
-    const mensagem = numeroUsuario === numeroSorteado 
-        ? `Os números são iguais! (${numeroUsuario})`
-        : `Os números são diferentes! Número digitado: ${numeroUsuario}, Número sorteado: ${numeroSorteado}.`;
-    document.getElementById('resultadoMsg').innerText = mensagem;
-}
